4039a31ce5ff3b02ed3d125709fb4b22fbd26b25
Dmitry Alexeev <alexeedm@ethz.ch>
2014-07-08 15:14:59 +0200

    Initial Commit

---
+++ CTC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CTC
+//
+//  Created by Dmitry Alexeev on 08.07.14.
+//  Copyright (c) 2014 Dmitry Alexeev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
