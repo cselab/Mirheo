1dfa7547678b0670dde978fb174a3161e1271a16
Diego Rossinelli <rossinelli.diego@gmail.com>
2014-07-10 18:25:20 +0200

    dpd with cuda and cell-lists

---
+++ cuda-dpd/dpd-cuda.cu
@@ -0,0 +1,376 @@
+#include <cstdio>
+#include <cassert>
+
+struct InfoDPD
+{
+    int nx, ny, nz, np, nsamples;
+    float XL, YL, ZL;
+    float xstart, ystart, zstart, rc, aij, gamma, sigma, invsqrtdt;
+    float * xp, *yp, *zp, *xv, *yv, *zv, *xa, *ya, *za, *rsamples;
+    int * starts;
+};
+
+__constant__ InfoDPD info;
+
+__global__ void pid2code(int * codes, int * pids)
+{
+    const int pid = threadIdx.x + blockDim.x * blockIdx.x;
+
+    if (pid >= info.np)
+	return;
+
+    const int ix = (int)((info.xp[pid] - info.xstart) / info.rc);
+    const int iy = (int)((info.yp[pid] - info.ystart) / info.rc);
+    const int iz = (int)((info.zp[pid] - info.zstart) / info.rc);
+    
+    assert(ix >= 0 && ix < info.nx);
+    assert(iy >= 0 && iy < info.ny);
+    assert(iz >= 0 && iz < info.nz);
+    
+    codes[pid] = ix + info.nx * (iy + info.nx * iz);
+    pids[pid] = pid;
+};
+
+__global__ void _gather(const float * input, const int * indices, float * output, const int n)
+{
+    const int tid = threadIdx.x + blockDim.x * blockIdx.x;
+
+    if (tid < n)
+	output[tid] = input[indices[tid]];
+}
+
+__device__ void _ccfc(const int s1, const int e1, const int s2, const int e2, const bool self, const int sr)
+{
+    if (threadIdx.x + threadIdx.y == 0)
+    {
+	for(int c = 0, i = s1; i < e1; ++i)
+	    for(int j = self ? i + 1 : s2 ; j < e2; ++j, ++c)
+	    {
+		float xr = info.xp[i] - info.xp[j];
+		float yr = info.yp[i] - info.yp[j];
+		float zr = info.zp[i] - info.zp[j];
+				
+		xr -= info.XL * floor(0.5f + xr / info.XL);
+		yr -= info.YL * floor(0.5f + yr / info.YL);
+		zr -= info.ZL * floor(0.5f + zr / info.ZL);
+
+		float rij = sqrtf(xr * xr + yr * yr + zr * zr);
+
+		xr /= rij;
+		yr /= rij;
+		zr /= rij;
+		    
+		float fc = max((float)0, info.aij * (1 - rij / info.rc));
+		float wr = max((float)0, 1 - rij / info.rc);
+		float wd = wr * wr;
+
+		float rdotv = xr * (info.xv[i] - info.xv[j]) + yr * (info.yv[i] - info.yv[j]) + zr * (info.zv[i] - info.zv[j]);
+		float gij = info.rsamples[sr + c] * info.invsqrtdt;
+		
+		float xf = (fc - info.gamma * wd * rdotv + info.sigma * wr * gij) * xr;
+		float yf = (fc - info.gamma * wd * rdotv + info.sigma * wr * gij) * yr;
+		float zf = (fc - info.gamma * wd * rdotv + info.sigma * wr * gij) * zr;
+
+		assert(!isnan(xf));
+		assert(!isnan(yf));
+		assert(!isnan(zf));
+
+		info.xa[i] += xf;
+		info.ya[i] += yf;
+		info.za[i] += zf;
+
+		info.xa[j] -= xf;
+		info.ya[j] -= yf;
+		info.za[j] -= zf;
+	    }
+    }
+}
+
+__device__ int _cid(int shiftcode)
+{
+    int3 indx = make_int3(blockIdx.x + (shiftcode & 1),
+			  blockIdx.y + ((shiftcode >> 1) & 1),
+			  blockIdx.z + ((shiftcode >> 2) & 1));
+
+    indx.x = (indx.x + info.nx) % info.nx;
+    indx.y = (indx.y + info.ny) % info.ny;
+    indx.z = (indx.z + info.nz) % info.nz;
+
+    return indx.x + info.nx * (indx.y + info.ny * indx.z);
+}
+
+__constant__ int edgeslutcount[4] = {8, 3, 2, 1};
+__constant__ int edgeslutstart[4] = {0, 8, 11, 13};
+__constant__ int edgeslut[14] = {0, 1, 2, 3, 4, 5, 6, 7, 2, 4, 6, 4, 5, 4};
+
+__global__ void _fc(const int xoffset, const int yoffset, const int zoffset, int * consumed)
+{
+    if (blockIdx.x % 2 != xoffset ||
+	blockIdx.y % 2 != yoffset ||
+	blockIdx.z % 2 != zoffset)
+	return;
+
+    const bool master = threadIdx.x + threadIdx.y == 0;
+
+    __shared__ volatile int offsetrsamples;
+    
+    for(int i = 0; i < 4; ++i)
+    {
+	const int cid1 = _cid(i);
+	const int s1 = info.starts[cid1];
+	const int e1 = info.starts[cid1 + 1];
+	
+	const int nentries = edgeslutcount[i];
+	const int entrystart = edgeslutstart[i];
+	
+	for(int j = 0; j < nentries; ++j)
+	{
+	    const int cid2 = _cid(edgeslut[j + entrystart]);
+	    const int s2 = info.starts[cid2];
+	    const int e2 = info.starts[cid2 + 1];
+
+	    const bool self = cid1 == cid2;
+	    const int rconsumption = self ? (e1 - s1) * (e1 - s1 - 1) / 2 : (e1 - s1) * (e2 - s2); 
+
+	    if(master)
+		offsetrsamples = atomicAdd(consumed, rconsumption);
+
+	    __syncthreads();
+
+	    if (offsetrsamples + rconsumption >= info.nsamples)
+		return;
+	    
+	    _ccfc(s1, e1, s2, e2, cid1 == cid2, offsetrsamples);
+	}
+    }
+}
+
+#include <unistd.h>
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/sequence.h>
+#include <thrust/sort.h>
+#include <thrust/binary_search.h>
+
+using namespace thrust;
+
+#define CUDA_CHECK(ans) do { cudaAssert((ans), __FILE__, __LINE__); } while(0)
+inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+    if (code != cudaSuccess) 
+    {
+	fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+	if (abort) exit(code);
+    }
+}
+
+template<typename T> T * _ptr(device_vector<T>& v) { return raw_pointer_cast(v.data()); }
+
+void _reorder(device_vector<float>& v, device_vector<int>& indx)
+{
+    device_vector<float> tmp(v.begin(), v.end());
+   
+    _gather<<<(v.size() + 127) / 128, 128>>>(_ptr(tmp), _ptr(indx), _ptr(v), v.size());
+
+    CUDA_CHECK(cudaPeekAtLastError());
+    CUDA_CHECK(cudaThreadSynchronize());
+}
+
+void forces_dpd_cuda(float * const _xp, float * const _yp, float * const _zp,
+		     float * const _xv, float * const _yv, float * const _zv,
+		     float * const _xa, float * const _ya, float * const _za,
+		     int * const order, const int np,
+		     const float rc,
+		     const float XL, const float YL, const float ZL,
+		     const float aij,
+		     const float gamma,
+		     const float sigma,
+		     const float invsqrtdt,
+   		     float * const _rsamples, const int nsamples)
+{
+    static bool initialized = false;
+    if (!initialized)
+    {
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, 0);
+	if (!prop.canMapHostMemory)
+	{
+	    printf("Capability zero-copy not there! Aborting now.\n");
+	    abort();
+	}
+	else
+	    cudaSetDeviceFlags(cudaDeviceMapHost);
+
+	initialized = true;
+    }
+	
+    int * consumed = NULL, * dconsumed = NULL;
+    cudaHostAlloc((void **)&consumed, sizeof(int), cudaHostAllocMapped);
+    assert(consumed != NULL);
+    *consumed = 0;
+    cudaHostGetDevicePointer(&dconsumed, consumed, 0);
+	
+    int nx = (int)ceil(XL / rc);
+    int ny = (int)ceil(YL / rc);
+    int nz = (int)ceil(ZL / rc);
+    const int ncells = nx * ny * nz;
+    
+    device_vector<int> starts(ncells + 1);
+    
+    device_vector<float> xp(_xp, _xp + np), yp(_yp, _yp + np), zp(_zp, _zp + np),
+	xv(_xv, _xv + np), yv(_yv, _yv + np), zv(_zv, _zv + np), rsamples(_rsamples, _rsamples + nsamples);
+
+    device_vector<float> xa(np), ya(np), za(np);
+    fill(xa.begin(), xa.end(), 0);
+    fill(ya.begin(), ya.end(), 0);
+    fill(za.begin(), za.end(), 0);
+    
+    InfoDPD c;
+    c.nx = nx;
+    c.ny = ny;
+    c.nz = nz;
+    c.np = np;
+    c.nsamples = nsamples;
+    c.XL = XL;
+    c.YL = YL;
+    c.ZL = ZL;
+    c.xstart = -XL * 0.5; 
+    c.ystart = -YL * 0.5; 
+    c.zstart = -ZL * 0.5; 
+    c.rc = rc;
+    c.aij = aij;
+    c.gamma = gamma;
+    c.sigma = sigma;
+    c.invsqrtdt = invsqrtdt;
+    c.xp = _ptr(xp);
+    c.yp = _ptr(yp);
+    c.zp = _ptr(zp);
+    c.xv = _ptr(xv);
+    c.yv = _ptr(yv);
+    c.zv = _ptr(zv);
+    c.xa = _ptr(xa);
+    c.ya = _ptr(ya);
+    c.za = _ptr(za);
+    c.rsamples = _ptr(rsamples);
+    c.starts = _ptr(starts);
+    
+    CUDA_CHECK(cudaMemcpyToSymbol(info, &c, sizeof(c)));
+
+    device_vector<int> codes(np), pids(np);
+    pid2code<<<(np + 127) / 128, 128>>>(_ptr(codes), _ptr(pids));
+
+    sort_by_key(codes.begin(), codes.end(), pids.begin());
+
+    _reorder(xp, pids);
+    _reorder(yp, pids);
+    _reorder(zp, pids);
+    
+    _reorder(xv, pids);
+    _reorder(yv, pids);
+    _reorder(zv, pids);
+    
+    device_vector<int> cids(ncells + 1);
+    sequence(cids.begin(), cids.end());
+
+    lower_bound(codes.begin(), codes.end(), cids.begin(), cids.end(), starts.begin());
+
+    for(int code = 0; code < 8; ++code)
+    {
+	_fc<<<dim3(c.nx, c.ny, c.nz), dim3(8, 8, 1)>>>(code & 1, (code >> 1) & 1, (code >> 2) & 1, dconsumed);
+	
+	CUDA_CHECK(cudaPeekAtLastError());
+	CUDA_CHECK(cudaThreadSynchronize());
+
+	if (*consumed >= nsamples)
+	{
+	    printf("done with code %d: consumed: %d\n", code, *consumed);
+	    printf("not a nice situation.\n");
+	    abort();
+	}
+    }
+
+    cudaFreeHost(consumed);
+    
+    const bool check = false;
+    if (!check)
+    {
+	copy(xp.begin(), xp.end(), _xp);
+	copy(yp.begin(), yp.end(), _yp);
+	copy(zp.begin(), zp.end(), _zp);
+	
+	copy(xv.begin(), xv.end(), _xv);
+	copy(yv.begin(), yv.end(), _yv);
+	copy(zv.begin(), zv.end(), _zv);
+
+	copy(xa.begin(), xa.end(), _xa);
+	copy(ya.begin(), ya.end(), _ya);
+	copy(za.begin(), za.end(), _za);
+    }
+    else
+    {
+	for(int i = 0; i < np; ++i)
+	{
+	    _xa[i] = _ya[i] = _za[i] = 0;
+	}
+	
+	for(int i = 0; i < np; ++i)
+	{
+	    printf("computing p %d\n", i);
+	    for(int j = i + 1; j < np; ++j)
+	    {
+		float xr = _xp[i] - _xp[j];
+		float yr = _yp[i] - _yp[j];
+		float zr = _zp[i] - _zp[j];
+				
+		xr -= c.XL * floor(0.5f + xr / c.XL);
+		yr -= c.YL * floor(0.5f + yr / c.YL);
+		zr -= c.ZL * floor(0.5f + zr / c.ZL);
+
+		float rij = sqrtf(xr * xr + yr * yr + zr * zr);
+
+		xr /= rij;
+		yr /= rij;
+		zr /= rij;
+		    
+		float fc = max((float)0, aij * (1 - rij / rc));
+		float wr = max((float)0, 1 - rij / rc);
+		float wd = wr * wr;
+
+		float rdotv = xr * (_xv[i] - _xv[j]) + yr * (_yv[i] - _yv[j]) + zr * (_zv[i] - _zv[j]);
+		float gij = 1;//dgauss(gen) / sqrt(dt);
+		
+		float xf = (fc - gamma * wd * rdotv + sigma * wr * gij) * xr;
+		float yf = (fc - gamma * wd * rdotv + sigma * wr * gij) * yr;
+		float zf = (fc - gamma * wd * rdotv + sigma * wr * gij) * zr;
+
+		assert(!isnan(xf));
+		assert(!isnan(yf));
+		assert(!isnan(zf));
+		
+		_xa[i] += xf;
+		_ya[i] += yf;
+		_za[i] += zf;
+		
+		_xa[j] -= xf;
+		_ya[j] -= yf;
+		_za[j] -= zf;
+	    }
+	}
+
+	for(int i = 0; i < np; ++i)
+	{
+	    const int mypid = pids[i];
+	    printf("gpu: pid %d -> %d, %f %f %f\n", i, mypid, (float)xp[i], (float)yp[i], (float)zp[i]);
+	       
+	    printf("ref: pid %d -> p %f %f %f\n", mypid, _xp[mypid], _yp[mypid], _zp[mypid]);
+	    printf("gpu: force %f %f %f, ref: force %f %f %f\n", (float)xa[i], (float)ya[i], (float)za[i], _xa[mypid], _ya[mypid], _za[mypid]);
+
+	    assert(fabs(xa[i] - _xa[mypid]) < 1e-5);
+	    assert(fabs(ya[i] - _ya[mypid]) < 1e-5);
+	    assert(fabs(za[i] - _za[mypid]) < 1e-5);
+	}
+
+	sleep(1);
+    }   
+}
---
+++ cuda-dpd/main.cpp
@@ -0,0 +1,222 @@
+#include <cstdlib>
+#include <cmath>
+#include <cstdio>
+#include <cassert>
+
+#include <algorithm>
+#include <vector>
+#include <random>
+
+#include "dpd-cuda.h"
+
+using namespace std;
+
+typedef float real;
+
+void vmd_xyz(const char * path, real * xs, real * ys, real * zs, const int n, bool append)
+{
+    FILE * f = fopen(path, append ? "a" : "w");
+
+    if (f == NULL)
+    {
+	printf("I could not open the file <%s>\n", path);
+	printf("Aborting now.\n");
+	abort();
+    }
+    
+    fprintf(f, "%d\n", n);
+    fprintf(f, "mymolecule\n");
+    
+    for(int i = 0; i < n; ++i)
+	fprintf(f, "1 %f %f %f\n", xs[i], ys[i], zs[i]);
+    
+    fclose(f);
+
+    printf("vmd_xyz: wrote to <%s>\n", path);
+}
+
+int main()
+{
+    real L = 10;
+
+    const int Nm = 3;
+    const int n = L * L * L * Nm;
+    real gamma = 45;
+    real sigma = 3;
+    real aij = 2.5;
+    real rc = 1;
+    bool cuda = true;
+    
+    vector<real> xp(n), yp(n), zp(n), xv(n), yv(n), zv(n), xa(n), ya(n), za(n);
+
+    
+    for(int i = 0; i < n; ++i)
+    {
+	const int cid = i / 3;
+	
+	const int xcid = cid % (int)L;
+	const int ycid = (cid / (int) L) % (int)L;
+	const int zcid = (cid / (int) L / (int) L) % (int)L;
+	
+	xp[i] = -L * 0.5f + drand48() * L;
+	yp[i] = -L * 0.5f + drand48() * L;
+	zp[i] = -L * 0.5f + drand48() * L;
+//	xp[i] = -L * 0.5 + xcid + 0.5;//  + 0.5 * (drand48() - 0.5);
+//	yp[i] = -L * 0.5 + ycid + 0.5;//  + 0.5 * (drand48() - 0.5);
+//	zp[i] = -L * 0.5 + zcid + 0.5;//  + 0.5 * (drand48() - 0.5);
+	
+    }
+    
+    auto _diag = [&](FILE * f, float t)
+	{
+	    real sv2 = 0, xm = 0, ym = 0, zm = 0;
+	    
+	    for(int i = 0; i < n; ++i)
+	    {
+		sv2 += xv[i] * xv[i] + yv[i] * yv[i] + zv[i] * zv[i];
+		
+		xm += xv[i];
+		ym += yv[i];
+		zm += zv[i];
+	    }
+
+	    real T = 0.5 * sv2 / (n * 3. / 2);
+
+	    if (ftell(f) == 0)
+		fprintf(f, "TIME\tkBT\tX-MOMENTUM\tY-MOMENTUM\tZ-MOMENTUM\n");
+
+	    fprintf(f, "%s %+e\t%+e\t%+e\t%+e\t%+e\n", (f == stdout ? "DIAG:" : ""), t, T, xm, ym, zm);
+	};
+    
+    auto _up = [=](vector<real>& x, vector<real>& v, real f)
+	{
+	    for(int i = 0; i < n; ++i)
+		x[i] += f * v[i];
+	};
+
+    auto _up_enforce = [=](vector<real>& x, vector<real>& v, real f)
+	{
+	    for(int i = 0; i < n; ++i)
+	    {
+		x[i] += f * v[i];
+		x[i] -= L * floor(x[i] / L + 0.5);
+	    }
+	};
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<> dgauss(0, 1);
+    
+    const real dt = 0.02;
+ 
+    auto _f = [&]()
+	{
+	    if (cuda)
+	    {
+		vector<int> order(n);
+		vector<float> rsamples(n * 50);
+
+		for(auto& e : rsamples)
+		    e = dgauss(gen);
+		
+		forces_dpd_cuda(
+		    &xp.front(), &yp.front(), &zp.front(),
+		    &xv.front(), &yv.front(), &zv.front(),
+		    &xa.front(), &ya.front(), &za.front(),
+		    &order.front(), n,
+		    rc, L, L, L, aij, gamma, sigma, 1./sqrt(dt),
+		    &rsamples.front(), rsamples.size());
+		
+		return ;
+	    }
+
+	    fill(xa.begin(), xa.end(), 0);
+	    fill(ya.begin(), ya.end(), 0);
+	    fill(za.begin(), za.end(), 0);
+	    
+	    for(int i = 0; i < n; ++i)
+	    {
+		for(int j = i + 1; j < n; ++j)
+		{
+		    real xr = xp[i] - xp[j];
+		    real yr = yp[i] - yp[j];
+		    real zr = zp[i] - zp[j];
+
+		    xr -= L * floor(0.5 + xr / L);
+		    yr -= L * floor(0.5 + yr / L);
+		    zr -= L * floor(0.5 + zr / L);
+
+		    real rij = sqrt(xr * xr + yr * yr + zr * zr);
+
+		    xr /= rij;
+		    yr /= rij;
+		    zr /= rij;
+		
+		    real rc = 1; 
+		    real fc = max((real)0, aij * (1 - rij / rc));
+
+		    real wr = max((real)0, 1 - rij / rc);
+		    real wd = wr * wr;
+
+		    real rdotv = xr * (xv[i] - xv[j]) + yr * (yv[i] - yv[j]) + zr * (zv[i] - zv[j]);
+		    real gij = dgauss(gen) / sqrt(dt);
+		
+		    real xf = (fc - gamma * wd * rdotv + sigma * wr * gij) * xr;
+		    real yf = (fc - gamma * wd * rdotv + sigma * wr * gij) * yr;
+		    real zf = (fc - gamma * wd * rdotv + sigma * wr * gij) * zr;
+
+		    assert(!::isnan(xf));
+		    assert(!::isnan(yf));
+		    assert(!::isnan(zf));
+
+		    xa[i] += xf;
+		    ya[i] += yf;
+		    za[i] += zf;
+
+		    xa[j] -= xf;
+		    ya[j] -= yf;
+		    za[j] -= zf;
+		}
+	    }
+	};
+    
+    _f();
+
+    vmd_xyz("ic.xyz", &xp.front(), &yp.front(), &zp.front(), n, false);
+
+    FILE * fdiag = fopen("diag.txt", "w");
+
+    const real tend = 10;
+    const size_t nt = (int)(tend / dt);
+
+    for(int it = 0; it < nt; ++it)
+    {
+	if (it % 1 == 0)
+	{
+	    float t = it * dt;
+	    _diag(fdiag, t);
+	    _diag(stdout, t);
+	}
+		
+	_up(xv, xa, dt * 0.5);
+	_up(yv, ya, dt * 0.5);
+	_up(zv, za, dt * 0.5);
+
+	_up_enforce(xp, xv, dt);
+	_up_enforce(yp, yv, dt);
+	_up_enforce(zp, zv, dt);
+	
+	_f();
+
+	_up(xv, xa, dt * 0.5);
+	_up(yv, ya, dt * 0.5);
+	_up(zv, za, dt * 0.5);
+	
+	if (it % 30 == 0)
+	    vmd_xyz("evolution.xyz", &xp.front(), &yp.front(), &zp.front(), n, it > 0);
+    }
+
+    fclose(fdiag);
+    
+    return 0;
+}
