c7ea4135c1ec1e5dd7f735b18dd5becfd593d6ed
Diego Rossinelli <rossinelli.diego@gmail.com>
2014-07-30 15:50:05 +0200

    vanilla-sphere

--- vanilla-walls/main.cpp
+++ vanilla-walls/main.cpp
@@ -3,11 +3,8 @@
 #include <cstdio>
 #include <cassert>
 
-#include <algorithm>
 #include <vector>
-#include <random>
-
-using namespace std;
+#include <string>
 
 inline float saru(unsigned int seed1, unsigned int seed2, unsigned int seed3)
 {
@@ -36,39 +33,165 @@ inline float saru(unsigned int seed1, unsigned int seed2, unsigned int seed3)
     return res;
 }
 
+using namespace std;
+
 struct Bouncer;
 
 struct Particles
 {
     static int idglobal;
-    int n, saru_tag, myidstart;
-    const float L;
+    int n, saru_tag, myidstart, steps_per_dump = 30;
+    float L, xg = 0, yg = 0, zg = 0;
     vector<float> xp, yp, zp, xv, yv, zv, xa, ya, za;
 
-    vector<Bouncer *> bouncers;
+    Bouncer * bouncer = nullptr;
 
-    void aquire_global_id()
+    string name;
+
+    void _dpd_forces_bipartite(const float kBT, const double dt,
+			       const float * const srcxp, const float * const srcyp, const float * const srczp,
+			       const float * const srcxv, const float * const srcyv, const float * const srczv,
+			       const int nsrc,
+			       const int giddstart, const int gidsstart)
+	{
+	    const float xinvdomainsize = 1 / L;
+	    const float yinvdomainsize = 1 / L;
+	    const float zinvdomainsize = 1 / L;
+
+	    const float xdomainsize = L;
+	    const float ydomainsize = L;
+	    const float zdomainsize = L;
+
+	    const float invrc = 1.;
+	    const float gamma = 45;
+	    const float sigma = sqrt(2 * gamma * kBT);
+	    const float sigmaf = sigma / sqrt(dt);
+	    const float aij = 2.5;
+
+#pragma omp parallel for
+	    for(int i = 0; i < n; ++i)
+	    {
+		float xf = 0, yf = 0, zf = 0;
+
+		const int dpid = giddstart + i;
+	
+		for(int j = 0; j < nsrc; ++j)
+		{
+		    const int spid = gidsstart + j;
+	    		
+		    if (spid == dpid)
+			continue;
+		    
+		    const float xdiff = xp[i] - srcxp[j];
+		    const float ydiff = yp[i] - srcyp[j];
+		    const float zdiff = zp[i] - srczp[j];
+		    
+		    const float _xr = xdiff - xdomainsize * floorf(0.5f + xdiff * xinvdomainsize);
+		    const float _yr = ydiff - ydomainsize * floorf(0.5f + ydiff * yinvdomainsize);
+		    const float _zr = zdiff - zdomainsize * floorf(0.5f + zdiff * zinvdomainsize);
+		    
+		    const float rij2 = _xr * _xr + _yr * _yr + _zr * _zr;
+		    float invrij = 1./sqrtf(rij2);
+
+		    if (rij2 == 0)
+			invrij = 100000;
+	    
+		    const float rij = rij2 * invrij;
+		    const float wr = max((float)0, 1 - rij * invrc);
+		    
+		    const float xr = _xr * invrij;
+		    const float yr = _yr * invrij;
+		    const float zr = _zr * invrij;
+		
+		    const float rdotv = 
+			xr * (xv[i] - srcxv[j]) +
+			yr * (yv[i] - srcyv[j]) +
+			zr * (zv[i] - srczv[j]);
+
+		    const float mysaru = saru(min(spid, dpid), max(spid, dpid), saru_tag);
+		    const float myrandnr = 3.464101615f * mysaru - 1.732050807f;
+		 
+		    const float strength = (aij - gamma * wr * rdotv + sigmaf * myrandnr) * wr;
+
+		    xf += strength * xr;
+		    yf += strength * yr;
+		    zf += strength * zr;
+		}
+
+		xa[i] += xf;
+		ya[i] += yf;
+		za[i] += zf;
+	    }
+
+	    saru_tag++;
+	}
+    
+    void acquire_global_id()
 	{
 	    myidstart = idglobal;
 	    idglobal += n;
 	}
     
-    Particles (const int n, const float L);
+    Particles (const int n, const float L):
+	n(n), L(L), xp(n), yp(n), zp(n), xv(n), yv(n), zv(n), xa(n), ya(n), za(n), saru_tag(0)
+	{
+	    if (n > 0)
+		acquire_global_id();
+    
+	    for(int i = 0; i < n; ++i)
+	    {
+		xp[i] = -L * 0.5 + drand48() * L;
+		yp[i] = -L * 0.5 + drand48() * L;
+		zp[i] = -L * 0.5 + drand48() * L; 
+	    }
+	}
+    
+    void diag(FILE * f, float t)
+	{
+	    float sv2 = 0, xm = 0, ym = 0, zm = 0;
+	    
+	    for(int i = 0; i < n; ++i)
+	    {
+		sv2 += xv[i] * xv[i] + yv[i] * yv[i] + zv[i] * zv[i];
+		
+		xm += xv[i];
+		ym += yv[i];
+		zm += zv[i];
+	    }
 
-    tuple<float, float, float, float> diag(FILE * f, float t);
-    void _up(vector<float>& x, vector<float>& v, float f);
-    void _up_enforce(vector<float>& x, vector<float>& v, float f);
-    void _dpd_forces_bipartite(const float kBT, const double dt,
-			       const float * const srcxp, const float * const srcyp, const float * const srczp,
-			       const float * const srcxv, const float * const srcyv, const float * const srczv,
-			       const int nsrc,
-			       const int gididstart, const int gididend);
+	    float T = 0.5 * sv2 / (n * 3. / 2);
+
+	    if (ftell(f) == 0)
+		fprintf(f, "TIME\tkBT\tX-MOMENTUM\tY-MOMENTUM\tZ-MOMENTUM\n");
+
+	    fprintf(f, "%s %+e\t%+e\t%+e\t%+e\t%+e\n", (f == stdout ? "DIAG:" : ""), t, T, xm, ym, zm);
+	}
+   
+    
+    void vmd_xyz(const char * path, bool append = false)
+	{
+	    FILE * f = fopen(path, append ? "a" : "w");
+
+	    if (f == NULL)
+	    {
+		printf("I could not open the file <%s>\n", path);
+		printf("Aborting now.\n");
+		abort();
+	    }
+    
+	    fprintf(f, "%d\n", n);
+	    fprintf(f, "mymolecule\n");
+    
+	    for(int i = 0; i < n; ++i)
+		fprintf(f, "1 %f %f %f\n", xp[i], yp[i], zp[i]);
+    
+	    fclose(f);
+
+	    printf("vmd_xyz: wrote to <%s>\n", path);
+	}
+
     void _dpd_forces(const float kBT, const double dt);
     void equilibrate(const float kBT, const double tend, const double dt);
-    void vmd_xyz(const char * path, bool append = false);
-    tuple<Particles, Particles> carve_zsandwich(const float half_width);
-
-    void add_bouncer(Bouncer * b);
 };
 
 int Particles::idglobal;
@@ -76,154 +199,69 @@ int Particles::idglobal;
 struct Bouncer
 {
     Particles frozen;
+    Bouncer(const float L): frozen(0, L) {}
 
-    Bouncer(Particles frozen): frozen(frozen) {}
-    
+    virtual void _mark(bool * const freeze, Particles p) = 0;
     virtual void bounce(Particles& dest, const float dt) = 0;
+    
+    Particles carve(const Particles p)
+	{
+	    bool * const freeze = new bool[p.n];
+	    
+	    _mark(freeze, p);
+	    
+	    Particles partition[2] = {Particles(0, p.L), Particles(0, p.L)};
+	    
+	    for(int i = 0; i < p.n; ++i)
+	    {
+		const int slot = !freeze[i];
+		
+		partition[slot].xp.push_back(p.xp[i]);
+		partition[slot].yp.push_back(p.yp[i]);
+		partition[slot].zp.push_back(p.zp[i]);
+			  	       
+		partition[slot].xv.push_back(p.xv[i]);
+		partition[slot].yv.push_back(p.yv[i]);
+		partition[slot].zv.push_back(p.zv[i]);
+			  
+		partition[slot].xa.push_back(0);
+		partition[slot].ya.push_back(0);
+		partition[slot].za.push_back(0);
+			  
+		partition[slot].n++;
+	    }
+
+	    partition[0].acquire_global_id();
+	    partition[1].acquire_global_id();
+
+	    frozen = partition[0];
+	    frozen.name = "frozen";
+	    
+	    for(int i = 0; i < frozen.n; ++i)
+		frozen.xv[i] = frozen.yv[i] = frozen.zv[i] = 0;
+
+	    delete [] freeze;
+	    
+	    return partition[1];
+	}
 };
     
-Particles::Particles (const int n, const float L):
-    n(n), L(L), xp(n), yp(n), zp(n), xv(n), yv(n), zv(n), xa(n), ya(n), za(n), saru_tag(0)
-{
-    if (n > 0)
-	aquire_global_id();
-    
-    for(int i = 0; i < n; ++i)
-    {
-	xp[i] = -L * 0.5 + drand48() * L;
-	yp[i] = -L * 0.5 + drand48() * L;
-	zp[i] = -L * 0.5 + drand48() * L; 
-    }
-}
-
-tuple<float, float, float, float> Particles::diag(FILE * f, float t)
-{
-    float sv2 = 0, xm = 0, ym = 0, zm = 0;
-	    
-    for(int i = 0; i < n; ++i)
-    {
-	sv2 += xv[i] * xv[i] + yv[i] * yv[i] + zv[i] * zv[i];
-		
-	xm += xv[i];
-	ym += yv[i];
-	zm += zv[i];
-    }
-
-    float T = 0.5 * sv2 / (n * 3. / 2);
-
-    if (ftell(f) == 0)
-	fprintf(f, "TIME\tkBT\tX-MOMENTUM\tY-MOMENTUM\tZ-MOMENTUM\n");
-
-    fprintf(f, "%s %+e\t%+e\t%+e\t%+e\t%+e\n", (f == stdout ? "DIAG:" : ""), t, T, xm, ym, zm);
-
-    return make_tuple(T, xm, ym, zm);
-}
-
-void Particles::_up(vector<float>& x, vector<float>& v, float f)
-{
-    for(int i = 0; i < n; ++i)
-	x[i] += f * v[i];
-}
-
-void Particles::_up_enforce(vector<float>& x, vector<float>& v, float f)
-{
-    for(int i = 0; i < n; ++i)
-    {
-	x[i] += f * v[i];
-	x[i] -= L * floor(x[i] / L + 0.5);
-    }
-}
-
-void Particles::_dpd_forces_bipartite(const float kBT, const double dt,
-				      const float * const srcxp, const float * const srcyp, const float * const srczp,
-				      const float * const srcxv, const float * const srcyv, const float * const srczv,
-				      const int nsrc,
-				      const int giddstart, const int gidsstart)
-{
-    const float xinvdomainsize = 1 / L;
-    const float yinvdomainsize = 1 / L;
-    const float zinvdomainsize = 1 / L;
-
-    const float xdomainsize = L;
-    const float ydomainsize = L;
-    const float zdomainsize = L;
-
-    const float invrc = 1.;
-    const float gamma = 45;
-    const float sigma = sqrt(2 * gamma * kBT);
-    const float sigmaf = sigma / sqrt(dt);
-    const float aij = 2.5;
-
-#pragma omp parallel for
-    for(int i = 0; i < n; ++i)
-    {
-	float xf = 0, yf = 0, zf = 0;
-
-	const int dpid = giddstart + i;
-	
-	for(int j = 0; j < nsrc; ++j)
-	{
-	    const int spid = gidsstart + j;
-	    		
-	    if (spid == dpid)
-		continue;
-		    
-	    const float xdiff = xp[i] - srcxp[j];
-	    const float ydiff = yp[i] - srcyp[j];
-	    const float zdiff = zp[i] - srczp[j];
-		    
-	    const float _xr = xdiff - xdomainsize * floorf(0.5f + xdiff * xinvdomainsize);
-	    const float _yr = ydiff - ydomainsize * floorf(0.5f + ydiff * yinvdomainsize);
-	    const float _zr = zdiff - zdomainsize * floorf(0.5f + zdiff * zinvdomainsize);
-		    
-	    const float rij2 = _xr * _xr + _yr * _yr + _zr * _zr;
-	    float invrij = 1./sqrtf(rij2);
-
-	    if (rij2 == 0)
-		invrij = 100000;
-	    
-	    const float rij = rij2 * invrij;
-	    const float wr = max((float)0, 1 - rij * invrc);
-		    
-	    const float xr = _xr * invrij;
-	    const float yr = _yr * invrij;
-	    const float zr = _zr * invrij;
-		
-	    const float rdotv = 
-		xr * (xv[i] - srcxv[j]) +
-		yr * (yv[i] - srcyv[j]) +
-		zr * (zv[i] - srczv[j]);
-
-	    const float mysaru = saru(min(spid, dpid), max(spid, dpid), saru_tag);
-	    const float myrandnr = 3.464101615f * mysaru - 1.732050807f;
-		 
-	    const float strength = (aij - gamma * wr * rdotv + sigmaf * myrandnr) * wr;
-
-	    xf += strength * xr;
-	    yf += strength * yr;
-	    zf += strength * zr;
-	}
-
-	xa[i] += xf;
-	ya[i] += yf;
-	za[i] += zf;
-    }
-
-    saru_tag++;
-}
-
 void Particles::_dpd_forces(const float kBT, const double dt)
 {
     for(int i = 0; i < n; ++i)
-	xa[i] = ya[i] = za[i] = 0;
+    {
+	xa[i] = xg;
+	ya[i] = yg;
+	za[i] = zg;
+    }
     
     _dpd_forces_bipartite(kBT, dt,
 			  &xp.front(), &yp.front(), &zp.front(),
 			  &xv.front(), &yv.front(), &zv.front(), n, myidstart, myidstart);
 
-    for(int b = 0; b < bouncers.size(); ++b)
+    if (bouncer != nullptr)
     {
-	Particles src = bouncers[b]->frozen;
+	Particles src = bouncer->frozen;
 	
 	_dpd_forces_bipartite(kBT, dt,
 			      &src.xp.front(), &src.yp.front(), &src.zp.front(),
@@ -233,6 +271,21 @@ void Particles::_dpd_forces(const float kBT, const double dt)
 
 void Particles::equilibrate(const float kBT, const double tend, const double dt)
 {
+    auto _up  = [&](vector<float>& x, vector<float>& v, float f)
+	{
+	    for(int i = 0; i < n; ++i)
+		x[i] += f * v[i];
+	};
+
+    auto _up_enforce = [&](vector<float>& x, vector<float>& v, float f)
+	{
+	    for(int i = 0; i < n; ++i)
+	    {
+		x[i] += f * v[i];
+		x[i] -= L * floor(x[i] / L + 0.5);
+	    }
+	};
+    
     _dpd_forces(kBT, dt);
 
     vmd_xyz("ic.xyz", false);
@@ -259,8 +312,8 @@ void Particles::equilibrate(const float kBT, const double tend, const double dt)
 	_up_enforce(yp, yv, dt);
 	_up_enforce(zp, zv, dt);
 
-	for(int b = 0; b < bouncers.size(); ++b)
-	    bouncers[b]->bounce(*this, dt);
+	if (bouncer != nullptr)
+	    bouncer->bounce(*this, dt);
 	
 	_dpd_forces(kBT, dt);
 
@@ -268,76 +321,19 @@ void Particles::equilibrate(const float kBT, const double tend, const double dt)
 	_up(yv, ya, dt * 0.5);
 	_up(zv, za, dt * 0.5);
 	
-	if (it % 30 == 0)
-	    vmd_xyz("evolution.xyz", it > 0);
+	if (it % steps_per_dump == 0)
+	    vmd_xyz((name == "" ? "evolution.xyz" : (name + "-evolution.xyz")).c_str(), it > 0);
     }
 
     fclose(fdiag);
 }
-    
-void Particles::vmd_xyz(const char * path, bool append)
-{
-    FILE * f = fopen(path, append ? "a" : "w");
-
-    if (f == NULL)
-    {
-	printf("I could not open the file <%s>\n", path);
-	printf("Aborting now.\n");
-	abort();
-    }
-    
-    fprintf(f, "%d\n", n);
-    fprintf(f, "mymolecule\n");
-    
-    for(int i = 0; i < n; ++i)
-	fprintf(f, "1 %f %f %f\n", xp[i], yp[i], zp[i]);
-    
-    fclose(f);
-
-    printf("vmd_xyz: wrote to <%s>\n", path);
-}
-    
-tuple<Particles, Particles> Particles::carve_zsandwich(const float half_width)
-{
-    Particles partition[2] = {Particles(0, L), Particles(0, L)};
-	    
-    for(int i = 0; i < n; ++i)
-    {
-	const bool inside = fabs(zp[i]) <= half_width;
-
-	partition[inside].xp.push_back(xp[i]);
-	partition[inside].yp.push_back(yp[i]);
-	partition[inside].zp.push_back(zp[i]);
-
-	partition[inside].xv.push_back(xv[i]);
-	partition[inside].yv.push_back(yv[i]);
-	partition[inside].zv.push_back(zv[i]);
-
-	partition[inside].xa.push_back(0);
-	partition[inside].ya.push_back(0);
-	partition[inside].za.push_back(0);
-		
-	partition[inside].n++;
-    }
-
-    partition[0].aquire_global_id();
-    partition[1].aquire_global_id();
-    
-    return make_tuple(partition[0], partition[1]);
-}
-
-void Particles::add_bouncer(Bouncer* bouncer) { bouncers.push_back(bouncer); }
 
 struct SandwichBouncer: Bouncer
 {
-    float half_width;
+    float half_width = 1;
     
-    SandwichBouncer(Particles& frozen, const float half_width):
-	Bouncer(frozen), half_width(half_width)
-	{
-	    for(int i = 0; i < frozen.n; ++i)
-		frozen.xv[i] = frozen.yv[i] = frozen.zv[i] = 0;
-	}
+    SandwichBouncer( const float L):
+	Bouncer(L) { }
     
     void bounce(Particles& dest, const float dt)
 	{
@@ -352,20 +348,16 @@ struct SandwichBouncer: Bouncer
 		
 		if (fabs(z) - half_width > 0)
 		{
-		    printf("particle %d shoud bounce back!\n", i);
 		    const float xold = x - dt * u;
 		    const float yold = y - dt * v;
 		    const float zold = z - dt * w;
 
-		    printf("old z %e old w %e -> colliding z %e\n", zold, w, dest.zp[i]);
-		    printf("zold was inside? %e\n", fabs(zold) - half_width);
 		    assert(fabs(zold) - half_width <= 0);
 		    assert(fabs(w) > 0);
 
 		    const float s = 1 - 2 * signbit(w);
 		    const float t = (s * half_width - zold) / w;
 
-		    printf("old z %f old w %f -> t %f\n", zold, w, t);
 		    assert(t >= 0);
 		    assert(t <= dt);
 		    
@@ -374,7 +366,6 @@ struct SandwichBouncer: Bouncer
 		    dest.xp[i] = xold + lambda * u;
 		    dest.yp[i] = yold + lambda * v;
 		    dest.zp[i] = zold + lambda * w;
-		    printf("old z %e old w %e -> t %e -> new z %e\n", zold, w, t, dest.zp[i]);
 		    assert(fabs(zold + lambda * w) - half_width <= 0);
 
 		    dest.xv[i] = -u;
@@ -390,8 +381,121 @@ struct SandwichBouncer: Bouncer
 		}
 	    }
 
-	    printf("bounce done\n");
+	    // printf("bounce done\n");
 	}
+
+    void _mark(bool * const freeze, Particles p)
+	{
+	    for(int i = 0; i < p.n; ++i)
+		freeze[i] = !(fabs(p.zp[i]) <= half_width);
+	}
+};
+
+struct TomatoSandwich: SandwichBouncer
+{
+    float xc = 0, yc = 0, zc = 0;
+    float radius = 1;
+
+    TomatoSandwich(const float L): SandwichBouncer(L)
+	{
+	}
+
+     void _mark(bool * const freeze, Particles p)
+	{
+	    SandwichBouncer::_mark(freeze, p);
+//	    return;
+	    const float radius2 = radius * radius;
+	    for(int i = 0; i < p.n; ++i)
+	    {
+		const float x = p.xp[i] - xc;
+		const float y = p.yp[i] - yc;
+		const float z = p.zp[i] - zc;
+
+		const float r2 = x * x + y * y + z * z;
+
+		freeze[i] |= r2 < radius2;
+	    }
+	}
+
+    float _qsolve(const float a, const float b, const float c)
+	{
+	    const float d = sqrt(b * b - 4 * a * c);
+
+	    const float x0 = (-b - d) / (2 * a);
+	    const float x1 = (-b + d) / (2 * a);
+
+	    assert(x0 < x1);
+	    return x0;
+	}
+
+    float _collision_time(const float _x0, const float _y0, const float _z0,
+			   const float u, const float v, const float w,
+			   const float xc, const float yc, const float zc, const float r2)
+	{
+	    const float x0 = _x0 - xc;
+	    const float y0 = _y0 - yc;
+	    const float z0 = _z0 - zc;
+	    
+	    const float c = x0 * x0 + y0 * y0 + z0 * z0 - r2;
+	    const float b = 2 * (x0 * u + y0 * v + z0 * w);
+	    const float a = u * u + v * v + w * w;
+
+	    return _qsolve(a, b, c);
+	}
+
+    
+
+    void bounce(Particles& dest, const float dt)
+	{
+	    SandwichBouncer::bounce(dest, dt);
+
+	    const float radius2 = radius * radius;
+	    
+	    for(int i = 0; i < dest.n; ++i)
+	    {
+		const float x = dest.xp[i] - xc;
+		const float y = dest.yp[i] - yc;
+		const float z = dest.zp[i] - zc;
+		const float u = dest.xv[i];
+		const float v = dest.yv[i];
+		const float w = dest.zv[i];
+
+		const float r2 = x * x + y * y + z * z;
+		
+		if (r2 < radius2)
+		{
+		    const float xold = x - dt * u;
+		    const float yold = y - dt * v;
+		    const float zold = z - dt * w;
+
+		   
+		    const float t = _collision_time(xold, yold, zold, u, v, w, xc, yc, zc, radius2);
+		    printf("collision time %f -> xold %f %f %f v %f %f %f  xc %f %f %f r2 %f\n", t,
+			xold, yold, zold, u, v, w, xc, yc, zc, radius2);
+		    assert(t >= 0);
+		    assert(t <= dt);
+		    
+		    const float lambda = 2 * t - dt;
+		    
+		    dest.xp[i] = xold + lambda * u;
+		    dest.yp[i] = yold + lambda * v;
+		    dest.zp[i] = zold + lambda * w;
+		    assert(fabs(zold + lambda * w) - half_width <= 0);
+
+		    dest.xv[i] = -u;
+		    dest.yv[i] = -v;
+		    dest.zv[i] = -w;
+
+		    assert(!isnan(dest.xp[i]));
+		    assert(!isnan(dest.yp[i]));
+		    assert(!isnan(dest.zp[i]));
+		    assert(!isnan(dest.xv[i]));
+		    assert(!isnan(dest.yv[i]));
+		    assert(!isnan(dest.zv[i]));
+		}
+	    }
+	}
+    
 };
 
 int main()
@@ -404,14 +508,22 @@ int main()
     particles.equilibrate(.1, 5, 0.02);
 
     const float sandwich_half_width = L / 2 - 1.7;
-    auto parts = particles.carve_zsandwich(sandwich_half_width);
-    get<0>(parts).vmd_xyz("frozen.xyz");
-    get<1>(parts).vmd_xyz("fluid.xyz");
-
-    SandwichBouncer bouncer(get<0>(parts), sandwich_half_width);
-    Particles remaining(get<1>(parts));
-    remaining.add_bouncer(&bouncer);
-    remaining.equilibrate(.1, 10, 0.02);
+#if 1
+    TomatoSandwich bouncer(L);
+    bouncer.radius = 2;
+    bouncer.half_width = sandwich_half_width;
+#else
+    SandwichBouncer bouncer(L);
+    bouncer.half_width = sandwich_half_width;
+#endif
+    Particles remaining = bouncer.carve(particles);
+    bouncer.frozen.vmd_xyz("icy.xyz");
+    remaining.name = "fluid";
+    
+    remaining.bouncer = &bouncer;
+    remaining.xg = 0.05;
+    remaining.steps_per_dump = 3;
+    remaining.equilibrate(.1, 100, 0.02);
     printf("particles have been equilibrated");
 }
 
