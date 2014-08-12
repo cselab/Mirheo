a495dfb08bf17065ce7934cb54a5a8153d9c81e5
kirilllykov <lykov.kirill@gmail.com>
2014-08-12 12:34:27 +0200

    Added missed files to the previous commit

---
+++ vanilla-walls/particles.cpp
@@ -0,0 +1,301 @@
+/*
+ * Particles.cpp
+ *
+ *  Created on: Aug 12, 2014
+ *      Author: kirill
+ */
+#include "particles.h"
+
+#include <cstdlib>
+#include <cmath>
+#include <cstdio>
+#include <cassert>
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#ifdef USE_CUDA
+#include "cuda-dpd.h"
+#endif
+#include "funnel-obstacle.h"
+#include "funnel-bouncer.h"
+
+using namespace std;
+
+void Particles::_dpd_forces_bipartite(const float kBT, const double dt,
+               const float * const srcxp, const float * const srcyp, const float * const srczp,
+               const float * const srcxv, const float * const srcyv, const float * const srczv,
+               const int nsrc,
+               const int giddstart, const int gidsstart)
+{
+    const float xinvdomainsize = 1 / L;
+    const float yinvdomainsize = 1 / L;
+    const float zinvdomainsize = 1 / L;
+
+    const float xdomainsize = L;
+    const float ydomainsize = L;
+    const float zdomainsize = L;
+
+    const float invrc = 1.;
+    const float gamma = 45;
+    const float sigma = sqrt(2 * gamma * kBT);
+    const float sigmaf = sigma / sqrt(dt);
+    const float aij = 2.5;
+
+#ifdef USE_CUDA
+    if(srcxp == &xp.front())
+    forces_dpd_cuda(&xp.front(), &yp.front(), &zp.front(),
+            &xv.front(), &yv.front(), &zv.front(),
+            &xa.front(), &ya.front(), &za.front(),
+            n,
+            1, xdomainsize, ydomainsize, zdomainsize,
+            aij,  gamma,  sigma,  1 / sqrt(dt));
+    else
+    forces_dpd_cuda_bipartite(&xp.front(), &yp.front(), &zp.front(),
+                  &xv.front(), &yv.front(), &zv.front(),
+                  &xa.front(), &ya.front(), &za.front(),
+                  n, giddstart,
+                  srcxp,  srcyp,  srczp,
+                  srcxv,  srcyv,  srczv,
+                  NULL, NULL, NULL,
+                  nsrc, gidsstart,
+                  1, xdomainsize, ydomainsize, zdomainsize,
+                  aij,  gamma,  sigma,  1 / sqrt(dt));
+#else
+
+#pragma omp parallel for
+    for(int i = 0; i < n; ++i)
+    {
+    float xf = 0, yf = 0, zf = 0;
+
+    const int dpid = giddstart + i;
+
+    for(int j = 0; j < nsrc; ++j)
+    {
+        const int spid = gidsstart + j;
+
+        if (spid == dpid)
+        continue;
+
+        const float xdiff = xp[i] - srcxp[j];
+        const float ydiff = yp[i] - srcyp[j];
+        const float zdiff = zp[i] - srczp[j];
+
+        const float _xr = xdiff - xdomainsize * floorf(0.5f + xdiff * xinvdomainsize);
+        const float _yr = ydiff - ydomainsize * floorf(0.5f + ydiff * yinvdomainsize);
+        const float _zr = zdiff - zdomainsize * floorf(0.5f + zdiff * zinvdomainsize);
+
+        const float rij2 = _xr * _xr + _yr * _yr + _zr * _zr;
+        float invrij = 1./sqrtf(rij2);
+
+        if (rij2 == 0)
+        invrij = 100000;
+
+        const float rij = rij2 * invrij;
+        const float wr = max((float)0, 1 - rij * invrc);
+
+        const float xr = _xr * invrij;
+        const float yr = _yr * invrij;
+        const float zr = _zr * invrij;
+
+        const float rdotv =
+        xr * (xv[i] - srcxv[j]) +
+        yr * (yv[i] - srcyv[j]) +
+        zr * (zv[i] - srczv[j]);
+
+        const float mysaru = saru(min(spid, dpid), max(spid, dpid), saru_tag);
+        const float myrandnr = 3.464101615f * mysaru - 1.732050807f;
+
+        const float strength = (aij - gamma * wr * rdotv + sigmaf * myrandnr) * wr;
+
+        xf += strength * xr;
+        yf += strength * yr;
+        zf += strength * zr;
+    }
+
+    xa[i] += xf;
+    ya[i] += yf;
+    za[i] += zf;
+    }
+
+    saru_tag++;
+#endif
+}
+
+void Particles::acquire_global_id()
+{
+    myidstart = idglobal;
+    idglobal += n;
+}
+
+Particles::Particles (const int n, const float L):
+n(n), L(L), xp(n), yp(n), zp(n), xv(n), yv(n), zv(n), xa(n), ya(n), za(n), saru_tag(0)
+{
+    if (n > 0)
+    acquire_global_id();
+
+    for(int i = 0; i < n; ++i)
+    {
+    xp[i] = -L * 0.5 + drand48() * L;
+    yp[i] = -L * 0.5 + drand48() * L;
+    zp[i] = -L * 0.5 + drand48() * L;
+    }
+}
+
+void Particles::diag(FILE * f, float t)
+{
+    float sv2 = 0, xm = 0, ym = 0, zm = 0;
+
+    for(int i = 0; i < n; ++i)
+    {
+    sv2 += xv[i] * xv[i] + yv[i] * yv[i] + zv[i] * zv[i];
+
+    xm += xv[i];
+    ym += yv[i];
+    zm += zv[i];
+    }
+
+    float T = 0.5 * sv2 / (n * 3. / 2);
+
+    if (ftell(f) == 0)
+    fprintf(f, "TIME\tkBT\tX-MOMENTUM\tY-MOMENTUM\tZ-MOMENTUM\n");
+
+    fprintf(f, "%s %+e\t%+e\t%+e\t%+e\t%+e\n", (f == stdout ? "DIAG:" : ""), t, T, xm, ym, zm);
+}
+
+void Particles::vmd_xyz(const char * path, bool append)
+{
+    FILE * f = fopen(path, append ? "a" : "w");
+
+    if (f == NULL)
+    {
+    printf("I could not open the file <%s>\n", path);
+    printf("Aborting now.\n");
+    abort();
+    }
+
+    fprintf(f, "%d\n", n);
+    fprintf(f, "mymolecule\n");
+
+    for(int i = 0; i < n; ++i)
+    fprintf(f, "1 %f %f %f\n", xp[i], yp[i], zp[i]);
+
+    fclose(f);
+
+    printf("vmd_xyz: wrote to <%s>\n", path);
+}
+
+// might be opened by OVITO and xmovie (xwindow-based utility)
+void Particles::lammps_dump(const char* path, size_t timestep)
+{
+  bool append = timestep > 0;
+  FILE * f = fopen(path, append ? "a" : "w");
+
+  float boxLength = L;
+
+  if (f == NULL)
+  {
+  printf("I could not open the file <%s>\n", path);
+  printf("Aborting now.\n");
+  abort();
+  }
+
+  // header
+  fprintf(f, "ITEM: TIMESTEP\n%lu\n", timestep);
+  fprintf(f, "ITEM: NUMBER OF ATOMS\n%d\n", n);
+  fprintf(f, "ITEM: BOX BOUNDS pp pp pp\n%g %g\n%g %g\n%g %g\n",
+      -boxLength/2.0, boxLength/2.0, -boxLength/2.0, boxLength/2.0, -boxLength/2.0, boxLength/2.0);
+
+  fprintf(f, "ITEM: ATOMS id type xs ys zs\n");
+
+  // positions <ID> <type> <x> <y> <z>
+  // free particles have type 2, while rings 1
+  size_t type = 1; //skip for now
+  for (size_t i = 0; i < n; ++i) {
+    fprintf(f, "%lu %lu %g %g %g\n", i, type, xp[i], yp[i], zp[i]);
+  }
+
+  fclose(f);
+}
+
+int Particles::idglobal;
+
+void Particles::_dpd_forces(const float kBT, const double dt)
+{
+    for(int i = 0; i < n; ++i)
+    {
+    xa[i] = xg;
+    ya[i] = yg;
+    za[i] = zg;
+    }
+
+    _dpd_forces_bipartite(kBT, dt,
+              &xp.front(), &yp.front(), &zp.front(),
+              &xv.front(), &yv.front(), &zv.front(), n, myidstart, myidstart);
+}
+
+void Particles::equilibrate(const float kBT, const double tend, const double dt, const Bouncer* bouncer)
+{
+    auto _up  = [&](vector<float>& x, vector<float>& v, float f)
+    {
+        for(int i = 0; i < n; ++i)
+        x[i] += f * v[i];
+    };
+
+    auto _up_enforce = [&](vector<float>& x, vector<float>& v, float f)
+    {
+        for(int i = 0; i < n; ++i)
+        {
+        x[i] += f * v[i];
+        x[i] -= L * floor(x[i] / L + 0.5);
+        }
+    };
+
+    _dpd_forces(kBT, dt);
+
+    vmd_xyz("ic.xyz", false);
+
+    FILE * fdiag = fopen("diag-equilibrate.txt", "w");
+
+    const size_t nt = (int)(tend / dt);
+
+    for(int it = 0; it < nt; ++it)
+    {
+    if (it % steps_per_dump == 0)
+    {
+        printf("step %d\n", it);
+        float t = it * dt;
+        diag(fdiag, t);
+        diag(stdout, t);
+    }
+
+    _up(xv, xa, dt * 0.5);
+    _up(yv, ya, dt * 0.5);
+    _up(zv, za, dt * 0.5);
+
+    _up_enforce(xp, xv, dt);
+    _up_enforce(yp, yv, dt);
+    _up_enforce(zp, zv, dt);
+
+    if (bouncer != nullptr)
+        bouncer->bounce(*this, dt);
+
+    _dpd_forces(kBT, dt);
+
+    if (bouncer != nullptr)
+        bouncer->compute_forces(kBT, dt, *this);
+
+    _up(xv, xa, dt * 0.5);
+    _up(yv, ya, dt * 0.5);
+    _up(zv, za, dt * 0.5);
+
+    if (it % steps_per_dump == 0)
+        lammps_dump("evolution.dump", it);
+        //vmd_xyz((name == "" ? "evolution.xyz" : (name + "-evolution.xyz")).c_str(), it > 0);
+    }
+
+    fclose(fdiag);
+}
+
+
---
+++ vanilla-walls/particles.h
@@ -0,0 +1,77 @@
+/*
+ * Particles.h
+ *
+ *  Created on: Aug 12, 2014
+ *      Author: kirill
+ */
+#pragma once
+
+#include <cmath>
+
+#include <vector>
+#include <string>
+
+#include "funnel-obstacle.h"
+
+inline float saru(unsigned int seed1, unsigned int seed2, unsigned int seed3)
+{
+    seed3 ^= (seed1<<7)^(seed2>>6);
+    seed2 += (seed1>>4)^(seed3>>15);
+    seed1 ^= (seed2<<9)+(seed3<<8);
+    seed3 ^= 0xA5366B4D*((seed2>>11) ^ (seed1<<1));
+    seed2 += 0x72BE1579*((seed1<<4)  ^ (seed3>>16));
+    seed1 ^= 0X3F38A6ED*((seed3>>5)  ^ (((signed int)seed2)>>22));
+    seed2 += seed1*seed3;
+    seed1 += seed3 ^ (seed2>>2);
+    seed2 ^= ((signed int)seed2)>>17;
+
+    int state  = 0x79dedea3*(seed1^(((signed int)seed1)>>14));
+    int wstate = (state + seed2) ^ (((signed int)state)>>8);
+    state  = state + (wstate*(wstate^0xdddf97f5));
+    wstate = 0xABCB96F7 + (wstate>>1);
+
+    state  = 0x4beb5d59*state + 0x2600e1f7; // LCG
+    wstate = wstate + 0x8009d14b + ((((signed int)wstate)>>31)&0xda879add); // OWS
+
+    unsigned int v = (state ^ (state>>26))+wstate;
+    unsigned int r = (v^(v>>20))*0x6957f5a7;
+
+    float res = r / (4294967295.0f);
+    return res;
+}
+
+using namespace std;
+
+struct Bouncer;
+
+struct Particles
+{
+    static int idglobal;
+
+    int n, myidstart, steps_per_dump = 100;
+    mutable int saru_tag;
+    float L, xg = 0, yg = 0, zg = 0;
+    vector<float> xp, yp, zp, xv, yv, zv, xa, ya, za;
+    //Bouncer * bouncer = nullptr;
+    string name;
+
+    void _dpd_forces_bipartite(const float kBT, const double dt,
+                   const float * const srcxp, const float * const srcyp, const float * const srczp,
+                   const float * const srcxv, const float * const srcyv, const float * const srczv,
+                   const int nsrc,
+                   const int giddstart, const int gidsstart);
+
+    void acquire_global_id();
+
+    Particles (const int n, const float L);
+
+    void diag(FILE * f, float t);
+
+    void vmd_xyz(const char * path, bool append = false);
+
+    // might be opened by OVITO and xmovie (xwindow-based utility)
+    void lammps_dump(const char* path, size_t timestep);
+
+    void _dpd_forces(const float kBT, const double dt);
+    void equilibrate(const float kBT, const double tend, const double dt, const Bouncer* bouncer);
+};
