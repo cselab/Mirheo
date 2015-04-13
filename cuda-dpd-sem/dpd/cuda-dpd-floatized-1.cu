480908af650a2eb0188f2f73d344b09078925ca3
Yu-Hang Tang <Yuhang_Tang@brown.edu>
2015-04-12 23:55:22 -0400

    fixed bug: CUDA5.5 does not handle asm with shared memory access well even with 'memory' clobber, need also 'asm volatile'

--- cuda-dpd-sem/dpd/cuda-dpd-floatized-1.cu
+++ cuda-dpd-sem/dpd/cuda-dpd-floatized-1.cu
@@ -97,8 +97,8 @@ __inline__ __device__ uint2 __unpack_8_24( uint d )
     return make_uint2( a, d & 0x00FFFFFFU );
 }
 
-__device__ char4 tid2ind[32] =
-{   { -1, -1, -1, 0}, {0, -1, -1, 0}, {1, -1, -1, 0},
+__device__ char4 tid2ind[32] = {
+    { -1, -1, -1, 0}, {0, -1, -1, 0}, {1, -1, -1, 0},
     { -1,  0, -1, 0}, {0,  0, -1, 0}, {1,  0, -1, 0},
     { -1 , 1, -1, 0}, {0,  1, -1, 0}, {1,  1, -1, 0},
     { -1, -1,  0, 0}, {0, -1,  0, 0}, {1, -1,  0, 0},
@@ -117,7 +117,7 @@ __forceinline__ __device__ void core_ytang( const uint dststart, const uint psha
 {
     uint item;
     const uint offset = xmad( tid, 4.f, pshare );
-    asm( "ld.volatile.shared.u32 %0, [%1+1024];" : "=r"( item ) : "r"( offset ) : "memory" );
+    asm volatile( "ld.volatile.shared.u32 %0, [%1+1024];" : "=r"( item ) : "r"( offset ) : "memory" );
     const uint2 pid = __unpack_8_24( item );
     const uint dpid = xadd( dststart, pid.x );
     const uint spid = pid.y;
@@ -134,7 +134,7 @@ __forceinline__ __device__ void core_ytang( const uint dststart, const uint psha
     // can be completely killed by changing the integration kernel
 #ifdef TRANSPOSED_ATOMICS
     uint off  = dpid & 0x0000001FU;
-    uint base = xdiv( dpid, 1/32.f);
+    uint base = xdiv( dpid, 1 / 32.f );
     float* acc = info.axayaz + xmad( base, 96.f, off );
     atomicAdd( acc   , f.x );
     atomicAdd( acc + 32, f.y );
@@ -142,7 +142,7 @@ __forceinline__ __device__ void core_ytang( const uint dststart, const uint psha
 
     if( spid < spidext ) {
         uint off  = spid & 0x0000001FU;
-        uint base = xdiv( spid, 1/32.f );
+        uint base = xdiv( spid, 1 / 32.f );
         float* acc = info.axayaz + xmad( base, 96.f, off );
         atomicAdd( acc   , -f.x );
         atomicAdd( acc + 32, -f.y );
@@ -169,9 +169,10 @@ __forceinline__ __device__ void core_ytang( const uint dststart, const uint psha
 #define MYWPB   (4)
 
 __global__  __launch_bounds__( 32 * MYWPB, 16 )
-void _dpd_forces_symm_merged() {
+void _dpd_forces_symm_merged()
+{
 
-	asm volatile( ".shared .u32 smem[512];" ::: "memory" );
+    asm volatile( ".shared .u32 smem[512];" ::: "memory" );
     //* was: __shared__ uint2 volatile start_n_scan[MYWPB][32];
     //* was: __shared__ uint  volatile queue[MYWPB][64];
 
@@ -189,21 +190,21 @@ void _dpd_forces_symm_merged() {
 
     for( uint it = 0; it < 4 ; it = xadd( it, 1u ) ) {
 
-    	//* was: const int cid = cbase + ( it < 2u ) * info.ncells.x * info.ncells.y +
-    	//* was:                 ( ( it & 1u ) ^ ( it >> 1 ) ) * info.ncells.x;
-    	int cid;
-        asm("{  .reg .pred    p;"
-        	"   .reg .f32     incf;"
-        	"   .reg .s32     inc;"
-        	"    setp.lt.f32  p, %2, %3;"
-        	"    selp.f32     incf, %4, 0.0, p;"
-        	"    add.f32      incf, incf, %5;"
-        	"    mov.b32      inc, incf;"
-        	"    mul.lo.u32   inc, inc, %6;"
-        	"    add.s32 %0,  %1, inc;"
-        	"}" :
-        	"=r"(cid) : "r"(cbase), "f"(u2f(it)), "f"(u2f(2u)), "f"(i2f(info.ncells.y)), "f"( u2f( ( it & 1u ) ^ ( it >> 1 ) ) ),
-        	"r"(info.ncells.x) );
+        //* was: const int cid = cbase + ( it < 2u ) * info.ncells.x * info.ncells.y +
+        //* was:                 ( ( it & 1u ) ^ ( it >> 1 ) ) * info.ncells.x;
+        int cid;
+        asm( "{  .reg .pred    p;"
+             "   .reg .f32     incf;"
+             "   .reg .s32     inc;"
+             "    setp.lt.f32  p, %2, %3;"
+             "    selp.f32     incf, %4, 0.0, p;"
+             "    add.f32      incf, incf, %5;"
+             "    mov.b32      inc, incf;"
+             "    mul.lo.u32   inc, inc, %6;"
+             "    add.s32 %0,  %1, inc;"
+             "}" :
+             "=r"( cid ) : "r"( cbase ), "f"( u2f( it ) ), "f"( u2f( 2u ) ), "f"( i2f( info.ncells.y ) ), "f"( u2f( ( it & 1u ) ^ ( it >> 1 ) ) ),
+             "r"( info.ncells.x ) );
 
         //* was: uint mycount=0, myscan=0;
         //* was: if (tid < 14) {
@@ -232,10 +233,10 @@ void _dpd_forces_symm_merged() {
              "f"( u2f( tid ) ), "f"( u2f( 14u ) ), "r"( cid ), "f"( i2f( cid ) ),
              "r"( info.nxyz ) );
         myscan  = mycount;
-        asm( "st.volatile.shared.u32 [%0], %1;" ::
-             "r"( xmad( tid, 8.f, pshare ) ),
-             "r"( mystart ) :
-             "memory" );
+        asm volatile( "st.volatile.shared.u32 [%0], %1;" ::
+                      "r"( xmad( tid, 8.f, pshare ) ),
+                      "r"( mystart ) :
+                      "memory" );
 
         // was: #pragma unroll
         // was: for(int L = 1; L < 32; L <<= 1) {
@@ -257,27 +258,30 @@ void _dpd_forces_symm_merged() {
              "   mov.b32     %0, myscan;"
              "}" : "+r"( myscan ) );
 
+
         //* was: if (tid < 15) start_n_scan[wid][tid].y = myscan - mycount;
-        asm( "{    .reg .pred lt15;"
-             "      setp.lt.f32 lt15, %0, %1;"
-             "@lt15 st.volatile.shared.u32 [%2+4], %3;"
-             "}":: "f"( u2f( tid ) ), "f"( u2f( 15u ) ), "r"( xmad( tid, 8.f, pshare ) ), "r"( xsub( myscan, mycount ) ) : "memory" );
+        asm volatile( "{    .reg .pred lt15;"
+                      "      setp.lt.f32 lt15, %0, %1;"
+                      "@lt15 st.volatile.shared.u32 [%2+4], %3;"
+                      "}":: "f"( u2f( tid ) ), "f"( u2f( 15u ) ), "r"( xmad( tid, 8.f, pshare ) ), "r"( xsub( myscan, mycount ) ) : "memory" );
 
         //* was: const uint dststart = start_n_scan[wid][13].x;
         //* was: const uint lastdst  = xsub( xadd( dststart, start_n_scan[wid][14].y ), start_n_scan[wid][13].y );
         //* was: const uint nsrc     = start_n_scan[wid][14].y;
         //* was: const uint spidext  = start_n_scan[wid][13].x;
         uint x13, y13, y14; // TODO: LDS.128
-        asm( "ld.volatile.shared.v2.u32 {%0,%1}, [%3+104];" // 104 = 13 x 8-byte uint2
-             "ld.volatile.shared.u32     %2,     [%3+116];" // 116 = 14 x 8-bute uint2 + .y
-             : "=r"( x13 ), "=r"( y13 ), "=r"( y14 ) : "r"( pshare ) : "memory" );
+        asm volatile( "ld.volatile.shared.v2.u32 {%0,%1}, [%3+104];" // 104 = 13 x 8-byte uint2
+                      "ld.volatile.shared.u32     %2,     [%3+116];" // 116 = 14 x 8-bute uint2 + .y
+                      : "=r"( x13 ), "=r"( y13 ), "=r"( y14 ) : "r"( pshare ) : "memory" );
         const uint dststart = x13;
         const uint lastdst  = xsub( xadd( dststart, y14 ), y13 );
         const uint nsrc     = y14;
         const uint spidext  = x13;
 
         uint nb = 0;
+
         for( uint p = 0; p < nsrc; p = xadd( p, 32u ) ) {
+
             const uint pid = p + tid;
 
             //* was: const uint key9 = 9*(pid >= start_n_scan[wid][9].y);
@@ -285,40 +289,40 @@ void _dpd_forces_symm_merged() {
             //* was: key3 += (key9 < 9) ? 3*(pid >= start_n_scan[wid][key9 + 6].y) : 0;
             //* was: const uint spid = pid - start_n_scan[wid][key3+key9].y + start_n_scan[wid][key3+key9].x;
             uint spid;
-            asm( "{ .reg .pred p, q, r;" // TODO: HOW TO USE LDS.128
-                 "  .reg .f32  key;"
-                 "  .reg .f32  scan3, scan6, scan9;"
-                 "  .reg .f32  mystart, myscan;"
-                 "  .reg .s32  array;"
-                 "  .reg .f32  array_f;"
-                 "   mov.b32           array, %4;"
-                 "   ld.shared.f32     scan9,  [array +  9*8 + 4];"
-                 "   setp.ge.f32       p, %1, scan9;"
-                 "   selp.f32          key, %2, 0.0, p;"
-                 "   mov.b32           array_f, array;"
-                 "   fma.f32.rm        array_f, key, 8.0, array_f;"
-                 "   mov.b32 array,    array_f;"
-                 "   ld.shared.f32     scan3, [array + 3*8 + 4];"
-                 "   setp.ge.f32       p, %1, scan3;"
-                 "@p add.f32           key, key, %3;"
-                 "   setp.lt.f32       p, key, %2;"
-                 "   setp.lt.and.f32   p, %5, %6, p;"
-                 "   ld.shared.f32     scan6, [array + 6*8 + 4];"
-                 "   setp.ge.and.f32   q, %1, scan6, p;"
-                 "@q add.f32           key, key, %3;"
-                 "   fma.f32.rm        array_f, key, 8.0, %4;"
-                 "   mov.b32           array, array_f;"
-                 "   ld.shared.v2.f32 {mystart, myscan}, [array];"
-                 "   add.f32           mystart, mystart, %1;"
-                 "   sub.f32           mystart, mystart, myscan;"
-                 "   mov.b32           %0, mystart;"
-                 "}" : "=r"( spid ) : "f"( u2f( pid ) ), "f"( u2f( 9u ) ), "f"( u2f( 3u ) ), "f"( u2f( pshare ) ), "f"( u2f( pid ) ), "f"( u2f( nsrc ) ) );
+            asm volatile( "{ .reg .pred p, q, r;" // TODO: HOW TO USE LDS.128
+                          "  .reg .f32  key;"
+                          "  .reg .f32  scan3, scan6, scan9;"
+                          "  .reg .f32  mystart, myscan;"
+                          "  .reg .s32  array;"
+                          "  .reg .f32  array_f;"
+                          "   mov.b32           array, %4;"
+                          "   ld.shared.f32     scan9,  [array +  9*8 + 4];"
+                          "   setp.ge.f32       p, %1, scan9;"
+                          "   selp.f32          key, %2, 0.0, p;"
+                          "   mov.b32           array_f, array;"
+                          "   fma.f32.rm        array_f, key, 8.0, array_f;"
+                          "   mov.b32 array,    array_f;"
+                          "   ld.shared.f32     scan3, [array + 3*8 + 4];"
+                          "   setp.ge.f32       p, %1, scan3;"
+                          "@p add.f32           key, key, %3;"
+                          "   setp.lt.f32       p, key, %2;"
+                          "   setp.lt.and.f32   p, %5, %6, p;"
+                          "   ld.shared.f32     scan6, [array + 6*8 + 4];"
+                          "   setp.ge.and.f32   q, %1, scan6, p;"
+                          "@q add.f32           key, key, %3;"
+                          "   fma.f32.rm        array_f, key, 8.0, %4;"
+                          "   mov.b32           array, array_f;"
+                          "   ld.shared.v2.f32 {mystart, myscan}, [array];"
+                          "   add.f32           mystart, mystart, %1;"
+                          "   sub.f32           mystart, mystart, myscan;"
+                          "   mov.b32           %0, mystart;"
+                          "}" : "=r"( spid ) : "f"( u2f( pid ) ), "f"( u2f( 9u ) ), "f"( u2f( 3u ) ), "f"( u2f( pshare ) ), "f"( u2f( pid ) ), "f"( u2f( nsrc ) ) );
 
             const float4 xsrc = tex1Dfetch( texParticlesH4, xmin( spid, lastdst ) );
 
             for( uint dpid = dststart; dpid < lastdst; dpid = xadd( dpid, 1u ) ) {
 
-            	const float4 xdest = tex1Dfetch( texParticlesH4, dpid );
+                const float4 xdest = tex1Dfetch( texParticlesH4, dpid );
                 const float dx = xdest.x - xsrc.x;
                 const float dy = xdest.y - xsrc.y;
                 const float dz = xdest.z - xsrc.z;
@@ -336,10 +340,10 @@ void _dpd_forces_symm_merged() {
                 const uint insert = xadd( nb, i2u( __popc( overview & __lanemask_lt() ) ) );
 
                 //* was: if (interacting) queue[wid][insert] = __pack_8_24( xsub(dpid,dststart), spid );
-                asm( "@interacting st.volatile.shared.u32 [%0+1024], %1;" : :
-                     "r"( xmad( insert, 4.f, pshare ) ),
-                     "r"( __pack_8_24( xsub( dpid, dststart ), spid ) ) :
-                     "memory" );
+                asm volatile( "@interacting st.volatile.shared.u32 [%0+1024], %1;" : :
+                              "r"( xmad( insert, 4.f, pshare ) ),
+                              "r"( __pack_8_24( xsub( dpid, dststart ), spid ) ) :
+                              "memory" );
 
                 nb = xadd( nb, i2u( __popc( overview ) ) );
                 if( nb >= 32u ) {
@@ -347,10 +351,10 @@ void _dpd_forces_symm_merged() {
                     nb = xsub( nb, 32u );
 
                     //* was: queue[tid] = queue[tid+32];
-                    asm( "{ .reg .u32 tmp;"
-                         "   ld.volatile.shared.u32 tmp, [%0+1024+128];"
-                         "   st.volatile.shared.u32 [%0+1024], tmp;"
-                         "}" :: "r"( xmad( tid, 4.f, pshare ) ) : "memory" );
+                    asm volatile( "{ .reg .u32 tmp;"
+                                  "   ld.volatile.shared.u32 tmp, [%0+1024+128];"
+                                  "   st.volatile.shared.u32 [%0+1024], tmp;"
+                                  "}" :: "r"( xmad( tid, 4.f, pshare ) ) : "memory" );
                 }
 
             }
@@ -375,9 +379,9 @@ __global__ void make_texture( float4 * __restrict xyzouvwo, ushort4 * __restrict
     extern __shared__ volatile float  smem[];
     const uint warpid = threadIdx.x / 32;
     const uint lane = threadIdx.x % 32;
-    for( uint i = (blockIdx.x * blockDim.x + threadIdx.x)&0xFFFFFFE0U ; i < n ; i += blockDim.x * gridDim.x ) {
+    for( uint i = ( blockIdx.x * blockDim.x + threadIdx.x ) & 0xFFFFFFE0U ; i < n ; i += blockDim.x * gridDim.x ) {
         const float2 * base = ( float2* )( xyzuvw +  i * 6 );
-		#pragma unroll 3
+#pragma unroll 3
         for( uint j = lane; j < 96; j += 32 ) {
             float2 u = base[j];
             // NVCC bug: no operator = between volatile float2 and float2
@@ -387,16 +391,16 @@ __global__ void make_texture( float4 * __restrict xyzouvwo, ushort4 * __restrict
         uint pid = lane / 2;
         const uint x_or_v = ( lane % 2 ) * 3;
         xyzouvwo[ i * 2 + lane ] = make_float4( smem[ warpid * 192 + pid * 6 + x_or_v + 0 ],
-                                      smem[ warpid * 192 + pid * 6 + x_or_v + 1 ],
-                                      smem[ warpid * 192 + pid * 6 + x_or_v + 2 ], 0 );
+                                                smem[ warpid * 192 + pid * 6 + x_or_v + 1 ],
+                                                smem[ warpid * 192 + pid * 6 + x_or_v + 2 ], 0 );
         pid += 16;
         xyzouvwo[ i * 2 + lane + 32] = make_float4( smem[ warpid * 192 + pid * 6 + x_or_v + 0 ],
-                                          smem[ warpid * 192 + pid * 6 + x_or_v + 1 ],
-                                          smem[ warpid * 192 + pid * 6 + x_or_v + 2 ], 0 );
+                                       smem[ warpid * 192 + pid * 6 + x_or_v + 1 ],
+                                       smem[ warpid * 192 + pid * 6 + x_or_v + 2 ], 0 );
 
-        xyzo_half[i+lane] = make_ushort4( __float2half_rn( smem[ warpid * 192 + lane * 6 + 0 ] ),
-                                     __float2half_rn( smem[ warpid * 192 + lane * 6 + 1 ] ),
-                                     __float2half_rn( smem[ warpid * 192 + lane * 6 + 2 ] ), 0 );
+        xyzo_half[i + lane] = make_ushort4( __float2half_rn( smem[ warpid * 192 + lane * 6 + 0 ] ),
+                                            __float2half_rn( smem[ warpid * 192 + lane * 6 + 1 ] ),
+                                            __float2half_rn( smem[ warpid * 192 + lane * 6 + 2 ] ), 0 );
     }
 }
 
@@ -407,17 +411,16 @@ __global__ void make_texture2( uint2 *start_and_count, const int *start, const i
     }
 }
 
-__global__ void check_acc( int *signal, const int np )
+__global__ void check_acc( const int np )
 {
     double sx = 0, sy = 0, sz = 0;
     for( int i = 0; i < np; i++ ) {
-    	double ax = info.axayaz[i * 3 + 0];
-    	double ay = info.axayaz[i * 3 + 1];
-    	double az = info.axayaz[i * 3 + 2];
-    	if (ax!=ax || ay !=ay || az !=az) {
-    		printf("particle %d: %f %f %f\n",i,ax,ay,az);
-    		*signal = 1;
-    	}
+        double ax = info.axayaz[i * 3 + 0];
+        double ay = info.axayaz[i * 3 + 1];
+        double az = info.axayaz[i * 3 + 2];
+        if( ax != ax || ay != ay || az != az ) {
+            printf( "particle %d: %f %f %f\n", i, ax, ay, az );
+        }
         sx += ax;
         sy += ay;
         sz += az;
@@ -429,12 +432,12 @@ __global__ void check_acc_transposed( const int np )
 {
     double sx = 0, sy = 0, sz = 0;
     for( int i = 0; i < np; i++ ) {
-    	int base = i / 32;
-    	int off = i % 32;
-    	int p = base * 96 + off;
+        int base = i / 32;
+        int off = i % 32;
+        int p = base * 96 + off;
         sx += info.axayaz[p];
-        sy += info.axayaz[p+32];
-        sz += info.axayaz[p+64];
+        sy += info.axayaz[p + 32];
+        sz += info.axayaz[p + 64];
     }
     printf( "ACC-TRANSPOSED: %+.7lf %+.7lf %+.7lf\n", sx, sy, sz );
 }
@@ -447,14 +450,14 @@ void transpose_acc( const int np )
     const uint lane = threadIdx.x % 32;
     const uint warpid = threadIdx.x / 32;
 
-    for( uint i = (blockIdx.x * blockDim.x + threadIdx.x)&0xFFFFFFE0U; i < np; i += blockDim.x * gridDim.x ) {
+    for( uint i = ( blockIdx.x * blockDim.x + threadIdx.x ) & 0xFFFFFFE0U; i < np; i += blockDim.x * gridDim.x ) {
         const uint base = xmad( i, 3.f, lane );
         smem[warpid][lane   ] = info.axayaz[ base      ];
-        smem[warpid][lane+32] = info.axayaz[ base + 32 ];
-        smem[warpid][lane+64] = info.axayaz[ base + 64 ];
-		info.axayaz[ base      ] = smem[warpid][ xmad( __IMOD(lane+ 0,3), 32.f, (lane+ 0)/3 ) ];
-		info.axayaz[ base + 32 ] = smem[warpid][ xmad( __IMOD(lane+32,3), 32.f, (lane+32)/3 ) ];
-		info.axayaz[ base + 64 ] = smem[warpid][ xmad( __IMOD(lane+64,3), 32.f, (lane+64)/3 ) ];
+        smem[warpid][lane + 32] = info.axayaz[ base + 32 ];
+        smem[warpid][lane + 64] = info.axayaz[ base + 64 ];
+        info.axayaz[ base      ] = smem[warpid][ xmad( __IMOD( lane + 0, 3 ), 32.f, ( lane + 0 ) / 3 ) ];
+        info.axayaz[ base + 32 ] = smem[warpid][ xmad( __IMOD( lane + 32, 3 ), 32.f, ( lane + 32 ) / 3 ) ];
+        info.axayaz[ base + 64 ] = smem[warpid][ xmad( __IMOD( lane + 64, 3 ), 32.f, ( lane + 64 ) / 3 ) ];
     }
 }
 
@@ -520,7 +523,7 @@ void forces_dpd_cuda_nohost( const float * const xyzuvw, float * const axayaz,  
             cudaFree( xyzo_half );
         }
         last_size = np;
-        if (last_size%32) last_size += 32 - last_size%32;
+        if( last_size % 32 ) last_size += 32 - last_size % 32;
         cudaMalloc( &xyzouvwo,  sizeof( float4 ) * 2 * last_size );
         cudaMalloc( &xyzo_half, sizeof( ushort4 ) * last_size );
     }
@@ -567,7 +570,7 @@ void forces_dpd_cuda_nohost( const float * const xyzuvw, float * const axayaz,  
 
     // YUHANG: fixed bug: not using stream
     int np32 = np;
-    if (np32%32) np32 += 32 - np32%32;
+    if( np32 % 32 ) np32 += 32 - np32 % 32;
     CUDA_CHECK( cudaMemsetAsync( axayaz, 0, sizeof( float )* np32 * 3, stream ) );
 
     if( c.ncells.x % MYCPBX == 0 && c.ncells.y % MYCPBY == 0 && c.ncells.z % MYCPBZ == 0 ) {
@@ -581,7 +584,7 @@ void forces_dpd_cuda_nohost( const float * const xyzuvw, float * const axayaz,  
     }
 
 #ifdef ONESTEP
-    check_acc <<< 1, 1>>>( np );
+    check_acc <<< 1, 1, 0, stream>>>( np );
     CUDA_CHECK( cudaDeviceSynchronize() );
     CUDA_CHECK( cudaDeviceReset() );
     MPI_Finalize();
