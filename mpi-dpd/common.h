6a4d497389003dd4087945796ff7cb5762afc208
Diego Rossinelli <rossinelli.diego@gmail.com>
2015-08-07 14:42:56 +0200

    dummy commit

--- mpi-dpd/common.h
+++ mpi-dpd/common.h
@@ -18,13 +18,14 @@
 #define cuda_printf(...) do { printf(__VA_ARGS__); } while(0)
 #endif
 
-enum { 
-    XSIZE_SUBDOMAIN = 64,
-    YSIZE_SUBDOMAIN = 64,
-    ZSIZE_SUBDOMAIN = 64,
-    XMARGIN_WALL = 14,
-    YMARGIN_WALL = 14,
-    ZMARGIN_WALL = 14,
+enum 
+{ 
+    XSIZE_SUBDOMAIN = 48,
+    YSIZE_SUBDOMAIN = 48,
+    ZSIZE_SUBDOMAIN = 48,
+    XMARGIN_WALL = 6,
+    YMARGIN_WALL = 6,
+    ZMARGIN_WALL = 6,
 };
 
 const int numberdensity = 4;
