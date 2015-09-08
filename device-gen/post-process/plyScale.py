cd223377423c868d29727f17c51cdeb33bc14750
kirilllykov <lykov.kirill@gmail.com>
2015-09-08 20:35:34 +0200

    Modified plyScale so it can cut ply with more options

--- device-gen/post-process/plyScale.py
+++ device-gen/post-process/plyScale.py
@@ -29,14 +29,17 @@ def computeExtent(vertices):
     origExtent = [extentMax[i] - extentMin[i] for i in range(0, 3)]
     return (origOrigin, origExtent)
 
-parser = argparse.ArgumentParser(description='Scales ply file.\n Example: ./plyScale.py -f input.ply -o out.ply -x 150 -y 40 -z 48')
+parser = argparse.ArgumentParser(description='Modifies ply file to be used for rendering.\n Example: ./plyScale.py -f input.ply -o out.ply -r 210 -cutX 10.0')
 parser.add_argument('-f','--inputFile', help='Input file name', required=True)
 parser.add_argument('-o','--outputFile', help='Output file name', required=True)
-parser.add_argument('-x','--lx', help='', required=False, default="0")
-parser.add_argument('-y','--ly', help='', required=False, default="0")
-parser.add_argument('-z','--lz', help='', required=False, default="0")
-parser.add_argument('-r','--order', help='values are 0-2', required=False, default="012")
-parser.add_argument('-c','--cut', help='values are 0-2', required=False, default="none")
+parser.add_argument('--lx', help='Desired size of bounding box (X axis)', required=False, default="0")
+parser.add_argument('--ly', help='Desired size of bounding box (Y axis)', required=False, default="0")
+parser.add_argument('--lz', help='Desired size of bounding box (Z axis)', required=False, default="0")
+parser.add_argument('-r','--order', help='Reorder axis. By default 012, to swap x and z use 210', required=False, default="012")
+helpStringForCut = 'Remove all the faces which are above specified value for %s. The origin is in the center of mass. If axis reordering was applied, axis are in new coordinates.'
+parser.add_argument('--cutX', help=helpStringForCut%('X'), required=False, default="none")
+parser.add_argument('--cutY', help=helpStringForCut%('Y'), required=False, default="none")
+parser.add_argument('--cutZ', help=helpStringForCut%('Z'), required=False, default="none")
 args = vars(parser.parse_args())
 
 desiredBox = [float(args['lx']), float(args['ly']), float(args['lz'])]
@@ -68,14 +71,21 @@ for i in range(0, len(vertices)):
         vertices[i][dim] -= origOrigin[dim]
         vertices[i][dim] *= desiredBox[dim]/origExtent[dim]
 
-if (args['cut'] != "none"):
+if (args['cutX'] != "none" or args['cutY'] != "none" or args['cutZ'] != "none"):
     print "Cut it!"
+    cut = [1e6]*3
+    if args['cutX'] != "none":
+        cut[0] = float(args['cutX'])
+    if args['cutY'] != "none":
+        cut[1] = float(args['cutY'])
+    if args['cutZ'] != "none":
+        cut[2] = float(args['cutZ'])
+
     toDelete = list()
-    dim = int(args['cut'])
     newInx = [None]*len(vertices)
     j = 0
     for i in range(0, len(vertices)):
-        if (vertices[i][dim] > 0.0):
+        if (vertices[i][0] > cut[0] or vertices[i][1] > cut[1] or vertices[i][2] > cut[2]):
             toDelete.append(i)
         else:
             newInx[i] = j
@@ -104,5 +114,5 @@ if (args['cut'] != "none"):
 print ("Extent is (%f, %f, %f). Center is (%f, %f, %f)."%(finalExtent[0], finalExtent[1], finalExtent[2], 
                                                           finalOrigin[0], finalOrigin[1], finalOrigin[2]))
 
-
 plydata.write(args['outputFile'])
+
