2daaf55e99552d73e8122733bc9eae7063dad5d5
kirilllykov <lykov.kirill@gmail.com>
2015-09-09 17:04:39 +0200

    Extracted base class

--- device-gen/funnels/main.cpp
+++ device-gen/funnels/main.cpp
@@ -62,21 +62,36 @@ struct Parabola
     }
 };
 
-class FunnelsBuilder
+class DeviceBuilder
 {
+protected:
     typedef vector<float> SDF;
     int m_ncolumns, m_nrows;
     float m_resolution, m_zmargin;
     float m_unitSizeX, m_unitSizeY, m_unitSizeZ; // size of the egg with the empty space aroung it
     std::string m_outFileName2D, m_outFileName3D;
     int m_niterRedistance;
+    int m_unitNX, m_unitNY, m_unitNZ;
 public:
-    FunnelsBuilder()
+    DeviceBuilder(float unitSizeX, float unitSizeY, float unitSizeZ)
     : m_ncolumns(0), m_nrows(0), m_resolution(0), m_zmargin(0),
-      m_unitSizeX(24.0f), m_unitSizeY(96.0f), m_unitSizeZ(58.0f),
-      m_niterRedistance(1e3)
+      m_unitSizeX(unitSizeX), m_unitSizeY(unitSizeY), m_unitSizeZ(unitSizeZ),
+      m_niterRedistance(1e3), m_unitNX(0), m_unitNY(0), m_unitNZ(0)
     {}
 
+    virtual void build() = 0;
+
+    virtual ~DeviceBuilder() {}
+};
+
+class FunnelsBuilder : public DeviceBuilder
+{
+    float m_gapSpace; // unit gap between obstacles
+public:
+    FunnelsBuilder()
+    : DeviceBuilder(24.0f, 96.0f, 58.0f), m_gapSpace(1.0f)
+    {}
+    
     FunnelsBuilder& setNColumns(int ncolumns)
     {
         m_ncolumns = ncolumns;
@@ -113,31 +128,30 @@ public:
         return *this;
     }
 
-    void build() const;
+    void build();
 
 private:
-    void generateUnitSDF(const int NX, const int NY, const float xextent, const float yextent, float gap, vector<float>& sdf) const;
+    void generateUnitSDF(float gap, vector<float>& sdf) const;
 };
-// TODO reduce number of parameters
-void FunnelsBuilder::generateUnitSDF(const int NX, const int NY, const float xextent, const float yextent, float gap, vector<float>& sdf) const
+
+void FunnelsBuilder::generateUnitSDF(float gap, vector<float>& sdf) const
 {
+    assert(m_unitNX * m_unitNY * m_unitNZ != 0);
     vector<float> xs, ys;
-    Parabola par(gap, xextent);
+    Parabola par(gap, m_unitSizeX);
     par.line1(xs, ys);
     par.line2(xs, ys);   
 
-    const float xlb = -xextent/2.0f;
-    const float ylb = -(yextent - par.ymax)/2.0f; 
-    printf("starting brute force sdf with %d x %d starting from %f %f to %f %f\n",
-           NX, NY, xlb, ylb, xlb + xextent, ylb + yextent);
+    const float xlb = -m_unitSizeX/2.0f;
+    const float ylb = -(m_unitSizeY - par.ymax)/2.0f; 
     
-    sdf.resize(NX * NY, 0.0f);
-    const float dx = xextent / NX; //TODO NX-1
-    const float dy = yextent / NY;
+    sdf.resize(m_unitNX * m_unitNY, 0.0f);
+    const float dx = m_unitSizeX / (m_unitNX - 1); //TODO NX-1
+    const float dy = m_unitSizeY / (m_unitNY - 1);
     const int nsamples = xs.size();
     
-    for(int iy = 0; iy < NY; ++iy)
-    for(int ix = 0; ix < NX; ++ix)
+    for(int iy = 0; iy < m_unitNY; ++iy)
+    for(int ix = 0; ix < m_unitNX; ++ix)
     {
         const float x = xlb + ix * dx;
         const float y = ylb + iy * dy;
@@ -168,41 +182,39 @@ void FunnelsBuilder::generateUnitSDF(const int NX, const int NY, const float xex
         }
 
         
-        sdf[ix + NX * iy] = s * sqrt(distance2);
+        sdf[ix + m_unitNX * iy] = s * sqrt(distance2);
     }
 }
 
-void FunnelsBuilder::build() const
+void FunnelsBuilder::build()
 {
     if (m_ncolumns *  m_nrows * m_resolution * m_zmargin == 0.0f || m_outFileName3D.length() == 0)
         throw std::runtime_error("Invalid parameters");
 
     // 1 Create obstacles with different gaps
-    const float m_gapSpace = 1.0;
-
-    const int unitNX = static_cast<int>(m_unitSizeX * m_resolution);
-    const int unitNY = static_cast<int>(m_unitSizeY * m_resolution);
-    const int unitNZ = static_cast<int>(m_unitSizeZ * m_resolution); 
+    m_unitNX = static_cast<int>(m_unitSizeX * m_resolution);
+    m_unitNY = static_cast<int>(m_unitSizeY * m_resolution);
+    m_unitNZ = static_cast<int>(m_unitSizeZ * m_resolution); 
  
     std::vector<SDF> unitSDF(m_nrows);
     for (int i = 0; i < m_nrows; ++i) {
         float gap = m_gapSpace * (i + 3);
-        generateUnitSDF(unitNX, unitNY, m_unitSizeX, m_unitSizeY, gap, unitSDF[i]);
+        generateUnitSDF(gap, unitSDF[i]);
     }
 
     // 2 Create rows of obstacles
     std::vector<SDF> rows(m_nrows);
     for (int i = 0; i < m_nrows; ++i) {
-        populateSDF(unitNX, unitNY, m_unitSizeX, m_unitSizeY, unitSDF[i], m_ncolumns, 1, rows[i]);
+        populateSDF(m_unitNX, m_unitNY, m_unitSizeX, m_unitSizeY, unitSDF[i], m_ncolumns, 1, rows[i]);
     }    
 
     // 3 Collage rows
     SDF finalSDF;
-    collageSDF(unitNX * m_ncolumns, unitNY, m_unitSizeX * m_ncolumns, m_unitSizeY, rows, m_nrows, false, finalSDF);
+    collageSDF(m_unitNX * m_ncolumns, m_unitNY, m_unitSizeX * m_ncolumns, m_unitSizeY, rows, m_nrows, false, finalSDF);
 
     // 4 Apply redistancing for the result
     float finalExtent[] = {m_unitSizeX * m_ncolumns, m_unitSizeY * m_nrows};
-    int finalN[] = {unitNX * m_ncolumns, unitNY * m_nrows};
+    int finalN[] = {m_unitNX * m_ncolumns, m_unitNY * m_nrows};
     const float dx = finalExtent[0] / (finalN[0] - 1);
     const float dy = finalExtent[1] / (finalN[1] - 1);
     Redistance redistancer(0.25f * min(dx, dy), dx, dy, finalN[0], finalN[1]);
@@ -211,18 +223,19 @@ void FunnelsBuilder::build() const
     if (m_outFileName2D.length() != 0)
         writeDAT(m_outFileName2D.c_str(), finalSDF, finalExtent[0], finalExtent[1], 1.0f, finalN[0], finalN[1], 1);
     
-    conver2Dto3D(finalN[0], finalN[1], finalExtent[0], finalExtent[1], finalSDF, unitNZ, m_unitSizeZ - 2.0f*m_zmargin, m_zmargin, m_outFileName3D);
+    conver2Dto3D(finalN[0], finalN[1], finalExtent[0], finalExtent[1], finalSDF, m_unitNZ, m_unitSizeZ - 2.0f*m_zmargin, m_zmargin, m_outFileName3D);
 }
 
 int main(int argc, char ** argv)
 {
-    //ArgumentParser argp(vector<string>(argv, argv + argc));
+    ArgumentParser argp(vector<string>(argv, argv + argc));
 
-    int nColumns = 4; //argp("-nColumns").asInt(1);
-    int nRows = 2; //argp("-nRows").asInt(1);
-    float zMargin = 5.0f; //static_cast<float>(argp("-zMargin").asDouble(5.0));
+    int nColumns = argp("-nColumns").asInt(1);
+    int nRows = argp("-nRows").asInt(1);
+    float zMargin = static_cast<float>(argp("-zMargin").asDouble(5.0));
+    float resolution = static_cast<float>(argp("-zResolution").asDouble(1.0));
 
-    std::string outFileName = "3d.dat";//argp("-out").asString("3d");
+    std::string outFileName = argp("-out").asString("3d");
 
     FunnelsBuilder builder;
     try {
@@ -230,11 +243,12 @@ int main(int argc, char ** argv)
                .setNRows(nRows)
                .setResolution(1.0f)
                .setZWallWidth(zMargin)
-               .setFileNameFor2D("2d.dat")
                .setFileNameFor3D(outFileName)
+               .setResolution(resolution)
                .build();
     } catch(const std::exception& ex) {
         std::cout << "ERROR: " << ex.what() << std::endl;
+        return 1;
     }
     return 0;
 }
