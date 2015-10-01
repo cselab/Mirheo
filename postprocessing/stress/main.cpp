ad9595b720190c0d01b2ce87fb5dde8e52470ead
Diego Rossinelli <rossinelli.diego@gmail.com>
2015-10-01 15:39:39 +0200

    refactoring stress post processor

--- postprocessing/stress/main.cpp
+++ postprocessing/stress/main.cpp
@@ -1,18 +1,32 @@
 #include <cstdio>
+#include <vector>
 
 #include <argument-parser.h>
 
-int main()
+using namespace std;
+
+int main(const int argc, const char ** argv)
 {
-    printf("hello\n");
+    const bool verbose = false;
 
-    const float origin[3] = {0, 0, 0};
-    const float extent[3] = {48, 48, 48};
-    const bool project[3] = {true, true, true};
+    ArgumentParser argp(argc, argv);//("-lightpos").asVecFloat(4)
+
+    const bool avg = argp("-average").asBool(true);
+    vector<float> origin = argp("-origin").asVecFloat(3);
+    vector<float> extent = argp("-extent").asVecFloat(3);
+    vector<float> projectf = argp("-project").asVecFloat(3);
+
+    bool project[3];
+    for(int c = 0; c < 3; ++c)
+	project[c] = projectf[c] != 0;
+
+    int nprojections = 0;
+    for(int c = 0; c < 3; ++c)
+	nprojections += project[c];
+
     const int noutputchannels = 6;
-
     const size_t chunksize = (1 << 29) / 9 / sizeof(float);
-    
+
     float * const pbuf = new float[9 * chunksize];
 
     float binsize[3];
@@ -27,16 +41,18 @@ int main()
 
     int * const bincount = new int[ntotbins];
     memset(bincount, 0, sizeof(int) * ntotbins);
-    
+
     const int noutput = noutputchannels * ntotbins;
-    
+
     float * const bindata = new float[noutput];
     memset(bindata, 0, sizeof(float) * noutput);
-    
+
     FILE * fin = fopen("../../mpi-dpd/stress/stresses-00019.data", "r");
     assert(fin);
 
-    printf("reading...\n");
+    if (verbose)				\
+	printf("reading...\n");
+
     fseek(fin, 0, SEEK_END);
     const size_t filesize = ftell(fin);
     fseek(fin, 0, SEEK_SET);
@@ -44,70 +60,66 @@ int main()
     const size_t nparticles = filesize / 9 / sizeof(float);
     assert(filesize % (9 * sizeof(float)) == 0);
 
-    const int nhotparticles = min(nparticles, chunksize);
-    fread(pbuf, sizeof(float) * 9, nhotparticles, fin);
-    
-    printf("i have found %d particles\n", nparticles);
-    printf("particle chunk %d\n", chunksize);
-
-    size_t nvalid = 0;
-
+    if (verbose)
     {
-	float avgs[9];
-	for(int i = 0; i < 9; ++i)
-	    avgs[i] = 0;
+	printf("i have found %d particles\n", nparticles);
+	printf("particle chunk %d\n", chunksize);
+    }
+
+    for(size_t base = 0; base < nparticles; base += chunksize)
+    {
+	const int nhotparticles = min(nparticles - base, chunksize);
+	fread(pbuf, sizeof(float) * 9, nhotparticles, fin);
+
+#ifndef NDEBUG
+	if (verbose)
+	{
+	    float avgs[9];
+	    for(int i = 0; i < 9; ++i)
+		avgs[i] = 0;
+
+	    for(int i = 0; i < nhotparticles; ++i)
+		for(int c = 0; c < 9; ++c)
+		    avgs[c] += pbuf[9 * i + c];
+
+	    for(int i = 0; i < 9; ++i)
+		printf("AVG %d: %.3e\n", i, avgs[i] / nhotparticles);
+	}
+#endif
 
 	for(int i = 0; i < nhotparticles; ++i)
-	    for(int c = 0; c < 9; ++c)
-		avgs[c] += pbuf[9 * i + c];
-
-	for(int i = 0; i < 9; ++i)
-	    printf("AVG %d: %.3e\n", i, avgs[i] / nhotparticles);
-    }
-    
-    for(int i = 0; i < nhotparticles; ++i)
-    {
-	int index[3];
-	for(int c = 0; c < 3; ++c)
-	    index[c] = (int)((pbuf[9 * i + c] - origin[c]) / binsize[c]);
-
-	bool valid = true;
-	for(int c = 0; c < 3; ++c)
-	    valid &= index[c] >= 0 && index[c] < nbins[c];
-
-	if (!valid)
-	    continue;
-
-	const int binid = index[0] + nbins[0] * (index[1] + nbins[1] * index[2]);
-	++bincount[binid];
-	
-	const int base = noutputchannels * binid;
-	
-	for(int c = 0; c < 6; ++c)
-	    bindata[base + c] += pbuf[9 * i + 3 + c];
-
-	++nvalid;
-    }
-
-    const bool avg = true;
-    if (avg)
-	for(int i = 0; i < ntotbins; ++i)
 	{
-	    printf("bincount %03d\n", bincount[i]);
+	    int index[3];
+	    for(int c = 0; c < 3; ++c)
+		index[c] = (int)((pbuf[9 * i + c] - origin[c]) / binsize[c]);
+
+	    bool valid = true;
+	    for(int c = 0; c < 3; ++c)
+		valid &= index[c] >= 0 && index[c] < nbins[c];
+
+	    if (!valid)
+		continue;
+
+	    const int binid = index[0] + nbins[0] * (index[1] + nbins[1] * index[2]);
+	    ++bincount[binid];
+
+	    const int base = noutputchannels * binid;
 
 	    for(int c = 0; c < 6; ++c)
-		bindata[9 * i + c] /= bincount[i];
+		bindata[base + c] += pbuf[9 * i + 3 + c];
 	}
+    }
 
+    fclose(fin);
 
-    int nprojections = 0;
-    for(int c = 0; c < 3; ++c)
-	nprojections += project[c];
+    if (avg)
+	for(int i = 0; i < ntotbins; ++i)
+	    for(int c = 0; c < noutputchannels; ++c)
+		bindata[noutputchannels * i + c] /= bincount[i];
 
-    if (nprojections == 3) //six numbers
+    if (nprojections == 3)
     {
 	assert(noutput == noutputchannels);
-	printf("result: ");
 
 	for(int c = 0; c < noutputchannels; ++c)
 	    printf("%+.3e\t", bindata[c]);
@@ -116,13 +128,14 @@ int main()
     }
     else if (nprojections == 2)
     {
+	
 	int ctr = 0;
 	for(int iz = 0; iz < nbins[2]; ++iz)
 	    for(int iy = 0; iy < nbins[1]; ++iy)
 		for(int ix = 0; ix < nbins[0]; ++ix)
 		{
 		    printf("%03d ", ctr);
-		    
+
 		    for(int c = 0; c < noutputchannels; ++c)
 			printf("%+.3e ", bindata[noutputchannels * ctr + c]);
 
@@ -132,7 +145,7 @@ int main()
 		}
     }
     else if (nprojections == 1)
-    {	
+    {
 	int nx;
 	for(int c = 0; c < 3; ++c)
 	    if (nbins[c] > 1)
@@ -140,11 +153,12 @@ int main()
 		nx = nbins[c];
 		break;
 	    }
-	    
+
 	for(int c = 0; c < noutputchannels; ++c)
 	{
+	    //printf("OUTPUT %d:\n", c);
 	    int ctr = 0;
-	    
+	    //printf("start\n");
 	    for(int iz = 0; iz < nbins[2]; ++iz)
 		for(int iy = 0; iy < nbins[1]; ++iy)
 		    for(int ix = 0; ix < nbins[0]; ++ix)
@@ -156,12 +170,17 @@ int main()
 			if (ctr % nx == 0)
 			    printf("\n");
 		    }
+	    //printf("stop\n");
+	    
+	    if (c < noutputchannels - 1)
+		printf("END OUTPUT\n");
 	}
     }
-    
-    printf("valid: %d\n", nvalid);
-    
-    fclose(fin);
+    else
+    {
+	printf("woops invalid number of projections. Exiting now...\n");
+	exit(-1);
+    }
 
     delete [] pbuf;
     delete [] bincount;
