d4349ac92cc9d4a7d8f09722060d60a7891dd656
Dmitry <alexeedm@ethz.ch>
2016-09-13 20:04:49 +0200

    shared, doesn't look good

--- cuda-dpd/dpd/dummy-dpd.cu
+++ cuda-dpd/dpd/dummy-dpd.cu
@@ -14,12 +14,6 @@ __device__ __forceinline__ float3 readCoos(const ushort4* half_xyzo, int pid)
 	return make_float3(__half2float(tmp.x), __half2float(tmp.y), __half2float(tmp.z));
 }
 
-__device__ __forceinline__ void readRow(const float2 * xyzuvw, int n, float2* shmem, int tid)
-{
-	for (int i=tid; i<n; i+=warpSize)
-		shmem[i] = xyzuvw[i];
-}
-
 __device__ __forceinline__ float sqr(float x)
 {
 	return x*x;
@@ -85,35 +79,54 @@ __device__ __forceinline__ float3 _dpd_interaction(const float4* __restrict__ xy
 	return make_float3(strength * xr, strength * yr, strength * zr);
 }
 
-__device__ __forceinline__ int warpAggrFetchAdd(int* addr, bool pred)
+__device__ __forceinline__ int warpAggrFetchAdd(int val, bool pred, int tid)
 {
-	const int tid = threadIdx.x & 31;
 	int res;
 	const int mask = ballot(pred); // i-th bit is if lane i is interacting
 	const int leader = __ffs(mask) - 1;  // 0-th thread is not always active
 
 	if (tid == leader)
 	{
-		res = *addr;
-		*addr += __popc(mask); // n of non-zero bits
+		res = val;
+		val += __popc(mask); // n of non-zero bits
 	}
 
 	res = __shfl(res, leader);
 	return res + __popc( mask & ((1 << tid) - 1) );
 }
 
+__device__ __forceinline__ void execInteractions(const int nentries, const int2* inters, const float4* xyzouvwo, float* axayaz, int tid, int dstStart, int dstEnd)
+{
+	const int2 pids = inters[tid];
+	if (pids.x == pids.y || tid >= nentries) return;
+	float3 frc = _dpd_interaction(xyzouvwo, pids.x, pids.y);
+
+	float* dest = axayaz + pids.x*3;
+	atomicAdd(dest,     frc.x);
+	atomicAdd(dest + 1, frc.y);
+	atomicAdd(dest + 2, frc.z);
+
+	// If src is in the same cell as dst,
+	// interaction will be computed twice
+	// no need for atomics in this case
+	if (pids.y < dstStart || pids.y >= dstEnd)
+	{
+		dest = axayaz + pids.y*3;
+		atomicAdd(dest,     -frc.x);
+		atomicAdd(dest + 1, -frc.y);
+		atomicAdd(dest + 2, -frc.z);
+	}
+}
+
 //__launch_bounds__(128, 14)
 __global__ void smth(const float4 * __restrict__ xyzouvwo, const ushort4 *  half_xyzo, float* axayaz, const int * __restrict__ cellsstart, const int * __restrict__ cellscount,
 		int nCellsX, int nCellsY, int nCellsZ, int ndens, int np, int ncells)
 {
-	extern __shared__ int shmem[];
+	__shared__ int  nentries[4];
+	__shared__ int2 inters[4][32];
 
 	const int tid = threadIdx.x;
 	const int wid = threadIdx.z * blockDim.z + threadIdx.y;
-
-	int*  myshmem  = shmem + wid * ndens * ndens*3 * 2*2 + 1;
-	int*  nentries = myshmem;
-	int2* inters   = (int2*)(myshmem+1);
 
 	const int cellX0 = blockIdx.x;
 	const int cellY0 = blockIdx.y * blockDim.y + threadIdx.y;
@@ -124,7 +137,7 @@ __global__ void smth(const float4 * __restrict__ xyzouvwo, const ushort4 *  half
 	const int dstStart = cellsstart[cid];
 	const int dstEnd   = cellsstart[cid+1];
 
-	*nentries = 0;
+	nentries[wid] = 0;
 
 	for (int curDst=dstStart; curDst < dstEnd; curDst+=nDstPerIter)
 	{
@@ -139,78 +152,99 @@ __global__ void smth(const float4 * __restrict__ xyzouvwo, const ushort4 *  half
 		const int groupSize = 6;
 		const int groupId = tid / groupSize;
 		const int idInGroup = tid - groupSize*groupId;
-		const int cellY = cellY0 + (groupId > 2) ? groupId - 4 : groupId - 1;
-		const int cellZ = cellZ0 + (groupId > 2) ? 0 : 1;
+		const int cellY = cellY0 + ((groupId > 2) ? groupId - 4 : groupId - 1);
+		const int cellZ = cellZ0 + ((groupId > 2) ? 0 : 1);
 
-		if (cellY < 0 || cellY >= nCellsY || cellZ < 0 || cellZ >= nCellsZ) break;
-		if (groupId > 4) break;
+		bool valid = (cellY >= 0 && cellY < nCellsY && cellZ >= 0 && cellZ < nCellsZ && groupId < 5);
 
 		const int midCellId = (cellZ*nCellsY + cellY)*nCellsX + cellX0;
 		int rowStart  = max(midCellId-1, 0);//(cellX0 == 0)         ? midCellId     : midCellId - 1;
 		int rowEnd    = min(midCellId+2, ncells+1);
 		if (midCellId == cid) rowEnd = midCellId + 1; // this row is already partly covered
 
-		const int pstart = cellsstart[rowStart];
-		const int pend   = cellsstart[rowEnd];
+		const int pstart = valid ? cellsstart[rowStart] : 0;
+		const int pend   = valid ? cellsstart[rowEnd] : -1;
 
-#pragma unroll 1
-		for (int srcId = pstart + idInGroup; srcId < pend; srcId += groupSize)
+		//#pragma unroll 3
+		for (int srcId = pstart + idInGroup; __any(srcId < pend); srcId += groupSize)
 		{
-			const float3 srcCoos = readCoos(half_xyzo, srcId);
+			if (srcId < pend)
+			{
+				const float3 srcCoos = readCoos(half_xyzo, srcId);
 
 #pragma unroll
-			for (int i=0; i<nDstPerIter; i++)
-			{
-				bool interacting = distance2(srcCoos, dstCoos[i]) < 1.01f && curDst+i < dstEnd;
-				const int myentry = warpAggrFetchAdd(nentries, interacting);
-
-				if (interacting)
+				for (int i=0; i<nDstPerIter; i++)
 				{
-					const int dstId = curDst+i;
-					if (srcId == dstId) continue;
-					float3 frc = _dpd_interaction(xyzouvwo, dstId, srcId);
+					bool interacting = distance2(srcCoos, dstCoos[i]) < 1.02f && curDst+i < dstEnd;
+					const int myentry = warpAggrFetchAdd(nentries[wid], interacting, tid);
 
-					atomicAdd(axayaz + dstId*3 + 0, frc.x);
-					atomicAdd(axayaz + dstId*3 + 1, frc.y);
-					atomicAdd(axayaz + dstId*3 + 2, frc.z);
-
-					atomicAdd(axayaz + srcId*3 + 0, -frc.x);
-					atomicAdd(axayaz + srcId*3 + 1, -frc.y);
-					atomicAdd(axayaz + srcId*3 + 2, -frc.z);
-
-
-					inters[myentry] = make_int2(curDst+i, frc.x);
-					//if (myentry > 80) printf("lane %d, nentry: %d\n", tid, myentry);
+					if (interacting)
+					{
+						inters[wid][myentry] = make_int2(curDst+i, srcId);
+						//if (myentry > 80) printf("lane %d, nentry: %d\n", tid, myentry);
+					}
 				}
+			}
+
+			if (nentries[wid] >= warpSize)
+			{
+				execInteractions(nentries[wid], inters[wid] + nentries[wid] - warpSize, xyzouvwo, axayaz, tid, dstStart, dstEnd);
+				nentries[wid] -= warpSize;
 			}
 		}
 	}
 
-	return;
+	if (nentries[wid])
+		execInteractions(nentries[wid], inters[wid], xyzouvwo, axayaz, tid, dstStart, dstEnd);
+}
 
-	const int totEntries = *nentries;
-//	if (tid == 0)
-//		printf("cid %02d (%d %d %d):   %d\n", cid, cellX0, cellY0, cellZ0, totEntries/4);
-
-#pragma unroll 2
-	for (int i=tid; i<totEntries; i+=warpSize)
+__device__ __forceinline__ void loadRow(const ushort4 * __restrict__ half_xyzo, int n, float3* shmem, int tid)
+{
+	for (int i=tid; i<n; i+=warpSize)
 	{
-		const int2 pids = inters[i];
-		if (pids.x == pids.y) continue;
-		float3 frc = _dpd_interaction(xyzouvwo, pids.x, pids.y);
+		ushort4 tmp = half_xyzo[i];
+		shmem[i] = make_float3(__half2float(tmp.x), __half2float(tmp.y), __half2float(tmp.z));
+	}
+}
 
-		atomicAdd(axayaz + pids.x*3 + 0, frc.x);
-		atomicAdd(axayaz + pids.x*3 + 1, frc.y);
-		atomicAdd(axayaz + pids.x*3 + 2, frc.z);
+const static __device__ float2 id2cooYZ[] = { {0,0}, {1,0}, {0,1}, {1,1}, {2,1}, {0,2}, {1,2}, {2,2}, {0,3}, {1,3}, {2,3} };
 
-		// If src is in the same cell as dst,
-		// interaction will be computed twice
-		// no need for atomics in this case
-		if (pids.y < dstStart || pids.y >= dstEnd)
+__global__ void smth2(const float4 * __restrict__ xyzouvwo, const ushort4 * __restrict__ half_xyzo, float* axayaz, const int * __restrict__ cellsstart, const int * __restrict__ cellscount,
+		float nCellsX, float nCellsY, float nCellsZ, int ndens, int np, int ncells)
+{
+	__shared__ float3 cache[11][16];
+
+	const int tid = threadIdx.x;
+	const int wid = threadIdx.y;
+
+	const float cellX0 = blockIdx.x;
+	const float cellY0 = blockIdx.y*2.0f;
+	const float cellZ0 = blockIdx.z*2.0f;
+
+	//const int cid = (cellZ0*nCellsY + cellY0)*nCellsX + cellX0;
+	//const int blockBaseCellid = (cellZ0*nCellsY + cellY0)*nCellsX + cellX0;
+
+//	const int dstStart = cellsstart[cid];
+//	const int dstEnd   = cellsstart[cid+1];
+
+#pragma unroll 3
+	for (int row = wid; row < 11; row+=4)
+	{
+		float cellY = id2cooYZ[row].x + cellY0;
+		float cellZ = id2cooYZ[row].y + cellZ0;
+
+		if (cellY >= 0.0f && cellY < nCellsY && cellZ >= 0.0f && cellZ < nCellsZ)
 		{
-			atomicAdd(axayaz + pids.y*3 + 0, -frc.x);
-			atomicAdd(axayaz + pids.y*3 + 1, -frc.y);
-			atomicAdd(axayaz + pids.y*3 + 2, -frc.z);
+			float midCellId = (cellZ*nCellsY + cellY)*nCellsX + cellX0;
+
+			int rowStart  = max((int)round(midCellId-1.0f), 0);
+			int rowEnd    = min((int)round(midCellId+2.0f), ncells+1);
+
+			const int pstart = cellsstart[rowStart];
+			const int pend   = cellsstart[rowEnd];
+
+			//printf("start %d end %d,  id %d\n", pstart, pend, (wid << 2) + cellY);
+			loadRow(half_xyzo + pstart, (pend - pstart), cache[row], tid);
 		}
 	}
 }
@@ -234,13 +268,13 @@ void forces_dpd_cuda_nohost( const float * const xyzuvw, const float4 * const xy
 	const int nz = round(ZL / rc);
 
 	dim3 nblocks(nx, ny/2, nz/2);
-	dim3 nthreads(32, 2, 2);
+	dim3 nthreads(32, 4, 1);
 	const int shmemSize = (nthreads.x * nthreads.y * nthreads.z / 32) * ndens * ndens*3 * 2*2 * sizeof(int) + 4*sizeof(int);
 
 	cudaFuncSetCacheConfig( smth, cudaFuncCachePreferNone );
 
 	cudaMemsetAsync( axayaz, 0, sizeof( float )* np * 3, stream );
-	smth<<< nblocks, nthreads, shmemSize >>>(xyzouvwo, xyzo_half, axayaz, cellsstart, cellscount, nx, ny, nz, ndens, np, nx*ny*nz);
+	smth2<<< nblocks, nthreads >>>(xyzouvwo, xyzo_half, axayaz, cellsstart, cellscount, nx, ny, nz, ndens, np, nx*ny*nz);
 	cudaPeekAtLastError();
 }
 
