bc9a856b460680e65f633f60b7ce0c0fcf8a4e0d
Dmitry Alexeev <dimaleks@gmail.com>
2017-09-01 00:47:02 +0200

    only debug version is working

--- core/rbc_kernels/bounce.h
+++ core/rbc_kernels/bounce.h
@@ -20,14 +20,21 @@ __device__ __host__ __forceinline__ void expandBy(float3& r0, float3& r1, float3
 }
 
 
-// https://en.wikipedia.org/wiki/Barycentric_coordinate_system#Determining_location_with_respect_to_a_triangle
-__device__ __host__ __forceinline__ float3 computeBaricentric(float3 r0, float3 r1, float3 r2, float3 v)
+// https://gamedev.stackexchange.com/questions/23743/whats-the-most-efficient-way-to-find-barycentric-coordinates
+__device__ __host__ __forceinline__ float3 computeBarycentric(float3 a, float3 b, float3 c, float3 p)
 {
-	float denominator = 1.0f / ( (r1.y-r2.y)*(r0.x-r2.x) + (r2.x-r1.x)*(r0.y-r2.y) );
+	float3 v0 = b - a, v1 = c - a, v2 = p - a;
 
-	float l0 = ((r1.y-r2.y)*(v.x-r2.x) + (r2.x-r1.x)*(v.y-r2.y)) * denominator;
-	float l1 = ((r2.y-r0.y)*(v.x-r2.x) + (r0.x-r2.x)*(v.y-r2.y)) * denominator;
-	float l2 = 1.0f - l0 - l1;
+	float d00 = dot(v0, v0);
+	float d01 = dot(v0, v1);
+	float d11 = dot(v1, v1);
+	float d20 = dot(v2, v0);
+	float d21 = dot(v2, v1);
+	float invDenom = 1.0f / (d00 * d11 - d01 * d01);
+
+	float l1 = (d11 * d20 - d01 * d21) * invDenom;
+	float l2 = (d00 * d21 - d01 * d20) * invDenom;
+	float l0 = 1.0f - l1 - l2;
 
 	return make_float3(l0, l1, l2);
 }
@@ -37,7 +44,7 @@ __device__ __host__ __forceinline__ float3 computeBaricentric(float3 r0, float3 
 // what are the vertex forces induced by the collision?
 __device__ __host__ __forceinline__ void triangleForces(
 		float3 x0, float3 x1, float3 x2, float m,
-		float3 O_baricentric, float3 U, float M,
+		float3 O_barycentric, float3 U, float M,
 		float dt,
 		float3& f0, float3& f1, float3& f2)
 {
@@ -51,14 +58,14 @@ __device__ __host__ __forceinline__ void triangleForces(
 	const float3 U_par = U - IU_ortI * n;
 
 	const float a = 2.0f*M/m * IU_ortI;
-	const float v0_ort = O_baricentric.x * a;
-	const float v1_ort = O_baricentric.y * a;
-	const float v2_ort = O_baricentric.z * a;
+	const float v0_ort = O_barycentric.x * a;
+	const float v1_ort = O_barycentric.y * a;
+	const float v2_ort = O_barycentric.z * a;
 
 	const float3 C = 0.333333333f * (x0+x1+x2);
 	const float3 Vc = 0.666666666f * M/m * U_par;
 
-	const float3 O = O_baricentric.x * x0 + O_baricentric.y * x1 + O_baricentric.z * x2;
+	const float3 O = O_barycentric.x * x0 + O_barycentric.y * x1 + O_barycentric.z * x2;
 	const float3 L = 2.0f*M * cross(C-O, U_par);
 
 	const float J = m * (len2(C-x0) + len2(C-x1) + len2(C-x2));
@@ -82,9 +89,9 @@ __device__ __host__ __forceinline__ void triangleForces(
 	f2 += v2 * m * invdt;
 }
 
-// find baricentric coordinates of the collision
+// find barycentric coordinates and time (0.0 to 1.0) of the collision
 // if at least one of the value returned negative there was no collision
-__device__ __forceinline__ float3 intersectParticleTriangleBaricentric(
+__device__ __forceinline__ float4 intersectParticleTriangleBarycentric(
 		Particle v0, Particle v1, Particle v2, float3 n,
 		Particle p, float dt, float threshold, float& oldSign)
 {
@@ -99,7 +106,7 @@ __device__ __forceinline__ float3 intersectParticleTriangleBaricentric(
 
 		float3 x  = p .r - p .u*t;
 
-		return dot(cross(r1-r0, r2-r0), x - r0);
+		return dot( normalize(cross(r1-r0, r2-r0)), x - r0);
 	};
 
 	float vold = F(0.0f);
@@ -107,13 +114,13 @@ __device__ __forceinline__ float3 intersectParticleTriangleBaricentric(
 	oldSign = vold;
 
 	int trid = (blockIdx.x * blockDim.x + threadIdx.x) / warpSize;
-	if (p.i1 == 20886 && trid == 184)
-		printf(" %d with tr %d: %f  ->  %f\n",
-					p.i1, trid, vold, vnew);
+//	if (p.i1 == 20886 && trid == 184)
+//		printf(" %d with tr %d: %f  ->  %f\n",
+//					p.i1, trid, vold, vnew);
 
 	// Particle is far and not crossing
-	// No need for baricentric coordinates, skip it
-	if ( fabs(vnew) > threshold && vold*vnew >= 0.0f ) return make_float3(-1000.0f);
+	// No need for barycentric coordinates, skip it
+	if ( fabs(vnew) > threshold && vold*vnew >= 0.0f ) return make_float4(-1000.0f, -1000.0f, -1000.0f, 1000.0f);
 
 	const float3 C = 0.333333333f * (v0.r+v1.r+v2.r);
 
@@ -130,7 +137,7 @@ __device__ __forceinline__ float3 intersectParticleTriangleBaricentric(
 
 		// This should NEVER happen
 		if (alpha < -0.1f)
-			printf("Something awful happened with mesh bounce");
+			printf("Something awful happened with mesh bounce. F0 = %f,  F = %f\n", F(0.0f), F(1.0f));
 
 		// This is the collision point with the MOVING triangle
 		projected = p.r - p.u*(1.0f - alpha)*dt;
@@ -142,17 +149,17 @@ __device__ __forceinline__ float3 intersectParticleTriangleBaricentric(
 	float3 r1 = v1.r - v1.u*t;
 	float3 r2 = v2.r - v2.u*t;
 
-	float3 baricentric = computeBaricentric(r0, r1, r2, projected);
+	float3 barycentric = computeBarycentric(r0, r1, r2, projected);
 
-	if (p.i1 == 20886)
-		printf(" %d with tr %d: %f  ->  %f , dist to center: %f. T = %f,  bar_coo = %f %f %f,  real coo  %f %f %f\n",
-				p.i1, (blockIdx.x * blockDim.x + threadIdx.x) / warpSize,
-				vold, vnew, sqrtf(dot(p.r - C, p.r - C)), alpha, baricentric.x, baricentric.y, baricentric.z,
-				p.r.x, p.r.y, p.r.z);
+//	if (p.i1 == 38669 )//&& trid == 153)
+//		printf(" %d with tr %d: %f  ->  %f , dist to center: %f. T = %f,  bar_coo = %f %f %f,  real coo  %f %f %f\n",
+//				p.i1, (blockIdx.x * blockDim.x + threadIdx.x) / warpSize,
+//				vold, vnew, sqrtf(dot(p.r - C, p.r - C)), alpha, barycentric.x, barycentric.y, barycentric.z,
+//				p.r.x, p.r.y, p.r.z);
 
-	// Return the baricentric coordinates of the projected position
+	// Return the barycentric coordinates of the projected position
 	// on the corresponding moving triangle
-	return baricentric;
+	return make_float4(barycentric, alpha);
 }
 
 
@@ -161,7 +168,8 @@ __device__ __forceinline__ void bounceParticleArray(
 		int* validParticles, int nParticles,
 		Particle v0, Particle v1, Particle v2,
 		float3& f0, float3& f1, float3& f2,
-		float4* coosvels, float particleMass, float vertexMass, const float dt)
+		const float4* coosvels, int* collTime, float4* coosvels_new,
+		float particleMass, float vertexMass, const float dt)
 {
 	const int tid = threadIdx.x % warpSize;
 	const float threshold = 2e-6f;
@@ -174,30 +182,39 @@ __device__ __forceinline__ void bounceParticleArray(
 		const int pid = validParticles[i];
 		Particle p(coosvels[2*pid], coosvels[2*pid+1]);
 
-		if (p.i1 == 20886 && (blockIdx.x * blockDim.x + threadIdx.x) / warpSize == 184)
-			printf(" YYYYYYYYYYYAAAAAAAAAAAAAAAAAAAAAAAYYYYYY  pid %d\n", pid);
-
 		float oldSign;
-		float3 baricentricCoo = intersectParticleTriangleBaricentric(v0, v1, v2, n, p, dt, threshold, oldSign);
-		if (baricentricCoo.x >= 0.0f && baricentricCoo.y >= 0.0f && baricentricCoo.z >= 0.0f)
-		{
-			// A collision is found here
-			const float3 vtri = baricentricCoo.x*v0.u + baricentricCoo.y*v1.u + baricentricCoo.z*v2.u;
-			const float3 coo  = baricentricCoo.x*v0.r + baricentricCoo.y*v1.r + baricentricCoo.z*v2.r;
+		float4 res = intersectParticleTriangleBarycentric(v0, v1, v2, n, p, dt, threshold, oldSign);
+		float3 barycentricCoo = make_float3(res.x, res.y, res.z);
+		float alpha = res.w;
 
-			triangleForces(v0.r, v1.r, v2.r, vertexMass, baricentricCoo, p.u - vtri, particleMass, dt, f0, f1, f2);
+		if (barycentricCoo.x >= 0.0f && barycentricCoo.y >= 0.0f && barycentricCoo.z >= 0.0f)
+		{
+			float alphaMin = __int_as_float( atomicMin(collTime+pid, __float_as_int(alpha)) );
+
+			// A collision is found here
+			const float3 vtri = barycentricCoo.x*v0.u + barycentricCoo.y*v1.u + barycentricCoo.z*v2.u;
+			const float3 coo  = barycentricCoo.x*v0.r + barycentricCoo.y*v1.r + barycentricCoo.z*v2.r;
+
+			triangleForces(v0.r, v1.r, v2.r, vertexMass, barycentricCoo, p.u - vtri, particleMass, dt, f0, f1, f2);
 
 			float3 old = p.r;
 
 			float3 newV = 2.0f*vtri-p.u;
-			p.r = coo + threshold * n * ((oldSign > 0) ? 2.0f : -2.0f);
+			p.r = coo + threshold * n * ((oldSign > 0) ? 5.0f : -5.0f);
 			p.u = newV;
 
-			if (p.i1 == 20886)
-				printf(" Moving by %d %d!! %f %f %f  ->  %f %f %f\n", blockIdx.x, threadIdx.x, old.x, old.y, old.z, p.r.x, p.r.y, p.r.z);
+//			if (p.i1 == 39464)
+//				printf("BOUNCED from %d!! %f %f %f  ->  %f %f %f  (corr to %f). time %f, mintime %f\n",
+//						(blockIdx.x * blockDim.x + threadIdx.x) / warpSize,
+//						old.x, old.y, old.z, p.r.x, p.r.y, p.r.z,
+//						dot(cross(v1.r-v0.r, v2.r-v0.r), p.r - v0.r),
+//						alpha, alphaMin);
 
-			coosvels[2*pid]   = Float3_int(p.r, p.i1).toFloat4();
-			coosvels[2*pid+1] = Float3_int(p.u, p.i2).toFloat4();
+			if (alpha < alphaMin)
+			{
+				coosvels_new[2*pid]   = Float3_int(p.r, p.i1).toFloat4();
+				coosvels_new[2*pid+1] = Float3_int(p.u, p.i2).toFloat4();
+			}
 		}
 	}
 }
@@ -223,14 +240,14 @@ __device__ inline bool isCellCrossingTriangle(float3 cornerCoo, float3 len, floa
 	return (pos != 8 && neg != 8);
 }
 
-//__launch_bounds__(128, 7)
+//__launch_bounds__(128, 6)
 __global__ void bounceMesh(
-		float4* coosvels, const uint* __restrict__ cellsStartSize, CellListInfo cinfo,
+		const float4* coosvels, const uint* __restrict__ cellsStartSize, CellListInfo cinfo, int* collTime, float4* coosvels_new,
 		const int nObj, const int nvertices, const int ntriangles, const int3* __restrict__ triangles, float4* objCoosvels, float* objForces,
 		float particleMass, float vertexMass, const float dt)
 {
 	// About maximum distance a particle can cover in one step
-	const float tol = 0.25f;
+	const float tol = 0.1f;
 
 	// One warp per triangle
 	const int gid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -260,15 +277,8 @@ __global__ void bounceMesh(
 	float3 lo = fminf(v0.r, fminf(v1.r, v2.r));
 	float3 hi = fmaxf(v0.r, fmaxf(v1.r, v2.r));
 
-	if (tid == 0 && trid == 184)
-		printf("   %f %f %f - %f %f %f\n", lo.x, lo.y, lo.z, hi.x, hi.y, hi.z);
-
-
 	const int3 cidLow  = cinfo.getCellIdAlongAxis(lo - tol);
 	const int3 cidHigh = cinfo.getCellIdAlongAxis(hi + tol);
-
-	if (tid == 0 && trid == 184)
-		printf("   %d %d %d - %d %d %d\n", cidLow.x, cidLow.y, cidLow.z, cidHigh.x, cidHigh.y, cidHigh.z);
 
 	const int3 span = cidHigh - cidLow + make_int3(1,1,1);
 	const int totCells = span.x * span.y * span.z;
@@ -288,6 +298,7 @@ __global__ void bounceMesh(
 			}
 		}
 
+
 	// Second step. Make particle queue and process it
 	const int nCells = *count;
 	*count = 0;
@@ -295,55 +306,49 @@ __global__ void bounceMesh(
 	float3 f0 = make_float3(0.0f), f1 = make_float3(0.0f), f2 = make_float3(0.0f);
 	for (int i=tid; i<nCells+warpSize; i+=warpSize)
 	{
-		if (i < nCells)
+		if (i < -nCells)
 		{
 			int2 start_size = cinfo.decodeStartSize(cellsStartSize[validCells[i]]);
 
 			int id = atomicAdd(count, start_size.y);
 			for (int j = 0; j < start_size.y; j++)
-			{
 				validParticles[id + j] = j + start_size.x;
-				Particle p(coosvels, j + start_size.x);
-				if (trid == 184 && p.i1 == 20886)
-					printf("  %d  with  %d\n", trid, j + start_size.x);
-			}
 		}
-
-		if (trid == 184)
-			printf("  COUNT %d, %d\n", tid, *count);
 
 		if (*count >= particleBufSize/2)
 		{
-			bounceParticleArray(validParticles, particleBufSize/2, v0, v1, v2, f0, f1, f2, coosvels, particleMass, vertexMass, dt);
+			bounceParticleArray(validParticles, particleBufSize/2, v0, v1, v2, f0, f1, f2,
+					coosvels, collTime, coosvels_new, particleMass, vertexMass, dt);
 			*count -= particleBufSize/2;
-
-			if (trid == 184)
-				printf("  COUNT %d, %d\n", tid, *count);
 
 			for (int i=tid; i<particleBufSize/2; i+=warpSize)
 				validParticles[i] = validParticles[i + particleBufSize/2];
 		}
 	}
 
-
 	// Process remaining
-	bounceParticleArray(validParticles, *count, v0, v1, v2, f0, f1, f2, coosvels, particleMass, vertexMass, dt);
+	if (*count > 0)
+	bounceParticleArray(validParticles, *count, v0, v1, v2, f0, f1, f2,
+			coosvels, collTime, coosvels_new, particleMass, vertexMass, dt);
 
 	auto plus = [] (float a, float b) {return a+b;};
 	warpReduce(f0, plus);
 	warpReduce(f1, plus);
 	warpReduce(f2, plus);
 
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.x),   f0.x);
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.x)+1, f0.y);
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.x)+2, f0.z);
+	if (tid == 0)
+	{
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.x),   f0.x);
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.x)+1, f0.y);
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.x)+2, f0.z);
 
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.y),   f1.x);
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.y)+1, f1.y);
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.y)+2, f1.z);
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.y),   f1.x);
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.y)+1, f1.y);
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.y)+2, f1.z);
 
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.z),   f2.x);
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.z)+1, f2.y);
-	atomicAdd(objForces + 3*(nvertices*objId + triangle.z)+2, f2.z);
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.z),   f2.x);
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.z)+1, f2.y);
+		atomicAdd(objForces + 3*(nvertices*objId + triangle.z)+2, f2.z);
+	}
 }
 
