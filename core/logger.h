f51869fdff5aa0274f66a4254e1a17f9dbc25955
Dmitry Alexeev <dimaleks@gmail.com>
2017-10-08 17:29:00 +0200

    compile-time log level

--- core/logger.h
+++ core/logger.h
@@ -22,13 +22,14 @@
 #include <cuda.h>
 #endif
 
-#ifndef DEBUGLVL
-#define DEBUGLVL 9
+#ifndef COMPILE_DEBUG_LVL
+#define COMPILE_DEBUG_LVL 10
 #endif
 
 class Logger
 {
 	int runtimeDebugLvl;
+	const int flushThreshold = 7;
 
 	FILE* fout = nullptr;
 	int rank;
@@ -38,8 +39,14 @@ class Logger
 	template<int level, class ... Args>
 	inline void log(const char* fname, const int lnum, const char* pattern, Args... args)
 	{
-		if (level <= DEBUGLVL && level <= runtimeDebugLvl)
+		if (level <= runtimeDebugLvl)
 		{
+			if (fout == nullptr)
+			{
+				fprintf(stderr, "Logger file is not set\n");
+				exit(1);
+			}
+
 			using namespace std::chrono;
 
 			auto now   = system_clock::now();
@@ -56,7 +63,7 @@ class Logger
 			FILE* ftmp = (fout != nullptr) ? fout : stdout;
 			fprintf(ftmp, intro.c_str(), rank, (cappedLvl >= 0 ? lvl2text[cappedLvl] : "").c_str(), args...);
 
-			if (runtimeDebugLvl >= 7)
+			if (runtimeDebugLvl >= flushThreshold && COMPILE_DEBUG_LVL >= flushThreshold)
 				fflush(fout);
 		}
 	}
@@ -77,8 +84,6 @@ public:
 
 	void init(MPI_Comm&& comm, const std::string fname, int debugLvl = 3)
 	{
-		runtimeDebugLvl = debugLvl;
-
 		MPI_Comm_rank(comm, &rank);
 		std::string rankStr = std::string(5 - std::to_string(rank).length(), '0') + std::to_string(rank);
 
@@ -87,20 +92,23 @@ public:
 		auto end = fname.substr(pos);
 
 		fout = fopen( (start+"_"+rankStr+end).c_str(), "w");
+
+		setDebugLvl(debugLvl);
 	}
 
 	void init(MPI_Comm&& comm, FILE* fout, int debugLvl = 3)
 	{
-		runtimeDebugLvl = debugLvl;
 		MPI_Comm_rank(comm, &rank);
 		this->fout = fout;
+
+		setDebugLvl(debugLvl);
 	}
 
 
 	template<class ... Args>
 	inline void _say(Args ... args)
 	{
-		log<-1>(args...);
+		log<-10>(args...);
 	}
 
 	template<class ... Args>
@@ -167,9 +175,16 @@ public:
 		}
 	}
 
-	int& debugLvl()
+	int getDebugLvl()
 	{
 		return runtimeDebugLvl;
+	}
+
+	void setDebugLvl(int debugLvl)
+	{
+		runtimeDebugLvl = max(min(debugLvl, COMPILE_DEBUG_LVL), 0);
+		_say(__FILE__, __LINE__, "Compiled with maximum debug level %d", COMPILE_DEBUG_LVL);
+		_say(__FILE__, __LINE__, "Debug level requested %d, set to %d", debugLvl, runtimeDebugLvl);
 	}
 
 
@@ -185,13 +200,48 @@ public:
 extern Logger logger;
 
 #define   say(...)  logger._say      (__FILE__, __LINE__, ##__VA_ARGS__)
+
+#if COMPILE_DEBUG_LVL >= 0
 #define   die(...)  logger._die      (__FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define   die(...)  do { } while(0)
+#endif
+
+#if COMPILE_DEBUG_LVL >= 1
 #define error(...)  logger._error    (__FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define error(...)  do { } while(0)
+#endif
+
+#if COMPILE_DEBUG_LVL >= 2
 #define  warn(...)  logger._warn     (__FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define  warn(...)  do { } while(0)
+#endif
+
+#if COMPILE_DEBUG_LVL >= 3
 #define  info(...)  logger._info     (__FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define  info(...)  do { } while(0)
+#endif
+
+#if COMPILE_DEBUG_LVL >= 4
 #define debug(...)  logger._debug    (__FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define debug(...)  do { } while(0)
+#endif
+
+#if COMPILE_DEBUG_LVL >= 5
 #define debug2(...) logger._debugX<1>(__FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define debug2(...)  do { } while(0)
+#endif
+
+#if COMPILE_DEBUG_LVL >= 5
 #define debug3(...) logger._debugX<2>(__FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define debug3(...)  do { } while(0)
+#endif
 
 #define  MPI_Check(command) logger._MPI_Check (__FILE__, __LINE__, command)
 #define CUDA_Check(command) logger._CUDA_Check(__FILE__, __LINE__, command)
