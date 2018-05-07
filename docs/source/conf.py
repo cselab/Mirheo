7631d151f451c155c807b3d1b9e28ff724a4714f
Dmitry <dimaleks@gmail.com>
2018-05-07 16:47:10 +0200

    still trying

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,21 +3,23 @@
 import os
 import subprocess
 
-html_static_path = ['_static/']
+extensions = ['breathe', 'sphinx.ext.mathjax']
 
-def setup(app):
-    app.add_stylesheet('css/custom.css')
+#def setup(app):
+#    app.add_stylesheet('css/custom.css')
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if on_rtd:
     subprocess.call('cd ..; doxygen', shell=True)
-
+    
+   
+html_static_path = ['_static/']
+html_theme_path = ["_themes",]
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-extensions = ['breathe', 'sphinx.ext.mathjax']
 breathe_projects = { 'uDeviceX': '../xml' }
 breathe_default_project = 'uDeviceX'
 breathe_domain_by_extension = { "h" : "cpp", "cu" : "cpp" }
@@ -27,13 +29,13 @@ source_suffix = '.rst'
 master_doc = 'index'
 project = 'uDeviceX'
 copyright = 'ETH Zurich'
-author = ''
+author = 'Dmitry Alexeev'
 
 exclude_patterns = []
 highlight_language = 'cuda'
 cpp_id_attributes = ['__device__', '__global__', '__host__']
 cpp_paren_attributes = ['__launch_bounds__', '__align__']
-#pygments_style = 'sphinx'
+pygments_style = 'sphinx'
 todo_include_todos = False
 htmlhelp_basename = 'uDeviceX'
 
