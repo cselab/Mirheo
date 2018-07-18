5d5219e7e61f8a06b839f77db80f62d386504603
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-07-18 15:11:33 +0200

    tests/units

--- src/py/test.py
+++ src/py/test.py
@@ -7,17 +7,17 @@ Created on Fri Jul  6 12:35:18 2018
 """
 
 import sys
-sys.path.append('/home/alexeedm/udevicex/build')
+sys.path.append('/home/amlucas/dimudx/build')
 import _udevicex as udx
 import numpy as np
 
 dt = 0.001
 
-u = udx.udevicex((1,1,1), (2, 2, 2), debug_level=50, log_filename='log')
+u = udx.udevicex((1,1,1), (32, 32, 32), debug_level=50, log_filename='log')
 
 if u.isComputeTask():
     pv = udx.ParticleVectors.ParticleVector('pv', 1)
-    ic = udx.InitialConditions.Uniform(density=2)
+    ic = udx.InitialConditions.Uniform(density=10)
     u.registerParticleVector(pv=pv, ic=ic)
     
     dpd = udx.Interactions.DPD('dpd', 1.0, a=10.0, gamma=10.0, kbt=1.0, dt=dt, power=0.5)
@@ -29,7 +29,7 @@ if u.isComputeTask():
     u.setIntegrator(vv, pv)
 
 flag = u.isComputeTask()
-stats = udx.Plugins.createStats('smth', 10, flag)
+stats = udx.Plugins.createStats('smth', 1000, flag)
 u.registerPlugins(stats[0], stats[1])
 
-u.run(32)
+u.run(3000)
