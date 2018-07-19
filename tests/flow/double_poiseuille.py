94c13a15a0362bf9559ed3e524dba30b620f58e9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-07-19 08:32:02 +0200

    no hardcoded path in test scripts

--- tests/flow/double_poiseuille.py
+++ tests/flow/double_poiseuille.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
-sys.path.append('/home/amlucas/dimudx/build')
 import _udevicex as udx
 import numpy as np
 
 dt = 0.001
 
 ranks  = (1, 1, 1)
-domain = (8, 8, 8)
+domain = (8, 16, 8)
+a = 0.1
 
 u = udx.udevicex(ranks, domain, debug_level=2, log_filename='log')
 
@@ -21,23 +21,23 @@ if u.isComputeTask():
     u.registerInteraction(dpd)
     u.setInteraction(dpd, pv, pv)
     
-    vv = udx.Integrators.VelocityVerlet('vv', dt=dt)
+    vv = udx.Integrators.VelocityVerlet_withPeriodicForce('vv', dt=dt, force=a, direction='x')
     u.registerIntegrator(vv)
     u.setIntegrator(vv, pv)
 
 flag = u.isComputeTask()
-stats = udx.Plugins.createStats('diagnostics', 100, flag)
+stats = udx.Plugins.createStats('diagnostics', 1000, flag)
 
 sampleEvery = 2
 dumpEvery   = 1000
 binSize     = (1., 1., 1.)
 
-field = udx.Plugins.createDumpAveragePlugin('field', pv, sampleEvery, dumpEvery,
-                                            binSize,
-                                            [('velocity', 'vector')],
-                                            './h5', flag)
+# field = udx.Plugins.createDumpAveragePlugin('field', pv, sampleEvery, dumpEvery,
+#                                             binSize,
+#                                             [('velocity', 'vector_from_float8']],
+#                                             './h5', flag)
 u.registerPlugins(stats[0], stats[1])
-u.registerPlugins(field[0], field[1])
+#u.registerPlugins(field[0], field[1])
 
 u.run(2000)
 
--- tests/flow/equilibrium.py
+++ tests/flow/equilibrium.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-sys.path.append('/home/amlucas/dimudx/build')
 import _udevicex as udx
 import numpy as np
 
