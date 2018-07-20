d3c04e99eed39cb1ec4778bcf94bb4ba5e8de660
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-07-20 16:47:41 +0200

    adapt scipts in flow

--- tests/flow/double_poiseuille.py
+++ tests/flow/double_poiseuille.py
@@ -10,29 +10,27 @@ ranks  = (1, 1, 1)
 domain = (8, 16, 8)
 a = 0.1
 
-u = udx.udevicex(ranks, domain, debug_level=2, log_filename='log')
+u = udx.initialize(ranks, domain, debug_level=2, log_filename='log')
 
-if u.isComputeTask():
-    pv = udx.ParticleVectors.ParticleVector('pv', mass = 1)
-    ic = udx.InitialConditions.Uniform(density=4)
-    u.registerParticleVector(pv=pv, ic=ic)
+pv = udx.ParticleVectors.ParticleVector('pv', mass = 1)
+ic = udx.InitialConditions.Uniform(density=4)
+u.registerParticleVector(pv=pv, ic=ic)
     
-    dpd = udx.Interactions.DPD('dpd', 1.0, a=10.0, gamma=10.0, kbt=1.0, dt=dt, power=0.5)
-    u.registerInteraction(dpd)
-    u.setInteraction(dpd, pv, pv)
-    
-    vv = udx.Integrators.VelocityVerlet_withPeriodicForce('vv', dt=dt, force=a, direction='x')
-    u.registerIntegrator(vv)
-    u.setIntegrator(vv, pv)
+dpd = udx.Interactions.DPD('dpd', 1.0, a=10.0, gamma=10.0, kbt=1.0, dt=dt, power=0.5)
+u.registerInteraction(dpd)
+u.setInteraction(dpd, pv, pv)
 
-flag = u.isComputeTask()
-stats = udx.Plugins.createStats('diagnostics', 1000, flag)
+vv = udx.Integrators.VelocityVerlet_withPeriodicForce('vv', dt=dt, force=a, direction='x')
+u.registerIntegrator(vv)
+u.setIntegrator(vv, pv)
+
+stats = udx.Plugins.createStats('diagnostics', 1000)
 
 sampleEvery = 2
 dumpEvery   = 1000
 binSize     = (1., 1., 1.)
 
-field = udx.Plugins.createDumpAverage('field', pv, sampleEvery, dumpEvery, binSize, [("velocity", "vector_from_float8")], './h5', flag)
+field = udx.Plugins.createDumpAverage('field', pv, sampleEvery, dumpEvery, binSize, [("velocity", "vector_from_float8")], './h5/')
 u.registerPlugins(stats[0], stats[1])
 u.registerPlugins(field[0], field[1])
 
--- tests/flow/equilibrium.py
+++ tests/flow/equilibrium.py
@@ -9,23 +9,21 @@ dt = 0.001
 ranks  = (1, 1, 1)
 domain = (8, 8, 8)
 
-u = udx.udevicex(ranks, domain, debug_level=2, log_filename='log')
+u = udx.initialize(ranks, domain, debug_level=2, log_filename='log')
 
-if u.isComputeTask():
-    pv = udx.ParticleVectors.ParticleVector('pv', mass = 1)
-    ic = udx.InitialConditions.Uniform(density=4)
-    u.registerParticleVector(pv=pv, ic=ic)
-    
-    dpd = udx.Interactions.DPD('dpd', 1.0, a=10.0, gamma=10.0, kbt=1.0, dt=dt, power=0.5)
-    u.registerInteraction(dpd)
-    u.setInteraction(dpd, pv, pv)
-    
-    vv = udx.Integrators.VelocityVerlet('vv', dt=dt)
-    u.registerIntegrator(vv)
-    u.setIntegrator(vv, pv)
+pv = udx.ParticleVectors.ParticleVector('pv', mass = 1)
+ic = udx.InitialConditions.Uniform(density=4)
+u.registerParticleVector(pv=pv, ic=ic)
 
-flag = u.isComputeTask()
-stats = udx.Plugins.createStats('diagnostics', 100, flag)
+dpd = udx.Interactions.DPD('dpd', 1.0, a=10.0, gamma=10.0, kbt=1.0, dt=dt, power=0.5)
+u.registerInteraction(dpd)
+u.setInteraction(dpd, pv, pv)
+
+vv = udx.Integrators.VelocityVerlet('vv', dt=dt)
+u.registerIntegrator(vv)
+u.setIntegrator(vv, pv)
+
+stats = udx.Plugins.createStats('diagnostics', 100)
 u.registerPlugins(stats[0], stats[1])
 
 u.run(2000)
