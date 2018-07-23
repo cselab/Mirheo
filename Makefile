c070de6876ea0ffa6ee8987fbfbe3ee99b6a56af
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-07-23 10:30:51 +0200

    wip: quick build for tests

--- Makefile
+++ Makefile
@@ -1,5 +1,18 @@
+cmakecache=build/CMakeCache.txt
+
+build: $(cmakecache)
+	(cd build; make -j)
+
+$(cmakecache):
+	mkdir -p build
+	(cd build; cmake ../)
+
 install:
 	pip3 install . --user --upgrade
 
 test:
 	(cd tests; make test)
+
+clean:; rm -rf build
+
+.PHONY: install build test clean
---
+++ tests/context.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+__quick_inst_rel_path = "../build"
+__quick_py_rel_path = "../src/py"
+__dir_path = os.path.dirname(__file__)
+__pyudx_path = os.path.join(__dir_path, __quick_py_rel_path)
+__soudx_path = os.path.join(__dir_path, __quick_inst_rel_path)
+sys.path.insert(0, os.path.abspath(__pyudx_path))
+sys.path.insert(0, os.path.abspath(__soudx_path))
+
+import __init__ as udevicex
