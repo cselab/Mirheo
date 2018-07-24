d7220fe4913772f4357afb2b04d94938460c422b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-07-24 15:38:40 +0200

    adapt rest test

--- tests/flow/rest.py
+++ tests/flow/rest.py
@@ -9,7 +9,7 @@ dt = 0.001
 ranks  = (1, 1, 1)
 domain = (12, 8, 10)
 
-u = udx.initialize(ranks, domain, debug_level=4, log_filename='log')
+u = udx.udevicex(ranks, domain, debug_level=4, log_filename='log')
 
 pv = udx.ParticleVectors.ParticleVector('pv', mass = 1)
 ic = udx.InitialConditions.Uniform(density=2)
@@ -24,12 +24,12 @@ u.registerIntegrator(vv)
 u.setIntegrator(vv, pv)
 
 stats = udx.Plugins.createStats('stats', "stats.txt", 1000)
-u.registerPlugins(stats[0], stats[1])
+u.registerPlugins(stats)
 
 u.run(5001)
 
 # nTEST: flow.rest
 # cd flow
 # udx.run -n 2 ./rest.py > /dev/null
-# cat stats.txt | awk '{print $1, $2, $3, $4, $5}' | uscale 10 > stats.out.txt
+# cat stats.txt | awk '{print $1, $2, $3, $4, $5}' > stats.out.txt
 
