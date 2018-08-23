73997128854f5661adaf4b2831f52d9c3ba5a9b5
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-08-23 15:55:25 +0200

    adding plugin for particle recoloring between 2 pvs, crossing a plane

---
+++ src/plugins/exchange_pvs_flux_plane.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <plugins/interface.h>
+#include <core/containers.h>
+#include <vector>
+#include <string>
+
+class ParticleVector;
+
+class ExchangePVSFluxPlanePlugin : public SimulationPlugin
+{
+public:
+    ExchangePVSFluxPlanePlugin(std::string name, std::string pv1Name, std::string pv2Name, float4 plane);
+
+    void setup(Simulation* sim, const MPI_Comm& comm, const MPI_Comm& interComm) override;
+    void beforeParticleDistribution(cudaStream_t stream) override;
+
+    bool needPostproc() override { return false; }
+
+private:
+    std::string pv1Name, pv2Name;
+    ParticleVector *pv1, *pv2;
+    float4 plane;
+
+    PinnedBuffer<int> numberCrossedParticles;
+};
+
