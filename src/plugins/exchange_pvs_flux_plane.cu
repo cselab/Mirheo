2e0c34df69ea8bcbb7e0e150472cc6f77db7e20e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-08-23 18:05:08 +0200

    plane is in global coordinates

--- src/plugins/exchange_pvs_flux_plane.cu
+++ src/plugins/exchange_pvs_flux_plane.cu
@@ -9,24 +9,25 @@
 
 namespace exchange_pvs_flux_plane_kernels {
 
-__device__ inline bool hasCrossedPlane(float3 pos, float4 plane)
+__device__ inline bool hasCrossedPlane(DomainInfo domain, float3 pos, float4 plane)
 {
+    pos = domain.local2global(pos);
     return plane.x * pos.x + plane.y * pos.y + plane.z * pos.z + plane.w >= 0.f;
 }
 
 // TODO use cell lists
-__global__ void countParticles(PVview view1, float4 plane, int *numberCrossed)
+__global__ void countParticles(DomainInfo domain, PVview view1, float4 plane, int *numberCrossed)
 {
     int pid = blockIdx.x * blockDim.x + threadIdx.x;
     if (pid >= view1.size) return;
 
     Particle p;
     p.readCoordinate(view1.particles, pid);
-    if (hasCrossedPlane(p.r, plane))
+    if (hasCrossedPlane(domain, p.r, plane))
         atomicAdd(numberCrossed, 1);
 }
 
-__global__ void moveParticles(PVview view1, PVview view2, float4 plane, int oldsize2, int *numberCrossed)
+__global__ void moveParticles(DomainInfo domain, PVview view1, PVview view2, float4 plane, int oldsize2, int *numberCrossed)
 {
     int pid = blockIdx.x * blockDim.x + threadIdx.x;
     if (pid >= view1.size) return;
@@ -34,7 +35,7 @@ __global__ void moveParticles(PVview view1, PVview view2, float4 plane, int olds
     Particle p;
     p.readCoordinate(view1.particles, pid);
 
-    if (hasCrossedPlane(p.r, plane)) {
+    if (hasCrossedPlane(domain, p.r, plane)) {
         p.readVelocity(view1.particles, pid);
         int dst = atomicAdd(numberCrossed, 1);
         dst += oldsize2;
@@ -64,6 +65,7 @@ void ExchangePVSFluxPlanePlugin::setup(Simulation* sim, const MPI_Comm& comm, co
 
 void ExchangePVSFluxPlanePlugin::beforeParticleDistribution(cudaStream_t stream)
 {
+    DomainInfo domain = pv1->domain;
     PVview view1(pv1, pv1->local());
     PVview view2(pv2, pv2->local());
     const int nthreads = 128;
@@ -73,7 +75,7 @@ void ExchangePVSFluxPlanePlugin::beforeParticleDistribution(cudaStream_t stream)
     SAFE_KERNEL_LAUNCH(
             exchange_pvs_flux_plane_kernels::countParticles,
             getNblocks(view1.size, nthreads), nthreads, 0, stream,
-            view1, plane, numberCrossedParticles.devPtr() );
+            domain, view1, plane, numberCrossedParticles.devPtr() );
 
     numberCrossedParticles.downloadFromDevice(stream, ContainersSynch::Synch);
 
@@ -88,7 +90,7 @@ void ExchangePVSFluxPlanePlugin::beforeParticleDistribution(cudaStream_t stream)
     SAFE_KERNEL_LAUNCH(
                        exchange_pvs_flux_plane_kernels::moveParticles,
                        getNblocks(view1.size, nthreads), nthreads, 0, stream,
-                       view1, view2, plane, old_size2, numberCrossedParticles.devPtr() );
+                       domain, view1, view2, plane, old_size2, numberCrossedParticles.devPtr() );
 
 }
 
