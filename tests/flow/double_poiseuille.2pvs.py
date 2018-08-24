1b84cdcc3073208908eaae13b3990eea20ccd4ce
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-08-24 11:48:12 +0200

    activate test

--- tests/flow/double_poiseuille.2pvs.py
+++ tests/flow/double_poiseuille.2pvs.py
@@ -37,7 +37,7 @@ u.registerPlugins(field)
 
 u.run(5002)
 
-# cTEST: double_poiseuille.2pvs
+# nTEST: double_poiseuille.2pvs
 # cd flow
 # rm -rf h5
 # udx.run -n 2 ./double_poiseuille.2pvs.py > /dev/null
