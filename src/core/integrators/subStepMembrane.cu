8b42acae7a3d5f6e89e3a365c0cb20ec7f340590
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-08-27 17:51:23 +0200

    adding substep integration for membranes

---
+++ src/core/integrators/subStepMembrane.cu
@@ -0,0 +1,41 @@
+#include "subStepMembrane.h"
+
+#include <core/utils/kernel_launch.h>
+#include <core/logger.h>
+#include <core/pvs/particle_vector.h>
+#include <core/interactions/membrane.h>
+
+
+IntegratorSubStepMembrane::IntegratorSubStepMembrane(std::string name, float dt, int substeps, Interaction *fastForces) :
+    Integrator(name, dt), substeps(substeps),
+    subIntegrator(new IntegratorVV<Forcing_None>(name + "_sub", dt/substeps, Forcing_None()))
+{
+    if ( !(this->fastForces = dynamic_cast<InteractionMembrane*>(fastForces)) )
+        die("IntegratorSubStepMembrane expects an interaction of type <InteractionMembrane>.");
+}
+    
+
+void IntegratorSubStepMembrane::stage1(ParticleVector *pv, float t, cudaStream_t stream)
+{}
+
+void IntegratorSubStepMembrane::stage2(ParticleVector *pv, float t, cudaStream_t stream)
+{
+    // save "slow forces"
+    slowForces.copy(pv->local()->forces, stream);
+
+    // advance with internal vv integrator
+    for (int substep = 0; substep < substeps; ++ substep) {
+
+        if (substep != 0)
+            pv->local()->forces.copy(slowForces, stream);
+
+        fastForces->regular(pv, pv, nullptr, nullptr, t + substep * dt / substeps, stream);
+
+        subIntegrator->stage2(pv, t, stream);
+    }
+    
+    // PV may have changed, invalidate all
+    pv->haloValid = false;
+    pv->redistValid = false;
+    pv->cellListStamp++;
+}
---
+++ src/core/integrators/subStepMembrane.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "interface.h"
+
+#include <core/containers.h>
+#include <core/datatypes.h>
+
+#include "forcing_terms/none.h"
+#include "vv.h"
+
+class Interaction;
+class InteractionMembrane;
+
+class IntegratorSubStepMembrane : Integrator
+{
+public:
+    void stage1(ParticleVector *pv, float t, cudaStream_t stream) override;
+    void stage2(ParticleVector *pv, float t, cudaStream_t stream) override;
+
+    IntegratorSubStepMembrane(std::string name, float dt, int substeps, Interaction *fastForces);
+
+    ~IntegratorSubStepMembrane();
+
+private:
+
+    InteractionMembrane *fastForces; /* interactions (self) called `substeps` times per time step */
+    int substeps; /* number of substeps */
+    DeviceBuffer<Force> slowForces;
+    std::unique_ptr<IntegratorVV<Forcing_None>> subIntegrator;
+};
