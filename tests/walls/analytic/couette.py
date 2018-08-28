fc397d0f2b0230e90355a49042fb957d83b25c86
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-08-28 11:42:38 +0200

    update TODO: add tests

---
+++ tests/walls/analytic/couette.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import udevicex as udx
+
+dt = 0.001
+
+ranks  = (1, 1, 1)
+domain = (8, 16, 8)
+force = (1.0, 0, 0)
+
+density = 8
+rc      = 1.0
+gdot    = 0.5
+tend    = 7.1
+
+u = udx.udevicex(ranks, domain, debug_level=2, log_filename='log')
+
+pv = udx.ParticleVectors.ParticleVector('pv', mass = 1)
+ic = udx.InitialConditions.Uniform(density=density)
+u.registerParticleVector(pv=pv, ic=ic)
+    
+dpd = udx.Interactions.DPD('dpd', rc=rc, a=10.0, gamma=50.0, kbt=0.01, dt=dt, power=0.125)
+u.registerInteraction(dpd)
+
+vx = gdot*(domain[2] - rc)
+print("vx =" , vx)
+plate_lo = udx.Walls.Plane      ("plate_lo", normal=(0, 0, -1), pointThrough=(0, 0,              rc))
+plate_hi = udx.Walls.MovingPlane("plate_hi", normal=(0, 0,  1), pointThrough=(0, 0,  domain[2] - rc), velocity=(vx, 0, 0))
+u.registerWall(plate_lo, 0)
+u.registerWall(plate_hi, 0)
+
+vv = udx.Integrators.VelocityVerlet("vv", dt)
+frozen_lo = u.makeFrozenWallParticles(wall=plate_lo, interaction=dpd, integrator=vv, density=density)
+frozen_hi = u.makeFrozenWallParticles(wall=plate_hi, interaction=dpd, integrator=vv, density=density)
+
+u.setWall(plate_lo, pv)
+u.setWall(plate_hi, pv)
+
+for p in (pv, frozen_lo, frozen_hi):
+    u.setInteraction(dpd, p, pv)
+
+u.registerIntegrator(vv)
+u.setIntegrator(vv, pv)
+
+sampleEvery = 2
+dumpEvery   = 1000
+binSize     = (1., 1., 1.)
+
+field = udx.Plugins.createDumpAverage('field', [pv], sampleEvery, dumpEvery, binSize, [("velocity", "vector_from_float8")], 'h5/solvent-')
+u.registerPlugins(field)
+
+u.run((int)(tend/dt))
+
+# sTEST: walls.analytic.couette
+# cd walls/analytic
+# rm -rf h5
+# udx.run --runargs "-n 2" ./couette.py
+# udx.avgh5 xy velocity h5/solvent-0001[4-7].h5 | awk '{print $1}' > profile.out.txt
