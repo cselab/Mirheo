c099419d6eec9573b9068f267c15f56b2e4108b2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-08-30 15:24:42 +0200

    add test for new mesh constructor

--- tests/dump/mesh.py
+++ tests/dump/mesh.py
@@ -2,17 +2,28 @@
 
 import udevicex as udx
 import numpy as np
-import trimesh
+import trimesh, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--readFrom", choices=["off", 'trimesh'])
+args = parser.parse_args()
+
 
 path   = "ply/"
 pvname = "rbc"
+off    = "rbc_mesh.off"
 
 ranks  = (1, 1, 1)
 domain = (12, 8, 10)
 
 u = udx.udevicex(ranks, domain, debug_level=2, log_filename='log')
 
-mesh = udx.ParticleVectors.MembraneMesh("rbc_mesh.off")
+if args.readFrom == "off":
+    mesh = udx.ParticleVectors.MembraneMesh(off)
+elif args.readFrom == "trimesh":
+    m = trimesh.load(off);
+    mesh = udx.ParticleVectors.MembraneMesh(m.vertices.tolist(), m.faces.tolist())
+
 rbc  = udx.ParticleVectors.MembraneVector(pvname, mass=1.0, mesh=mesh)
 icrbc = udx.InitialConditions.Membrane([[6.0, 4.0, 5.0,   1.0, 0.0, 0.0, 0.0]])
 u.registerParticleVector(pv=rbc, ic=icrbc)
@@ -31,5 +42,12 @@ np.savetxt("faces.txt",    mesh.faces,    fmt="%d")
 # cd dump
 # rm -rf ply/ vertices.txt faces.txt mesh.out.txt 
 # cp ../../data/rbc_mesh.off .
-# udx.run --runargs "-n 2" ./mesh.py > /dev/null
+# udx.run --runargs "-n 2" ./mesh.py --readFrom off > /dev/null
+# cat vertices.txt faces.txt > mesh.out.txt
+
+# TEST: dump.mesh.fromTrimesh
+# cd dump
+# rm -rf ply/ vertices.txt faces.txt mesh.out.txt 
+# cp ../../data/rbc_mesh.off .
+# udx.run --runargs "-n 2" ./mesh.py --readFrom trimesh > /dev/null
 # cat vertices.txt faces.txt > mesh.out.txt
