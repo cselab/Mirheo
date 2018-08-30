6992d74e3e96a778e0d160a81d02affb7457e9b0
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-08-30 14:09:02 +0200

    use trimesh python module for tests

--- tests/dump/mesh.py
+++ tests/dump/mesh.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 import udevicex as udx
+import numpy as np
+import trimesh
+
+path   = "ply/"
+pvname = "rbc"
 
 ranks  = (1, 1, 1)
 domain = (12, 8, 10)
@@ -8,18 +13,23 @@ domain = (12, 8, 10)
 u = udx.udevicex(ranks, domain, debug_level=2, log_filename='log')
 
 mesh = udx.ParticleVectors.MembraneMesh("rbc_mesh.off")
-rbc  = udx.ParticleVectors.MembraneVector("rbc", mass=1.0, mesh=mesh)
+rbc  = udx.ParticleVectors.MembraneVector(pvname, mass=1.0, mesh=mesh)
 icrbc = udx.InitialConditions.Membrane([[6.0, 4.0, 5.0,   1.0, 0.0, 0.0, 0.0]])
 u.registerParticleVector(pv=rbc, ic=icrbc)
 
-mdump = udx.Plugins.createDumpMesh("mesh_dump", rbc, 1, "ply/")
+mdump = udx.Plugins.createDumpMesh("mesh_dump", rbc, 1, path)
 u.registerPlugins(mdump)
 
 u.run(3)
 
-# nTEST: dump.mesh
+mesh = trimesh.load(path + pvname + "_00000.ply")
+
+np.savetxt("vertices.txt", mesh.vertices, fmt="%g")
+np.savetxt("faces.txt",    mesh.faces,    fmt="%d")
+
+# TEST: dump.mesh
 # cd dump
-# rm -rf ply
+# rm -rf ply/ vertices.txt faces.txt mesh.out.txt 
 # cp ../../data/rbc_mesh.off .
 # udx.run --runargs "-n 2" ./mesh.py > /dev/null
-# ply2punto ply/rbc_00000.ply > ply.out.txt
+# cat vertices.txt faces.txt > mesh.out.txt
