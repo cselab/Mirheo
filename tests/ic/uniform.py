09646d0da7c256c232ee1895d2f41415c5829842
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-08-30 12:19:35 +0200

    add test for uniform ic

--- tests/ic/uniform.py
+++ tests/ic/uniform.py
@@ -4,17 +4,13 @@ import numpy as np
 import udevicex as udx
 
 ranks  = (1, 1, 1)
-domain = [4., 6., 8.]
+domain = [4., 2., 3.]
+density = 8
 
 u = udx.udevicex(ranks, tuple(domain), debug_level=2, log_filename='log')
 
 pv = udx.ParticleVectors.ParticleVector('pv', mass = 1)
-
-pos = [[a*domain[0], a*domain[1], a*domain[2]] for a in [0.1, 0.5, 0.8, 1.5]] # one particle is outside
-v=[1., 2., 3.]
-vel = [[a*v[0], a*v[1], a*v[2]] for a in [0.1, 0.5, 0.8, 1.5]]
-
-ic = udx.InitialConditions.FromArray(pos=pos, vel=vel)
+ic = udx.InitialConditions.Uniform(density=density)
 u.registerParticleVector(pv=pv, ic=ic)
 
 # xyz = udx.Plugins.createDumpXYZ('xyz', pv, 1, "xyz/")
@@ -29,8 +25,8 @@ if pv:
     np.savetxt("vel.ic.txt", icvel)
     
 
-# TEST: ic.fromArray
+# TEST: ic.uniform
 # cd ic
 # rm -rf pos*.txt vel*.txt
-# udx.run --runargs "-n 2" ./fromArray.py > /dev/null
+# udx.run --runargs "-n 2" ./uniform.py > /dev/null
 # paste pos.ic.txt vel.ic.txt | sort > ic.out.txt
