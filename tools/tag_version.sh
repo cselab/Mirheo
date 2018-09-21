c6ef072b19e8b6444f2296a3ea87a460c3f94969
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-09-21 15:16:38 +0200

    add tool for setting version

---
+++ tools/tag_version.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+usage() {
+    cat <<EOF
+wrapper to export tag to git for versioning udeviceX
+usage: 
+       ./tag_version.sh <VERSION>
+
+where VERSION has a format X.Y.Z
+      X: major version: increment after major feature changes
+      Y: minor version: increment after each new feature
+      Z: micro version: increment after bug fixes etc
+
+run 
+    git push origin master --tags
+
+to push all tags on the repo
+EOF
+    exit 1
+}
+
+if test $# -ne 0 && test "$1" = -h; then usage; fi
+
+VERSION=$1; shift
+
+git tag -a "v${VERSION}" -m "version v${VERSION}"
+
