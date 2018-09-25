5bf7405ef9c19ed441cd3642509d4750d58c97cf
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-09-25 15:43:30 +0200

    separate object file for folders

--- src/core/utils/folders.h
+++ src/core/utils/folders.h
@@ -2,61 +2,11 @@
 
 #include <string>
 #include <vector>
-#include <sstream>
-
-#include <core/logger.h>
 #include <mpi.h>
 
-static std::vector<std::string> splitByDelim(std::string str, char delim = ',')
-{
-    std::stringstream sstream(str);
-    std::string word;
-    std::vector<std::string> splitted;
+std::vector<std::string> splitByDelim(std::string str, char delim = ',');
 
-    while(std::getline(sstream, word, delim))
-    {
-        splitted.push_back(word);
-    }
+std::string parentPath(std::string path);
+std::string relativePath(std::string path);
 
-    return splitted;
-}
-
-static std::string parentPath(std::string path)
-{
-    return path.substr(0, path.find_last_of("/"));
-}
-
-static std::string relativePath(std::string path)
-{
-    auto pos = path.find_last_of("/");
-    if (pos == std::string::npos)
-        return path;
-    else
-        return path.substr(pos + 1);
-}
-
-static bool createFolders(std::string path)
-{
-    std::string command = "mkdir -p " + path;
-    if ( system(command.c_str()) != 0 )
-    {
-        error("Could not create folders or files by given path '%s'", path.c_str());
-        return false;
-    }
-
-    return true;
-}
-
-static bool createFoldersCollective(const MPI_Comm& comm, std::string path)
-{
-    bool res;
-    int rank;
-    MPI_Check( MPI_Comm_rank(comm, &rank) );
-
-    if (rank == 0)
-        res = createFolders(path);
-
-    MPI_Bcast(&res, 1, MPI_C_BOOL, 0, comm);
-
-    return res;
-}
+bool createFoldersCollective(const MPI_Comm& comm, std::string path);
