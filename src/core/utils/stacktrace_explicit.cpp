56c88d7ce017ac6d201230f9164dd09cb6570508
Dmitry <dimaleks@gmail.com>
2018-09-25 18:59:27 +0200

    added backward.hpp as a submodule, fixes segfault in python on die() call

--- src/core/utils/stacktrace_explicit.cpp
+++ src/core/utils/stacktrace_explicit.cpp
@@ -1,12 +1,14 @@
 #include "stacktrace_explicit.h"
-#include "stacktrace.h"
+
+#define BACKWARD_HAS_BFD 1
+#include <extern/backward-cpp/backward.hpp>
 
 void pretty_stacktrace(std::ostream& stream)
 {
     using namespace backward;
 
     StackTrace st;
-    st.load_here(32);
+    st.load_here(40);
     Printer p;
     p.object = true;
     p.color_mode = ColorMode::automatic;
