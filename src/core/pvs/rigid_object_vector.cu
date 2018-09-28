a5d06b5bc20132d4bc7a9589db54bf91a1a8a8b8
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-09-28 14:01:30 +0200

    forgot to shift motions coordinates

--- src/core/pvs/rigid_object_vector.cu
+++ src/core/pvs/rigid_object_vector.cu
@@ -147,12 +147,18 @@ void RigidObjectVector::_checkpointObjectData(MPI_Comm comm, std::string path)
     debug("Checkpoint for object vector '%s' successfully written", name.c_str());
 }
 
+static void shiftCoordinates(const DomainInfo& domain, std::vector<RigidMotion>& motions)
+{
+    for (auto& m : motions)
+        m.r = make_rigidReal3( domain.global2local(make_float3(m.r)) );
+}
+
 void RigidObjectVector::_restartObjectData(MPI_Comm comm, std::string path, const std::vector<int>& map)
 {
     CUDA_Check( cudaDeviceSynchronize() );
 
     std::string filename = path + "/" + name + ".obj.xmf";
-    info("Restarting object vector %s from file %s", name.c_str(), filename.c_str());
+    info("Restarting rigid object vector %s from file %s", name.c_str(), filename.c_str());
 
     XDMF::readRigidObjectData(filename, comm, this);
 
@@ -165,6 +171,8 @@ void RigidObjectVector::_restartObjectData(MPI_Comm comm, std::string path, cons
     restart_helpers::exchangeData(comm, map, ids, 1);
     restart_helpers::exchangeData(comm, map, motions, 1);
 
+    shiftCoordinates(domain, motions);
+    
     loc_ids->resize_anew(ids.size());
     loc_motions->resize_anew(motions.size());
 
