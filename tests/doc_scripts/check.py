aea99f3a6bf951899debcb73e5f5f8635f904570
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-10-01 09:52:11 +0200

    doc?scripts/check is shell; activate test

--- tests/doc_scripts/check.py
+++
@@ -1,5 +0,0 @@
-# cTEST: docs.basic
-# cd doc_scripts/
-# rm -f exit.out.txt
-# udx.run --runargs "-n 2" python basic.py
-# echo $? > exit.out.txt
---
+++ tests/doc_scripts/check.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# TEST: docs.basic
+cd doc_scripts/
+rm -f exit.out.txt
+udx.run --runargs "-n 2" python basic.py
+echo $? > exit.out.txt
