48e6e982a317e8c588df3ee72bbbc7b63e246c62
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-10-19 16:41:21 +0200

    separate implementation of interface in object file

---
+++ src/plugins/interface.cpp
@@ -0,0 +1,129 @@
+#include "interface.h"
+
+SimulationPlugin::SimulationPlugin(std::string name) :
+    name(name), sizeReq(MPI_REQUEST_NULL), dataReq(MPI_REQUEST_NULL)
+{}
+
+void SimulationPlugin::beforeForces               (cudaStream_t stream) {};
+void SimulationPlugin::beforeIntegration          (cudaStream_t stream) {};
+void SimulationPlugin::afterIntegration           (cudaStream_t stream) {};
+void SimulationPlugin::beforeParticleDistribution (cudaStream_t stream) {};
+
+void SimulationPlugin::serializeAndSend (cudaStream_t stream) {};
+void SimulationPlugin::handshake() {};
+void SimulationPlugin::talk() {};
+
+void SimulationPlugin::checkpoint(MPI_Comm& comm, std::string path) {}
+void SimulationPlugin::restart(MPI_Comm& comm, std::string path) {}
+
+
+void SimulationPlugin::setTime(float t, int tstep)
+{
+    currentTime = t;
+    currentTimeStep = tstep;
+}
+
+void SimulationPlugin::setup(Simulation* sim, const MPI_Comm& comm, const MPI_Comm& interComm)
+{
+    debug("Setting up simulation plugin '%s', MPI tag is %d", name.c_str(), tag());
+    this->sim = sim;
+    
+    MPI_Check( MPI_Comm_dup(comm, &this->comm) );
+    this->interComm = interComm;
+    
+    MPI_Check( MPI_Comm_rank(this->comm, &rank) );
+    MPI_Check( MPI_Comm_size(this->comm, &nranks) );
+}
+
+void SimulationPlugin::finalize()
+{
+    debug3("Plugin %s is finishing all the communications", name.c_str());
+    MPI_Check( MPI_Wait(&sizeReq, MPI_STATUS_IGNORE) );
+    MPI_Check( MPI_Wait(&dataReq, MPI_STATUS_IGNORE) );
+}
+
+int SimulationPlugin::tag()
+{
+    return (int)( nameHash(name) % 16767 );
+}
+    
+void SimulationPlugin::waitPrevSend()
+{
+    MPI_Check( MPI_Wait(&sizeReq, MPI_STATUS_IGNORE) );
+    MPI_Check( MPI_Wait(&dataReq, MPI_STATUS_IGNORE) );
+    sizeReq = MPI_REQUEST_NULL;
+    dataReq = MPI_REQUEST_NULL;
+}
+
+void SimulationPlugin::send(const std::vector<char>& data)
+{
+    send(data.data(), data.size());
+}
+
+void SimulationPlugin::send(const void* data, int sizeInBytes)
+{
+    // So that async Isend of the size works on
+    // valid address
+    localSendSize = sizeInBytes;
+
+    waitPrevSend();
+        
+    debug2("Plugin '%s' has is sending the data (%d bytes)", name.c_str(), sizeInBytes);
+    MPI_Check( MPI_Issend(&localSendSize, 1, MPI_INT, rank, 2*tag(), interComm, &sizeReq) );
+    MPI_Check( MPI_Issend(data, sizeInBytes, MPI_BYTE, rank, 2*tag()+1, interComm, &dataReq) );
+}
+
+
+
+// PostprocessPlugin
+
+PostprocessPlugin::PostprocessPlugin(std::string name) :
+    name(name)
+{}
+
+MPI_Request PostprocessPlugin::waitData()
+{
+    MPI_Request req;
+    MPI_Check( MPI_Irecv(&size, 1, MPI_INT, rank, 2*tag(), interComm, &req) );
+    return req;
+}
+
+void PostprocessPlugin::recv()
+{
+    data.resize(size);
+    MPI_Status status;
+    int count;
+    MPI_Check( MPI_Recv(data.data(), size, MPI_BYTE, rank, 2*tag()+1, interComm, &status) );
+    MPI_Check( MPI_Get_count(&status, MPI_BYTE, &count) );
+
+    if (count != size)
+        error("Plugin '%s' was going to receive %d bytes, but actually got %d. That may be fatal",
+              name.c_str(), size, count);
+
+    debug3("Plugin '%s' has received the data (%d bytes)", name.c_str(), count);
+}
+
+void PostprocessPlugin::deserialize(MPI_Status& stat) {};
+void PostprocessPlugin::handshake() {};
+void PostprocessPlugin::talk() {};
+
+void PostprocessPlugin::setup(const MPI_Comm& comm, const MPI_Comm& interComm)
+{
+    debug("Setting up postproc plugin '%s', MPI tag is %d", name.c_str(), tag());
+
+    MPI_Check( MPI_Comm_dup(comm, &this->comm) );
+    this->interComm = interComm;
+
+    MPI_Check( MPI_Comm_rank(this->comm, &rank) );
+    MPI_Check( MPI_Comm_size(this->comm, &nranks) );
+}
+
+int PostprocessPlugin::tag()
+{
+    return (int)( nameHash(name) % 16767 );
+}
+
+
+
+
+
--- src/plugins/interface.h
+++ src/plugins/interface.h
@@ -7,58 +7,33 @@
 
 class Simulation;
 
-// TODO: variable size messages
-
 class SimulationPlugin
 {
 public:
     std::string name;
 
-    SimulationPlugin(std::string name) :
-        name(name), sizeReq(MPI_REQUEST_NULL), dataReq(MPI_REQUEST_NULL)
-    {}
+    SimulationPlugin(std::string name);
 
-    virtual void beforeForces               (cudaStream_t stream) {};
-    virtual void beforeIntegration          (cudaStream_t stream) {};
-    virtual void afterIntegration           (cudaStream_t stream) {};
-    virtual void beforeParticleDistribution (cudaStream_t stream) {};
+    virtual void beforeForces               (cudaStream_t stream);
+    virtual void beforeIntegration          (cudaStream_t stream);
+    virtual void afterIntegration           (cudaStream_t stream);
+    virtual void beforeParticleDistribution (cudaStream_t stream);
 
-    virtual void serializeAndSend (cudaStream_t stream) {};
-    virtual void handshake() {};
-    virtual void talk() {};
+    virtual void serializeAndSend (cudaStream_t stream);
+    virtual void handshake();
+    virtual void talk();
 
     virtual bool needPostproc() = 0;
 
     /// Save handler state
-    virtual void checkpoint(MPI_Comm& comm, std::string path) {}
+    virtual void checkpoint(MPI_Comm& comm, std::string path);
     /// Restore handler state
-    virtual void restart(MPI_Comm& comm, std::string path) {}
+    virtual void restart(MPI_Comm& comm, std::string path);
 
 
-    void setTime(float t, int tstep)
-    {
-        currentTime = t;
-        currentTimeStep = tstep;
-    }
-
-    virtual void setup(Simulation* sim, const MPI_Comm& comm, const MPI_Comm& interComm)
-    {
-	debug("Setting up simulation plugin '%s', MPI tag is %d", name.c_str(), tag());
-        this->sim = sim;
-
-        MPI_Check( MPI_Comm_dup(comm, &this->comm) );
-        this->interComm = interComm;
-
-        MPI_Check( MPI_Comm_rank(this->comm, &rank) );
-        MPI_Check( MPI_Comm_size(this->comm, &nranks) );
-    }
-
-    virtual void finalize()
-    {
-        debug3("Plugin %s is finishing all the communications", name.c_str());
-        MPI_Check( MPI_Wait(&sizeReq, MPI_STATUS_IGNORE) );
-        MPI_Check( MPI_Wait(&dataReq, MPI_STATUS_IGNORE) );
-    }
+    void setTime(float t, int tstep);
+    virtual void setup(Simulation* sim, const MPI_Comm& comm, const MPI_Comm& interComm);
+    virtual void finalize();
 
     virtual ~SimulationPlugin() = default;
 
@@ -74,84 +49,33 @@ protected:
     int currentTimeStep;
 
     std::hash<std::string> nameHash;
-    int tag()
-    {
-        return (int)( nameHash(name) % 16767 );
-    }
-    
-    void waitPrevSend()
-    {
-        MPI_Check( MPI_Wait(&sizeReq, MPI_STATUS_IGNORE) );
-        MPI_Check( MPI_Wait(&dataReq, MPI_STATUS_IGNORE) );
-        sizeReq = MPI_REQUEST_NULL;
-        dataReq = MPI_REQUEST_NULL;
-    }
 
-    void send(const std::vector<char>& data)
-    {
-        send(data.data(), data.size());
-    }
-
-    void send(const void* data, int sizeInBytes)
-    {
-        // So that async Isend of the size works on
-        // valid address
-        localSendSize = sizeInBytes;
-
-        waitPrevSend();
-        
-        debug2("Plugin '%s' has is sending the data (%d bytes)", name.c_str(), sizeInBytes);
-        MPI_Check( MPI_Issend(&localSendSize, 1, MPI_INT, rank, 2*tag(), interComm, &sizeReq) );
-        MPI_Check( MPI_Issend(data, sizeInBytes, MPI_BYTE, rank, 2*tag()+1, interComm, &dataReq) );
-    }
+    int tag();    
+    void waitPrevSend();
+    void send(const std::vector<char>& data);
+    void send(const void* data, int sizeInBytes);
 };
+
+
+
 
 class PostprocessPlugin
 {
 public:
     std::string name;
 
-    PostprocessPlugin(std::string name) : name(name) { }
+    PostprocessPlugin(std::string name);
 
-    MPI_Request waitData()
-    {
-        MPI_Request req;
-        MPI_Check( MPI_Irecv(&size, 1, MPI_INT, rank, 2*tag(), interComm, &req) );
-        return req;
-    }
+    MPI_Request waitData();
+    void recv();
+    
+    virtual void deserialize(MPI_Status& stat);
+    virtual void handshake();
+    virtual void talk();
 
-    void recv()
-    {
-        data.resize(size);
-        MPI_Status status;
-        int count;
-        MPI_Check( MPI_Recv(data.data(), size, MPI_BYTE, rank, 2*tag()+1, interComm, &status) );
-        MPI_Check( MPI_Get_count(&status, MPI_BYTE, &count) );
-
-        if (count != size)
-            error("Plugin '%s' was going to receive %d bytes, but actually got %d. That may be fatal",
-                    name.c_str(), size, count);
-
-        debug3("Plugin '%s' has received the data (%d bytes)", name.c_str(), count);
-    }
-
-    virtual void deserialize(MPI_Status& stat) {};
-    virtual void handshake() {};
-    virtual void talk() {};
-
-    virtual void setup(const MPI_Comm& comm, const MPI_Comm& interComm)
-    {
-	debug("Setting up postproc plugin '%s', MPI tag is %d", name.c_str(), tag());
-
-        MPI_Check( MPI_Comm_dup(comm, &this->comm) );
-        this->interComm = interComm;
-
-        MPI_Check( MPI_Comm_rank(this->comm, &rank) );
-        MPI_Check( MPI_Comm_size(this->comm, &nranks) );
-    }
+    virtual void setup(const MPI_Comm& comm, const MPI_Comm& interComm);
 
     virtual ~PostprocessPlugin() = default;
-
 
 protected:
     MPI_Comm comm, interComm;
@@ -160,10 +84,8 @@ protected:
     int size;
 
     std::hash<std::string> nameHash;
-    int tag()
-    {
-        return (int)( nameHash(name) % 16767 );
-    }
+
+    int tag();
 };
 
 
