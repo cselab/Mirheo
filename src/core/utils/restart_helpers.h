33498300beee262882b6a621c9dba83b51d3e141
Dmitry <dimaleks@gmail.com>
2018-11-12 09:43:50 +0100

    forgot to add a file

---
+++ src/core/utils/restart_helpers.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+namespace TextIO
+{
+    template<typename Arg>
+    void writeToStream(std::ofstream& fout, const Arg& arg)
+    {
+        fout << arg << std::endl;
+    }
+
+    template<typename Arg, typename... Args>
+    void writeToStream(std::ofstream& fout, const Arg& arg, const Args&... args)
+    {
+        fout << arg << std::endl;
+        writeToStream(fout, args...);
+    }
+
+    template<typename... Args>
+    void write(std::string fname, const Args&... args)
+    {
+        std::ofstream fout(fname);
+        writeToStream(fout, args...);
+    }
+
+
+
+    template<typename Arg>
+    bool readFromStream(std::ifstream& fin, Arg& arg)
+    {
+        return (fin >> arg).fail();
+    }
+
+    template<typename Arg, typename... Args>
+    bool readFromStream(std::ifstream& fin, Arg& arg, Args&... args)
+    {
+        return (fin >> arg).fail() && readFromStream(fin, args...);
+    }
+
+    template<typename... Args>
+    bool read(std::string fname, Args&... args)
+    {
+        std::ifstream fin(fname);
+        return fin.good() && readFromStream(fin, args...);
+    }
+}
