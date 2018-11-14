b7331487fb65d1c7e2949253ea76b8211e8e2657
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-11-14 12:19:23 +0100

    add lj_with_stress for real

---
+++ src/core/interactions/lj_with_stress.cu
@@ -0,0 +1,41 @@
+#include <memory>
+
+#include "lj_with_stress.h"
+#include "pairwise_with_stress.h"
+#include "pairwise_interactions/lj.h"
+#include "pairwise_interactions/lj_object_aware.h"
+
+#include <core/utils/make_unique.h>
+#include <core/pvs/particle_vector.h>
+
+
+InteractionLJWithStress::InteractionLJWithStress(std::string name, float rc, float epsilon, float sigma, float maxForce, bool objectAware, float stressPeriod) :
+    InteractionLJ(name, rc, epsilon, sigma, maxForce, objectAware, false),
+    stressPeriod(stressPeriod)
+{
+    if (objectAware) {
+        Pairwise_LJObjectAware lj(rc, epsilon, sigma, maxForce);
+        impl = std::make_unique<InteractionPair_withStress<Pairwise_LJObjectAware>> (name, rc, stressPeriod, lj);
+    }
+    else {
+        Pairwise_LJ lj(rc, epsilon, sigma, maxForce);
+        impl = std::make_unique<InteractionPair_withStress<Pairwise_LJ>> (name, rc, stressPeriod, lj);
+    }
+}
+
+InteractionLJWithStress::~InteractionLJWithStress() = default;
+
+void InteractionLJWithStress::setSpecificPair(ParticleVector* pv1, ParticleVector* pv2, 
+                                              float epsilon, float sigma, float maxForce)
+{
+    if (objectAware) {
+        Pairwise_LJObjectAware lj(rc, epsilon, sigma, maxForce);
+        auto ptr = static_cast< InteractionPair_withStress<Pairwise_LJObjectAware>* >(impl.get());
+        ptr->setSpecificPair(pv1->name, pv2->name, lj);
+    }
+    else {
+        Pairwise_LJ lj(rc, epsilon, sigma, maxForce);
+        auto ptr = static_cast< InteractionPair_withStress<Pairwise_LJ>* >(impl.get());
+        ptr->setSpecificPair(pv1->name, pv2->name, lj);
+    }
+}
---
+++ src/core/interactions/lj_with_stress.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "lj.h"
+
+struct InteractionLJWithStress : public InteractionLJ
+{        
+    InteractionLJWithStress(std::string name, float rc, float epsilon, float sigma, float maxForce, bool objectAware, float stressPeriod);
+
+    ~InteractionLJWithStress();
+
+    void setSpecificPair(ParticleVector* pv1, ParticleVector* pv2, 
+                         float epsilon, float sigma, float maxForce) override;
+
+protected:
+    float stressPeriod;
+};
+
