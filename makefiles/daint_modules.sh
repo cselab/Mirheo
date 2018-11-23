59c07f88e785b591e7c4bfc9a63a1bfacc802c93
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-11-23 16:20:30 +0100

    remove very old makefiles

--- makefiles/daint_modules.sh
+++
@@ -1,8 +0,0 @@
-module load daint-gpu
-module rm cudatoolkit cray-mpich cray-hdf5-* CMake
-
-module load cray-python/3.6.1.1
-module load cudatoolkit/9.0.103_3.7-6.0.4.1_2.1__g72b395b
-module load cray-mpich
-module load cray-hdf5-parallel
-module load CMake/3.10.1
