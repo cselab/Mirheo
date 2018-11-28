052d00560e26e187f734d16fe3759c03a15e9ba9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-11-28 15:34:26 +0100

    wip: adding postprocessing for forces comparison

---
+++ tests/membrane/utils/post.juelicher.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+import argparse
+import numpy as np
+import h5py as h5
+
+def err(s): sys.stderr.write(s)
+
+def fopen(fname):
+    try:
+        f = h5.File(fname, "r")
+    except IOError:
+        err("u.avgh5: fails to open <%s>\n" % fname)
+        sys.exit(2)
+    return f
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', type=str, required=True)
+parser.add_argument('--out', type=str, required=True)
+args = parser.parse_args()
+
+
+f = fopen(args.file)
+pos = f["position"]
+forces = f["forces"].value
+areas  = f["areas"].value
+(n, dim) = pos.shape
+
+pos = pos.value
+
+pos = pos - (np.sum(pos, axis=0)/n)
+
+r = np.sqrt(pos[:,0]**2 + pos[:,1]**2)
+fmagn = np.sqrt(forces[:,0]**2 + forces[:,1]**2 + forces[:,2]**2)
+
+r = r.reshape((n,1))
+fmagn = fmagn.reshape((n,1))
+
+np.savetxt(args.out, np.hstack((r,fmagn)))
