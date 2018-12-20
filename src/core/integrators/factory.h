ea41befa378b3e9b35fecd911c20bbdce6ef2267
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-12-20 12:24:17 +0100

    use shared ptr in integrator factory

--- src/core/integrators/factory.h
+++ src/core/integrators/factory.h
@@ -7,7 +7,7 @@
 
 #include "const_omega.h"
 #include "forcing_terms/const_dp.h"
-#include "forcing_terms/none."
+#include "forcing_terms/none.h"
 #include "forcing_terms/periodic_poiseuille.h"
 #include "oscillate.h"
 #include "rigid_vv.h"
@@ -17,20 +17,22 @@
 
 namespace IntegratorFactory
 {
-    static IntegratorVV<Forcing_None>* createVV(std::string name, float dt)
+    static std::shared_ptr<IntegratorVV<Forcing_None>>
+    createVV(std::string name, float dt)
     {
         Forcing_None forcing;
-        return  new IntegratorVV<Forcing_None> (name, dt, forcing);
+        return std::make_shared<IntegratorVV<Forcing_None>> (name, dt, forcing);
     }
 
-    static IntegratorVV<Forcing_ConstDP>* createVV_constDP(std::string name, float dt, PyTypes::float3 extraForce)
+    static std::shared_ptr<IntegratorVV<Forcing_ConstDP>>
+    createVV_constDP(std::string name, float dt, PyTypes::float3 extraForce)
     {
         Forcing_ConstDP forcing(make_float3(extraForce));
-        return  new IntegratorVV<Forcing_ConstDP>(name, dt, forcing);
+        return std::make_shared<IntegratorVV<Forcing_ConstDP>> (name, dt, forcing);
     }
 
-    static IntegratorVV<Forcing_PeriodicPoiseuille>*
-        createVV_PeriodicPoiseuille(std::string name, float dt, float force, std::string direction)
+    static std::shared_ptr<IntegratorVV<Forcing_PeriodicPoiseuille>>
+    createVV_PeriodicPoiseuille(std::string name, float dt, float force, std::string direction)
     {
         Forcing_PeriodicPoiseuille::Direction dir;
         if      (direction == "x") dir = Forcing_PeriodicPoiseuille::Direction::x;
@@ -39,31 +41,36 @@ namespace IntegratorFactory
         else die("Direction can only be 'x' or 'y' or 'z'");
         
         Forcing_PeriodicPoiseuille forcing(force, dir);
-        return  new IntegratorVV<Forcing_PeriodicPoiseuille>(name, dt, forcing);
+        return std::make_shared<IntegratorVV<Forcing_PeriodicPoiseuille>> (name, dt, forcing);
     }
 
-    static IntegratorConstOmega* createConstOmega(std::string name, float dt, PyTypes::float3 center, PyTypes::float3 omega)
+    static std::shared_ptr<IntegratorConstOmega>
+    createConstOmega(std::string name, float dt, PyTypes::float3 center, PyTypes::float3 omega)
     {
-        return  new IntegratorConstOmega(name, dt, make_float3(center), make_float3(omega));
+        return std::make_shared<IntegratorConstOmega> (name, dt, make_float3(center), make_float3(omega));
     }
 
-    static IntegratorTranslate* createTranslate(std::string name, float dt, PyTypes::float3 velocity)
+    static std::shared_ptr<IntegratorTranslate>
+    createTranslate(std::string name, float dt, PyTypes::float3 velocity)
     {
-        return  new IntegratorTranslate(name, dt, make_float3(velocity));
+        return std::make_shared<IntegratorTranslate> (name, dt, make_float3(velocity));
     }
 
-    static IntegratorOscillate* createOscillating(std::string name, float dt, PyTypes::float3 velocity, float period)
+    static std::shared_ptr<IntegratorOscillate>
+    createOscillating(std::string name, float dt, PyTypes::float3 velocity, float period)
     {
-        return  new IntegratorOscillate(name, dt, make_float3(velocity), period);
+        return std::make_shared<IntegratorOscillate> (name, dt, make_float3(velocity), period);
     }
 
-    static IntegratorVVRigid* createRigidVV(std::string name, float dt)
+    static std::shared_ptr<IntegratorVVRigid>
+    createRigidVV(std::string name, float dt)
     {
-        return  new IntegratorVVRigid(name, dt);
+        return std::make_shared<IntegratorVVRigid> (name, dt);
     }
 
-    static IntegratorSubStepMembrane* createSubStepMembrane(std::string name, float dt, int substeps, Interaction *fastForces)
+    static std::shared_ptr<IntegratorSubStepMembrane>
+    createSubStepMembrane(std::string name, float dt, int substeps, Interaction *fastForces)
     {
-        return new IntegratorSubStepMembrane(name, dt, substeps, fastForces);
+        return std::make_shared<IntegratorSubStepMembrane> (name, dt, substeps, fastForces);
     }    
 };
