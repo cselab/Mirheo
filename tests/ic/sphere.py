4e99ad4e1e5577d43c835d7ab9736aec9540d7ef
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-01-09 14:05:05 +0100

    add test

---
+++ tests/ic/sphere.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import numpy as np
+import ymero as ymr
+
+ranks  = (1, 1, 1)
+domain = [4., 4., 4.]
+density = 8
+
+u = ymr.ymero(ranks, tuple(domain), dt=0, debug_level=3, log_filename='log')
+
+pv = ymr.ParticleVectors.ParticleVector('pv', mass = 1)
+ic = ymr.InitialConditions.UniformSphere(density=density, center=(2., 2., 2.), radius=2.0, inside=True)
+u.registerParticleVector(pv=pv, ic=ic)
+
+u.run(2)
+
+if pv:
+    icpos = pv.getCoordinates()
+    icvel = pv.getVelocities()
+    np.savetxt("pos.ic.txt", icpos)
+    np.savetxt("vel.ic.txt", icvel)
+    
+
+# TEST: ic.uniform.sphere
+# cd ic
+# rm -rf pos*.txt vel*.txt
+# ymr.run --runargs "-n 2" ./sphere.py > /dev/null
+# paste pos.ic.txt vel.ic.txt | LC_ALL=en_US.utf8 sort > ic.out.txt
