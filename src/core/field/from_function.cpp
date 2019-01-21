8ef612fc592997e466fcabe7be603cf71d11d07f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-01-21 14:59:56 +0100

    minor

--- src/core/field/from_function.cpp
+++ src/core/field/from_function.cpp
@@ -9,17 +9,19 @@ FieldFromFunction::~FieldFromFunction() = default;
 
 FieldFromFunction::FieldFromFunction(FieldFromFunction&&) = default;
 
-static float3 make_periodic(float3 r, float3 L)
-{
-    auto oneD = [](float x, float L) {
-        if (x <  0) x += L;
-        if (x >= L) x -= L;
-        return x;
-    };
 
-    return {oneD(r.x, L.x),
-            oneD(r.y, L.y),
-            oneD(r.z, L.z)};
+inline float make_perioidc(float x, float L)
+{
+    if (x <  0) x += L;
+    if (x >= L) x -= L;
+    return x;
+};
+
+inline float3 make_periodic(float3 r, float3 L)
+{
+    return {make_perioidc(r.x, L.x),
+            make_perioidc(r.y, L.y),
+            make_perioidc(r.z, L.z)};
 }
 
 void FieldFromFunction::setup(MPI_Comm& comm)
