c0b04a91b7eb758733a6cf196de54ecf52b944a2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-01-24 09:26:38 +0100

    minor: rename

--- src/core/interactions/pairwise.cu
+++ src/core/interactions/pairwise.cu
@@ -27,14 +27,14 @@
 do{ debug2("Dispatched to "#TPP" thread(s) per particle variant");              \
     SAFE_KERNEL_LAUNCH(                                                         \
             computeExternalInteractions_##TPP##tpp<P1 COMMA P2 COMMA P3>,       \
-            getNblocks(TPP*view.size, nth), nth, 0, stream,                     \
-            view, cl2->cellInfo(), rc*rc, INTERACTION_FUNCTION); } while (0)
+            getNblocks(TPP*dstView.size, nth), nth, 0, stream,                  \
+            dstView, cl2->cellInfo(), rc*rc, INTERACTION_FUNCTION); } while (0)
 
 #define CHOOSE_EXTERNAL(P1, P2, P3, INTERACTION_FUNCTION)                                              \
-do{  if (view.size < 1000  ) { DISPATCH_EXTERNAL(P1, P2, P3, 27, INTERACTION_FUNCTION); }              \
-else if (view.size < 10000 ) { DISPATCH_EXTERNAL(P1, P2, P3, 9,  INTERACTION_FUNCTION); }              \
-else if (view.size < 400000) { DISPATCH_EXTERNAL(P1, P2, P3, 3,  INTERACTION_FUNCTION); }              \
-else                         { DISPATCH_EXTERNAL(P1, P2, P3, 1,  INTERACTION_FUNCTION); } } while(0)
+do{  if (dstView.size < 1000  ) { DISPATCH_EXTERNAL(P1, P2, P3, 27, INTERACTION_FUNCTION); }           \
+else if (dstView.size < 10000 ) { DISPATCH_EXTERNAL(P1, P2, P3, 9,  INTERACTION_FUNCTION); }           \
+else if (dstView.size < 400000) { DISPATCH_EXTERNAL(P1, P2, P3, 3,  INTERACTION_FUNCTION); }           \
+else                            { DISPATCH_EXTERNAL(P1, P2, P3, 1,  INTERACTION_FUNCTION); } } while(0)
 
 /**
  * Interface to _compute() with local interactions.
@@ -152,7 +152,7 @@ void InteractionPair<PairwiseInteraction>::_compute(InteractionType type,
             const int np2 = pv2->local()->size();
             debug("Computing external forces for %s - %s (%d - %d particles)", pv1->name.c_str(), pv2->name.c_str(), np1, np2);
 
-            auto view = cl1->getView<PVview>();
+            auto dstView = cl1->getView<PVview>();
 
             const int nth = 128;
             if (np1 > 0 && np2 > 0)
@@ -169,7 +169,7 @@ void InteractionPair<PairwiseInteraction>::_compute(InteractionType type,
         const int np2 = pv2->local()->size();
         debug("Computing halo forces for %s(halo) - %s (%d - %d particles)", pv1->name.c_str(), pv2->name.c_str(), np1, np2);
 
-        PVview view(pv1, pv1->halo());
+        PVview dstView(pv1, pv1->halo());
         const int nth = 128;
         if (np1 > 0 && np2 > 0)
             if (dynamic_cast<ObjectVector*>(pv1) == nullptr) // don't need forces for pure particle halo
