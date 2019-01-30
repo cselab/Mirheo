627a130a50b50134ad9f5c2e70652d8abf256fe2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-01-30 11:17:39 +0100

    test: mdpd at rest

--- tests/mdpd/rest.py
+++ tests/mdpd/rest.py
@@ -5,19 +5,19 @@ import ymero as ymr
 dt = 0.001
 
 ranks  = (1, 1, 1)
-domain = (12, 8, 10)
+domain = (16, 16, 16)
 
 u = ymr.ymero(ranks, domain, dt, debug_level=3, log_filename='log')
 
 pv = ymr.ParticleVectors.ParticleVector('pv', mass = 1)
-ic = ymr.InitialConditions.Uniform(density=2)
+ic = ymr.InitialConditions.Uniform(density=3)
 u.registerParticleVector(pv=pv, ic=ic)
 
 rc = 1.0
 rd = 0.75
 
 den  = ymr.Interactions.Density('den', rd)
-mdpd = ymr.Interactions.MDPD('mdpd', rc, rd, a=10.0, b=-5.0, gamma=10.0, kbt=1.0, power=0.5)
+mdpd = ymr.Interactions.MDPD('mdpd', rc, rd, a=10.0, b=20.0, gamma=10.0, kbt=1.0, power=0.5)
 u.registerInteraction(den)
 u.registerInteraction(mdpd)
 u.setInteraction(den, pv, pv)
@@ -28,7 +28,7 @@ u.registerIntegrator(vv)
 u.setIntegrator(vv, pv)
 
 u.registerPlugins(ymr.Plugins.createStats('stats', "stats.txt", 1000))
-#u.registerPlugins(ymr.Plugins.createDumpParticles('partDump', pv, 1000, [['densities', 'scalar']], 'h5/solvent_particles-'))
+u.registerPlugins(ymr.Plugins.createDumpParticles('partDump', pv, 1000, [], 'h5/solvent_particles-'))
 
 u.run(5001)
 
