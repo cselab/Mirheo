0775770d307d154eb8b6687f87cd4859754af74b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-02-06 17:39:34 +0100

    fix test output

--- tests/walls/forceCollector.py
+++ tests/walls/forceCollector.py
@@ -2,6 +2,7 @@
 
 import argparse
 import ymero as ymr
+import numpy as np
 
 dt = 0.001
 
@@ -14,13 +15,15 @@ rc      = 1.0
 gdot    = -0.5 # shear rate
 tend    = 10.1
 
+gdpd = 11.0
+
 u = ymr.ymero(ranks, domain, dt, debug_level=3, log_filename='log')
 
 pv = ymr.ParticleVectors.ParticleVector('pv', mass = 1)
 ic = ymr.InitialConditions.Uniform(density=density)
 u.registerParticleVector(pv=pv, ic=ic)
     
-dpd = ymr.Interactions.DPD('dpd', rc=rc, a=12.0, gamma=11.0, kbt=0.4, power=0.125)
+dpd = ymr.Interactions.DPD('dpd', rc=rc, a=12.0, gamma=gdpd, kbt=0.4, power=0.125)
 u.registerInteraction(dpd)
 
 vx = gdot*(domain[2] - 2*rc)
@@ -49,13 +52,30 @@ u.setIntegrator(translate, frozen_hi)
 
 sample_every = 1
 dump_every   = 1000
-
-u.registerPlugins(ymr.Plugins.createWallForceCollector('forceCollector', plate_hi, frozen_hi, sample_every, dump_every, 'wallForce.txt'))
+outname_hi = 'wallForceHi.txt'
+outname_lo = 'wallForceLo.txt'
+u.registerPlugins(ymr.Plugins.createWallForceCollector('forceCollectorHi', plate_hi, frozen_hi, sample_every, dump_every, outname_hi))
+u.registerPlugins(ymr.Plugins.createWallForceCollector('forceCollectorLo', plate_lo, frozen_lo, sample_every, dump_every, outname_lo))
 
 u.run((int)(tend/dt))
+
+if u.isComputeTask():
+    A = domain[0] * domain[1]
+    
+    data_hi = np.loadtxt(outname_hi)
+    data_lo = np.loadtxt(outname_lo)
+
+    avg_from = 5
+    fx_hi = abs(np.mean(data_hi[avg_from:,1]))
+    fx_lo = abs(np.mean(data_lo[avg_from:,1]))
+    
+    Sxy_hi = fx_hi / A
+    Sxy_lo = fx_lo / A
+    thSxy = abs(gdot) * 4.96 * gdpd
+    print(thSxy, Sxy_hi, Sxy_lo)
 
 # nTEST: walls.forceCollector.couette
 # cd walls/
 # rm -rf wallForce*txt
 # ymr.run --runargs "-n 2" ./forceCollector.py > /dev/null
-# cat wallForce.txt | awk '{print $1 / 100.0}' > wallForce.out.txt
+# cat wallForceHi.txt | awk '{print $1, $2 / 5000.0, $3 / 5000.0, $4 / 5000.0}' > wallForce.out.txt
