15774cc4fecdfe1ae87daeb9efe87454b10bf92f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-02-20 15:07:52 +0100

    add tests for the latest fix

--- tests/plugins/pvsExchange.py
+++ tests/plugins/pvsExchange.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
 import ymero as ymr
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ranks', type=int, nargs=3, default = (1,1,1))
+parser.add_argument('--fraction', type=float, default = 0.5)
+args = parser.parse_args()
 
 dt = 0.001
 density = 10
 
-ranks  = (1, 1, 1)
+ranks  = args.ranks
 domain = (16, 16, 16)
 a = 1
 
 u = ymr.ymero(ranks, domain, dt, debug_level=3, log_filename='log')
 
 def upHalf(r):
-    return r[1] > domain[1]/2
+    return r[1] > domain[1] * args.fraction
 def loHalf(r):
     return not upHalf(r)
 
@@ -35,13 +41,13 @@ u.setIntegrator(vv, pv1)
 u.setIntegrator(vv, pv2)
 
 sampleEvery = 2
-dumpEvery   = 1000
+dump_every  = 1000
 binSize     = (1., 1., 1.)
 
-u.registerPlugins(ymr.Plugins.createDumpAverage('field', [pv1], sampleEvery, dumpEvery, binSize, [("velocity", "vector_from_float8")], 'h5/solvent-'))
+u.registerPlugins(ymr.Plugins.createDumpAverage('field', [pv1], sampleEvery, dump_every, binSize, [("velocity", "vector_from_float8")], 'h5/solvent-'))
 u.registerPlugins(ymr.Plugins.createExchangePVSFluxPlane("color_exchanger", pv1, pv2, (1., 0., 0., 0.)))
-# u.registerPlugins(ymr.Plugins.createDumpParticles('partDump1', pv1, dumpEvery, [], 'h5/pv1-'))
-# u.registerPlugins(ymr.Plugins.createDumpParticles('partDump2', pv2, dumpEvery, [], 'h5/pv2-'))
+# u.registerPlugins(ymr.Plugins.createDumpParticles('partDump1', pv1, dump_every, [], 'h5/pv1-'))
+# u.registerPlugins(ymr.Plugins.createDumpParticles('partDump2', pv2, dump_every, [], 'h5/pv2-'))
 
 u.run(5002)
 
@@ -51,5 +57,17 @@ del(u)
 # cd plugins
 # rm -rf h5
 # ymr.run --runargs "-n 2" ./pvsExchange.py > /dev/null
+# ymr.avgh5 xz density h5/solvent-0000[2-4].h5 | awk '{print $1}' > profile.out.txt
+
+# nTEST: plugins.pvsExchange.mpi
+# cd plugins
+# rm -rf h5
+# ymr.run --runargs "-n 4" ./pvsExchange.py --ranks 2 1 1 > /dev/null
+# ymr.avgh5 xz density h5/solvent-0000[2-4].h5 | awk '{print $1}' > profile.out.txt
+
+# nTEST: plugins.pvsExchange.mpi.zero
+# cd plugins
+# rm -rf h5
+# ymr.run --runargs "-n 4" ./pvsExchange.py --ranks 2 1 1 --fraction 0.0 > /dev/null
 # ymr.avgh5 xz density h5/solvent-0000[2-4].h5 | awk '{print $1}' > profile.out.txt
 
