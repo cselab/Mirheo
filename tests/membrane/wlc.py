9d61f5995048c50a5ebea75957c7673ddc4dcebd
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-02-22 12:10:47 +0100

    add tests for wlc shear forces

---
+++ tests/membrane/wlc.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import numpy as np
+import ymero as ymr
+import sys, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--stressFree', action="store_true", default=False)
+args = parser.parse_args()
+
+dt = 0.001
+
+ranks  = (1, 1, 1)
+domain = (12, 8, 10)
+
+u = ymr.ymero(ranks, domain, dt, debug_level=3, log_filename='log')
+
+mesh_rbc = ymr.ParticleVectors.MembraneMesh("rbc_mesh.off")
+pv_rbc   = ymr.ParticleVectors.MembraneVector("rbc", mass=1.0, mesh=mesh_rbc)
+ic_rbc   = ymr.InitialConditions.Membrane([[8.0, 4.0, 5.0,   1.0, 0.0, 0.0, 0.0]])
+u.registerParticleVector(pv_rbc, ic_rbc)
+
+prm_rbc         = ymr.Interactions.MembraneParameters()
+prm_bending_rbc = ymr.Interactions.KantorBendingParameters()
+
+if prm_rbc:
+    lscale = 1.0
+    p                 = 0.000906667 * lscale
+    prm_rbc.x0        = 0.457
+    prm_rbc.ka        = 0.0
+    prm_rbc.kd        = 0.0
+    prm_rbc.kv        = 0.0
+    prm_rbc.gammaC    = 0.0
+    prm_rbc.gammaT    = 0.0
+    prm_rbc.kbT       = 0.0
+    prm_rbc.mpow      = 2.0
+    prm_rbc.totArea   = 62.2242 * lscale**2
+    prm_rbc.totVolume = 26.6649 * lscale**3
+
+    prm_rbc.ks        = 1000.0
+    prm_rbc.rnd = False
+
+if prm_bending_rbc:
+    prm_bending_rbc.kb     = 0.0
+    prm_bending_rbc.theta  = 0.0
+    
+int_rbc = ymr.Interactions.MembraneForcesKantor("int_rbc", prm_rbc, prm_bending_rbc, stressFree=args.stressFree)
+u.registerInteraction(int_rbc)
+u.setInteraction(int_rbc, pv_rbc, pv_rbc)
+
+
+dump_every = 1
+
+u.registerPlugins(ymr.Plugins.createForceSaver("forceSaver", pv_rbc))
+
+u.registerPlugins(ymr.Plugins.createDumpParticlesWithMesh("meshdump",
+                                                          pv_rbc,
+                                                          dump_every,
+                                                          [["forces", "vector"]],
+                                                          "h5/rbc-"))
+
+u.run(2)
+
+# nTEST: membrane.shear.wlc
+# cd membrane
+# cp ../../data/rbc_mesh.off .
+# ymr.run --runargs "-n 2" ./wlc.py > /dev/null
+# ymr.post ./utils/post.forces.py --file h5/rbc-00000.h5 --out forces.out.txt
+
+# nTEST: membrane.shear.wlc.stressFree.biconcave
+# cd membrane
+# cp ../../data/rbc_mesh.off .
+# ymr.run --runargs "-n 2" ./wlc.py --stressFree > /dev/null
+# ymr.post ./utils/post.forces.py --file h5/rbc-00000.h5 --out forces.out.txt
