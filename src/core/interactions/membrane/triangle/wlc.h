55b76e08529c3024803877ab6249638cc9a401e9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-02-25 11:01:46 +0100

    more cleanup

--- src/core/interactions/membrane/triangle/wlc.h
+++ src/core/interactions/membrane/triangle/wlc.h
@@ -1,8 +1,13 @@
 #pragma once
 
-#include "local_area.h"
+#include "../parameters.h"
 
+#include <core/utils/cpu_gpu_defines.h>
+#include <core/utils/helper_math.h>
 #include <core/mesh/membrane.h>
+
+#include <cmath>
+
 
 enum class StressFreeState
 {
@@ -12,7 +17,7 @@ enum class StressFreeState
 
 
 template <StressFreeState stressFreeState>
-class TriangleWLCForce : public LocalAreaForce
+class TriangleWLCForce
 {
 public:    
     struct LengthArea
@@ -24,13 +29,14 @@ public:
     using ParametersType          = WLCParameters;
     
     TriangleWLCForce(ParametersType p, const Mesh *mesh, float lscale) :
-        LocalAreaForce(p.kd, lscale),
         lscale(lscale)
     {
         x0   = p.x0;
         ks   = p.ks * lscale * lscale;
         mpow = p.mpow;
 
+        kd = p.kd * lscale * lscale;
+        
         area0   = p.totArea0 * lscale * lscale / mesh->getNtriangles();
         length0 = sqrt(area0 * 4.0 / sqrt(3.0));
     }
@@ -56,7 +62,7 @@ public:
 
     __D__ inline float3 operator()(float3 v1, float3 v2, float3 v3, EquilibriumTriangleDesc eq) const
     {
-        return LocalAreaForce::areaForce(v1, v2, v3, eq.a0) + bondForce(v1, v2, eq.l0);
+        return areaForce(v1, v2, v3, eq.a0) + bondForce(v1, v2, eq.l0);
     }
         
 private:
@@ -82,8 +88,25 @@ private:
         return IfI * (v2 - v1);
     }
 
+    __D__ inline float3 areaForce(float3 v1, float3 v2, float3 v3, float area0) const
+    {
+        float3 x21 = v2 - v1;
+        float3 x32 = v3 - v2;
+        float3 x31 = v3 - v1;
+
+        float3 normal = cross(x21, x31);
+
+        float area = 0.5f * length(normal);
+
+        float coef = kd * (area - area0) / (area * area0);
+
+        return -0.25f * coef * cross(normal, x32);
+    }
+
+
     static constexpr float forceCap = 1500.f;
     float x0, ks, mpow;
+    float kd;
 
     float length0, area0; ///< only useful when StressFree is false
     float lscale;
