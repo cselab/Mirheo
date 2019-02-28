552f1b7187a06f3134d3c4a168189f1caefb5b83
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-02-28 16:44:25 +0100

    add unit test for triangle invariants (useful for Lim)

---
+++ units/triangle_invariants/main.cpp
@@ -0,0 +1,136 @@
+#include <core/logger.h>
+#include <core/utils/helper_math.h>
+
+#include <cmath>
+#include <cstdio>
+#include <gtest/gtest.h>
+
+Logger logger;
+
+using real = double;
+using real3 = double3;
+
+struct Triangle
+{
+    real3 a, b, c;
+};
+
+static Triangle genTriangle(long seed)
+{    
+    Triangle t;
+    srand48(seed);
+
+    auto gen3 = []() -> real3 { return {(real) drand48(), (real) drand48(), (real) drand48()}; };
+    
+    t.a = gen3();
+    t.b = gen3();
+    t.c = gen3();
+
+    return t;
+};
+
+static void print(Triangle t)
+{
+    printf("%g %g %g  %g %g %g  %g %g %g\n",
+           t.a.x, t.a.y, t.a.z,
+           t.b.x, t.b.y, t.b.z,
+           t.c.x, t.c.y, t.c.z);
+}
+
+static void shuffle(Triangle& t)
+{
+    auto tmp = t.a;
+    t.a = t.b;
+    t.b = t.c;
+    t.c = tmp;
+}
+
+static real area(Triangle t)
+{
+    real3 v12 = t.b - t.a;
+    real3 v13 = t.c - t.a;
+    real3 n = cross(v12, v13);
+    return 0.5 * length(n);
+}
+
+static real alpha(Triangle t, Triangle tref)
+{
+    return area(t) / area(tref) - 1;
+}
+
+inline real safeSqrt(real a)
+{
+    return a > 0.0 ? sqrt(a) : 0.0;
+}
+
+static real beta(Triangle t, Triangle tref)
+{
+    real3 v12 = t.b - t.a;
+    real3 v13 = t.c - t.a;
+
+    real3 v12ref = tref.b - tref.a;
+    real3 v13ref = tref.c - tref.a;
+
+    real area_inv  = 1.0 / area(t);
+    real area0_inv = 1.0 / area(tref);
+
+    real e0sq_A = dot(v12, v12) * area_inv;
+    real e1sq_A = dot(v13, v13) * area_inv;
+
+    real e0sq_A0 = dot(v12ref, v12ref) * area0_inv;
+    real e1sq_A0 = dot(v13ref, v13ref) * area0_inv;
+
+    // 
+    real v12v13 = dot(v12, v13);
+    real v12v13ref = dot(v12ref, v13ref);
+    real sign = (v12v13ref * v12v13) > 0.0 ? 1.0 : -1.0;
+    // this sign is introduced because of the angle which can be > pi/2
+    
+    real beta = 0.125 * (e0sq_A0*e1sq_A + e1sq_A0*e0sq_A
+                         - 2. * sign *  sqrt((e0sq_A0 * e1sq_A0 - 4.) * (e0sq_A * e1sq_A - 4.))
+                         - 8.);
+    // print(t);
+    // print(tref);
+    // printf("beta = %g\n", beta);
+    return beta;
+}
+
+TEST (TRINAGLE_STRAIN_INVARIANTS, alpha)
+{
+    constexpr real tolerance = 1e-6;
+    
+    auto t    = genTriangle(4242);
+    auto tref = genTriangle(2424);
+
+    real a1, a2, a3;
+
+    a1 = alpha(t, tref); shuffle(t); shuffle(tref);
+    a2 = alpha(t, tref); shuffle(t); shuffle(tref);
+    a3 = alpha(t, tref);
+    
+    ASSERT_LE(fabs(a1-a2), tolerance);
+    ASSERT_LE(fabs(a1-a3), tolerance);
+}
+
+TEST (TRINAGLE_STRAIN_INVARIANTS, beta)
+{
+    constexpr real tolerance = 1e-6;
+    
+    auto t    = genTriangle(4242);
+    auto tref = genTriangle(2424);
+
+    real b1, b2, b3;
+
+    b1 = beta(t, tref); shuffle(t); shuffle(tref);
+    b2 = beta(t, tref); shuffle(t); shuffle(tref);
+    b3 = beta(t, tref);
+    
+    ASSERT_LE(fabs(b1-b2), tolerance);
+    ASSERT_LE(fabs(b1-b3), tolerance);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
