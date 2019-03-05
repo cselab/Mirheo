a0df92827ae28200ac6eecb5c106fe863e94488b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-03-05 08:28:35 +0100

    more stable version of beta in triangle invariants unit

--- units/triangle_invariants/main.cpp
+++ units/triangle_invariants/main.cpp
@@ -3,6 +3,7 @@
 
 #include <cmath>
 #include <cstdio>
+#include <functional>
 #include <gtest/gtest.h>
 
 Logger logger;
@@ -95,38 +96,63 @@ static real beta(Triangle t, Triangle tref)
     return beta;
 }
 
-TEST (TRINAGLE_STRAIN_INVARIANTS, alpha)
+// more stable version of beta
+static real beta_stable(Triangle t, Triangle tref)
+{
+    real3 v12 = t.b - t.a;
+    real3 v13 = t.c - t.a;
+
+    real3 v12ref = tref.b - tref.a;
+    real3 v13ref = tref.c - tref.a;
+
+    real area_inv  = 1.0 / area(t);
+    real area0_inv = 1.0 / area(tref);
+
+    real e0sq_A = dot(v12, v12) * area_inv;
+    real e1sq_A = dot(v13, v13) * area_inv;
+
+    real e0sq_A0 = dot(v12ref, v12ref) * area0_inv;
+    real e1sq_A0 = dot(v13ref, v13ref) * area0_inv;
+
+    real v12v13    = dot(v12, v13);
+    real v12v13ref = dot(v12ref, v13ref);
+    
+    real beta = 0.125 * (e0sq_A0*e1sq_A + e1sq_A0*e0sq_A
+                         - 2. * v12v13 * v12v13ref * area_inv * area0_inv
+                         - 8.);
+    return beta;
+}
+
+using InvariantFunction = std::function<real(Triangle, Triangle)>;
+
+static void testManyTriangles(long seed, int ntests, InvariantFunction invariant)
 {
     constexpr real tolerance = 1e-6;
-    
-    auto t    = genTriangle(4242);
-    auto tref = genTriangle(2424);
 
-    real a1, a2, a3;
+    for (int i = 0; i < ntests; ++i)
+    {
+        auto t    = genTriangle(seed * 42 + i);
+        auto tref = genTriangle(seed + 42 * i);
 
-    a1 = alpha(t, tref); shuffle(t); shuffle(tref);
-    a2 = alpha(t, tref); shuffle(t); shuffle(tref);
-    a3 = alpha(t, tref);
-    
-    ASSERT_LE(fabs(a1-a2), tolerance);
-    ASSERT_LE(fabs(a1-a3), tolerance);
+        real a1, a2, a3;
+        
+        a1 = invariant(t, tref); shuffle(t); shuffle(tref);
+        a2 = invariant(t, tref); shuffle(t); shuffle(tref);
+        a3 = invariant(t, tref);
+        
+        ASSERT_LE(fabs(a1-a2), tolerance);
+        ASSERT_LE(fabs(a1-a3), tolerance);
+    }
+}
+
+TEST (TRINAGLE_STRAIN_INVARIANTS, alpha)
+{
+    testManyTriangles(4242, 10000, &alpha);
 }
 
 TEST (TRINAGLE_STRAIN_INVARIANTS, beta)
 {
-    constexpr real tolerance = 1e-6;
-    
-    auto t    = genTriangle(4242);
-    auto tref = genTriangle(2424);
-
-    real b1, b2, b3;
-
-    b1 = beta(t, tref); shuffle(t); shuffle(tref);
-    b2 = beta(t, tref); shuffle(t); shuffle(tref);
-    b3 = beta(t, tref);
-    
-    ASSERT_LE(fabs(b1-b2), tolerance);
-    ASSERT_LE(fabs(b1-b3), tolerance);
+    testManyTriangles(4242, 10000, &beta_stable);
 }
 
 int main(int argc, char **argv)
