fde91fc596be43f8bd9a368fcc1d67e5e790e1d6
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-03-07 10:23:43 +0100

    pass state and allocate step gen

--- src/core/interactions/membrane.impl.h
+++ src/core/interactions/membrane.impl.h
@@ -3,6 +3,7 @@
 #include "interface.h"
 #include "membrane/forces_kernels.h"
 #include "membrane/parameters.h"
+#include "utils/step_random_gen.h"
 
 #include <core/pvs/membrane_vector.h>
 #include <core/pvs/views/ov.h>
@@ -21,7 +22,7 @@
  * @param m RBC membrane mesh
  * @return parameters to be passed to GPU kernels
  */
-static MembraneForcesKernels::GPU_RBCparameters setParams(const MembraneParameters& p, float dt, float t)
+static MembraneForcesKernels::GPU_RBCparameters setParams(const MembraneParameters& p, StepRandomGen& stepGen, const YmrState *state)
 {
     MembraneForcesKernels::GPU_RBCparameters devP;
 
@@ -37,6 +38,9 @@ static MembraneForcesKernels::GPU_RBCparameters setParams(const MembraneParamete
     devP.fluctuationForces = p.fluctuationForces;
 
     if (devP.fluctuationForces) {
+        float t = state->currentTime;
+        float dt = state->dt;
+        
         int v = *((int*)&t);
         std::mt19937 gen(v);
         std::uniform_real_distribution<float> udistr(0.001, 1);
@@ -68,12 +72,13 @@ public:
     InteractionMembraneImpl(const YmrState *state, std::string name, MembraneParameters parameters,
                             typename TriangleInteraction::ParametersType triangleParams,
                             typename DihedralInteraction::ParametersType dihedralParams,
-                            float growUntil) :
+                            float growUntil, long seed = 42424242) :
         Interaction(state, name, 1.0f),
         parameters(parameters),
         scaleFromTime( [growUntil] (float t) { return min(1.0f, 0.5f + 0.5f * (t / growUntil)); } ),
         dihedralParams(dihedralParams),
-        triangleParams(triangleParams)
+        triangleParams(triangleParams),
+        stepGen(seed)
     {}
 
     ~InteractionMembraneImpl() = default;
@@ -101,7 +106,7 @@ public:
         const int nthreads = 128;
         const int nblocks  = getNblocks(view.size, nthreads);
 
-        auto devParams = setParams(currentParams, state->dt, state->currentTime);
+        auto devParams = setParams(currentParams, stepGen, state);
 
         DihedralInteraction dihedralInteraction(dihedralParams, scale);
         TriangleInteraction triangleInteraction(triangleParams, mesh, scale);
@@ -122,4 +127,5 @@ protected:
     MembraneParameters parameters;
     typename DihedralInteraction::ParametersType dihedralParams;
     typename TriangleInteraction::ParametersType triangleParams;
+    StepRandomGen stepGen;
 };
