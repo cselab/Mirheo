4412c682535818077d490d3a2f078615423dc14c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-03-08 18:13:18 +0100

    add test doc for membranes + solvent

---
+++ tests/doc_scripts/membranes_solvents.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+import ymero as ymr
+
+dt = 0.001
+rc = 1.0
+density = 8.0
+
+ranks  = (1, 1, 1)
+domain = (16.0, 16.0, 16.0)
+
+u = ymr.ymero(ranks, domain, dt, debug_level=3, log_filename='log')
+
+# create the particle vectors
+#############################
+
+# create MembraneVector for membranes
+mesh_rbc = ymr.ParticleVectors.MembraneMesh("rbc_mesh.off")
+
+pv_rbc = ymr.ParticleVectors.MembraneVector("rbc", mass=1.0, mesh=mesh_rbc)
+pos_qs = [[ 5.0,  5.0, 2.0,     1.0, 0.0, 0.0, 0.0], # we create 4 membranes
+          [11.0,  5.0, 6.0,     1.0, 0.0, 0.0, 0.0],
+          [5.0,  11.0, 10.0,    1.0, 0.0, 0.0, 0.0],
+          [11.0, 11.0, 14.0,    1.0, 0.0, 0.0, 0.0]]
+ic_rbc   = ymr.InitialConditions.Membrane(pos_qs)
+u.registerParticleVector(pv_rbc, ic_rbc)
+
+# create particleVector for outer solvent
+pv_outer = ymr.ParticleVectors.ParticleVector('pv_outer', mass = 1.0)
+ic_outer = ymr.InitialConditions.Uniform(density)
+u.registerParticleVector(pv_outer, ic_outer)
+
+# To create the inner solvent, we split the outer solvent (which originally occupies
+# the whole domain) into outer and inner solvent
+# This is done thanks to the belonging checker:
+inner_checker = ymr.BelongingCheckers.Mesh("inner_solvent_checker")
+
+# the checker needs to be registered, as any other object; it is associated to a given object vector
+u.registerObjectBelongingChecker(inner_checker, pv_rbc)
+
+# we can now apply the checker to create the inner PV
+pv_inner = u.applyObjectBelongingChecker(inner_checker, pv_outer, correct_every = 0, inside = "pv_inner")
+
+
+# interactions
+##############
+
+prms_rbc = {
+    "x0"     : 0.457,
+    "ka_tot" : 4900.0,
+    "kv_tot" : 7500.0,
+    "ka"     : 5000,
+    "ks"     : 0.0444 / 0.000906667,
+    "mpow"   : 2.0,
+    "gammaC" : 52.0,
+    "gammaT" : 0.0,
+    "kBT"    : 0.0,
+    "tot_area"   : 62.2242,
+    "tot_volume" : 26.6649,
+    "kb"     : 44.4444,
+    "theta"  : 6.97
+}
+
+int_rbc = ymr.Interactions.MembraneForces("int_rbc", "wlc", "Kantor", **prms_rbc)
+int_dpd = ymr.Interactions.DPD('dpd', rc, a=10.0, gamma=10.0, kbt=1.0, power=0.5)
+
+u.registerInteraction(int_rbc)
+u.registerInteraction(int_dpd)
+
+u.setInteraction(int_dpd, pv_outer, pv_outer)
+u.setInteraction(int_dpd, pv_inner, pv_inner)
+u.setInteraction(int_dpd, pv_inner, pv_outer)
+
+u.setInteraction(int_rbc, pv_rbc, pv_rbc)
+
+u.setInteraction(int_dpd, pv_outer, pv_rbc)
+u.setInteraction(int_dpd, pv_inner, pv_rbc)
+
+# some properties of the fluids are different, we can set this as follows
+# instead of creating another interaction object
+# note that the postprocess rank has int_dpd = None
+
+if u.isComputeTask():
+    int_dpd.setSpecificPair(pv_inner, pv_inner, gamma = 20.0)
+    int_dpd.setSpecificPair(pv_inner, pv_outer, gamma = 15.0)
+    int_dpd.setSpecificPair(pv_inner, pv_rbc, a = 0.0, gamma = 15.0)
+    int_dpd.setSpecificPair(pv_outer, pv_rbc, a = 0.0, gamma = 15.0)
+
+# integrators
+#############
+
+vv = ymr.Integrators.VelocityVerlet('vv')
+u.registerIntegrator(vv)
+
+u.setIntegrator(vv, pv_outer)
+u.setIntegrator(vv, pv_inner)
+u.setIntegrator(vv, pv_rbc)
+
+# Bouncers
+##########
+
+# The solvent must not go through the membrane
+# we can enforce this by setting a bouncer
+bouncer = ymr.Bouncers.Mesh("membrane_bounce", kbt=0.5)
+
+# we register the bouncer object as any other object
+u.registerBouncer(bouncer)
+
+# now we can set what PVs bounce on what OV:
+u.setBouncer(bouncer, pv_rbc, pv_outer)
+u.setBouncer(bouncer, pv_rbc, pv_inner)
+
+# plugins
+#########
+
+u.registerPlugins(ymr.Plugins.createStats('stats', every=500))
+
+dump_every = 500
+u.registerPlugins(ymr.Plugins.createDumpParticles('part_dump_inner', pv_inner, dump_every, [], 'h5/inner-'))
+u.registerPlugins(ymr.Plugins.createDumpParticles('part_dump_outer', pv_outer, dump_every, [], 'h5/outer-'))
+u.registerPlugins(ymr.Plugins.createDumpMesh("mesh_dump", pv_rbc, dump_every, "ply/"))
+
+u.run(5002)
