19898c05677668c5a4abb1bc85de6fdbcff9400d
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-03-08 10:14:00 +0100

    add rest test for doc tutorials

--- tests/doc_scripts/check.sh
+++ tests/doc_scripts/check.sh
@@ -9,5 +9,11 @@ echo $? > exit.out.txt
 # TEST: docs.hello
 cd doc_scripts/
 rm -f exit.out.txt
-ymr.run --runargs "-n 2" python hello.py > /dev/null
+ymr.run --runargs "-n 1" python hello.py > /dev/null
+echo $? > exit.out.txt
+
+# TEST: docs.rest
+cd doc_scripts/
+rm -f exit.out.txt
+ymr.run --runargs "-n 2" python rest.py > /dev/null
 echo $? > exit.out.txt
--- tests/doc_scripts/hello.py
+++ tests/doc_scripts/hello.py
@@ -8,6 +8,6 @@ ranks  = (1, 1, 1)          # number of ranks in x, y, z directions
 domain = (32.0, 16.0, 16.0) # domain size in x, y, z directions
 
 # create the coordinator
-u = ymr.ymero(ranks, domain, dt, debug_level=2, log_filename='log')
+u = ymr.ymero(ranks, domain, dt, debug_level=3, log_filename='log')
 
 u.run(100) # Run 100 time-steps
