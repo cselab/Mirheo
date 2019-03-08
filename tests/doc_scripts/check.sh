986a3dcce6ac2ebd31bae9e49bc9b6bd85fa0caa
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-03-08 12:25:06 +0100

    add wall for docs

--- tests/doc_scripts/check.sh
+++ tests/doc_scripts/check.sh
@@ -17,3 +17,9 @@ cd doc_scripts/
 rm -f exit.out.txt
 ymr.run --runargs "-n 2" python rest.py > /dev/null
 echo $? > exit.out.txt
+
+# TEST: docs.walls
+cd doc_scripts/
+rm -f exit.out.txt
+ymr.run --runargs "-n 2" python walls.py > /dev/null
+echo $? > exit.out.txt
