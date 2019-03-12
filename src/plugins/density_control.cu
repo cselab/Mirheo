e9f08d94aa9adb29e507083ed153950f1715e6dc
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-03-12 17:37:51 +0100

    wip: adding density control

---
+++ src/plugins/density_control.cu
@@ -0,0 +1,293 @@
+#include "density_control.h"
+
+#include <core/field/from_function.h>
+#include <core/field/utils.h>
+#include <core/pvs/particle_vector.h>
+#include <core/pvs/views/pv.h>
+#include <core/simulation.h>
+#include <core/utils/cuda_common.h>
+#include <core/utils/cuda_rng.h>
+#include <core/utils/kernel_launch.h>
+#include <core/utils/make_unique.h>
+
+namespace DensityControlPluginKernels
+{
+
+enum {INVALID_LEVEL=-1};
+
+__device__ int getLevelId(const FieldDeviceHandler& field, const float3& r,
+                           const DensityControlPlugin::LevelBounds& lb)
+{
+    float l = field(r);
+    return (l > lb.lo && l < lb.hi) ?
+        (l - lb.lo) / lb.space :
+        INVALID_LEVEL;
+}
+
+__global__ void initPIDs(int n, float Kp, float Ki, float Kd, PidControl<float> *controllers)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (i >= n) return;
+
+    const float initError = 0;
+    controllers[i] = PidControl<float>(initError, Kp, Ki, Kd);
+}
+
+__global__ void countInsideRegions(int nSamples, DomainInfo domain, FieldDeviceHandler field, DensityControlPlugin::LevelBounds lb,
+                                   float seed, unsigned long long int *nInsides)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (i >= nSamples) return;
+    
+    float3 r {Saru::uniform01(seed, i - 2, i + 4242),
+              Saru::uniform01(seed, i - 3, i + 4343),
+              Saru::uniform01(seed, i - 4, i + 4444)};
+
+    r = domain.localSize * (r - 0.5f);
+
+    int levelId = getLevelId(field, r, lb);
+    
+    if (levelId != INVALID_LEVEL)
+        atomicAdd(&nInsides[levelId], 1);
+}
+
+__global__ void computeVolumes(int nLevels, int nSamples, const unsigned long long int *nInsides, double subdomainVolume, double *volumes)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i >= nLevels) return;
+
+    double v = subdomainVolume * (double) nInsides[i] / (double) nSamples;
+    volumes[i] = v;
+}
+
+
+
+__global__ void collectSamples(PVview view, FieldDeviceHandler field, DensityControlPlugin::LevelBounds lb, unsigned long long int *nInsides)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i >= view.size) return;
+
+    Particle p;
+    p.readCoordinate(view.particles, i);
+
+    int levelId = getLevelId(field, p.r, lb);
+
+    if (levelId != INVALID_LEVEL)
+        atomicAdd(&nInsides[levelId], 1);
+}
+
+__global__ void addDensity(int n, const unsigned long long int *nInsides, const float *volumes, float *densities)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i >= n) return;
+
+    densities[i] += nInsides[i] / volumes[i];
+}
+
+
+__global__ void updateForces(int n, float targetDensity, float h, const float *densities, PidControl<float> *controllers, float *forces)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i >= n) return;
+
+    float rho_m = i > 0     ? densities[i-1] : targetDensity;
+    float rho_p = i < n - 1 ? densities[i+1] : targetDensity;
+
+    float error = (rho_p - rho_m) / (2.f*h);
+    
+    forces[i] = controllers[i].update(error);
+}
+
+__global__ void applyForces(PVview view, FieldDeviceHandler field, DensityControlPlugin::LevelBounds lb, const float *forces)
+{
+    const float h = 0.25f;
+    const float zeroTolerance = 1e-10f;
+    
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i >= view.size) return;
+
+    Particle p;    
+    p.readCoordinate(view.particles, i);
+
+    int levelId = getLevelId(field, p.r, lb);
+
+    if (levelId == INVALID_LEVEL) return;
+
+    float forceMagn = forces[levelId];
+
+    float3 grad = computeGradient(field, p.r, h);
+
+    if (dot(grad, grad) < zeroTolerance) return;
+
+    float3 force = normalize(grad) * forceMagn;
+
+    atomicAdd(view.forces + i, force);
+}
+
+} // namespace DensityControlPluginKernels
+
+DensityControlPlugin::DensityControlPlugin(const YmrState *state, std::string name,
+                                           std::vector<std::string> pvNames, float target_density,
+                                           RegionFunc region, float3 resolution,
+                                           float levelLo, float levelHi, float levelSpace,
+                                           float Kp, float Ki, float Kd,
+                                           int tuneEvery, int sampleEvery) :
+    SimulationPlugin(state, name),
+    pvNames(pvNames),
+    target_density(target_density),
+    spaceDecompositionField(std::make_unique<FieldFromFunction>
+                            (state, name + "_decomposition", region, resolution)),
+    levelBounds({levelLo, levelHi, levelSpace}),
+    Kp(Kp), Ki(Ki), Kd(Kd),
+    tuneEvery(tuneEvery),
+    sampleEvery(sampleEvery),
+    nSamples(0)
+{}
+
+DensityControlPlugin::~DensityControlPlugin() = default;
+
+void DensityControlPlugin::setup(Simulation *simulation, const MPI_Comm& comm, const MPI_Comm& interComm)
+{
+    SimulationPlugin::setup(simulation, comm, interComm);
+
+    for (auto &pvName : pvNames)
+        pvs.push_back(simulation->getPVbyNameOrDie(pvName));
+
+    spaceDecompositionField->setup(comm);
+
+    int nLevelSets = (levelBounds.hi - levelBounds.lo) / levelBounds.space;
+    levelBounds.space = (levelBounds.hi - levelBounds.lo) / nLevelSets;
+
+    controllers  .resize_anew(nLevelSets);
+    nInsides     .resize_anew(nLevelSets);    
+    densities    .resize_anew(nLevelSets);
+    forces       .resize_anew(nLevelSets);
+
+    volumes.resize(nLevelSets);
+    
+    computeVolumes(defaultStream, 1000000);
+
+    int nthreads = 128;
+    
+    SAFE_KERNEL_LAUNCH(
+        DensityControlPluginKernels::initPIDs,
+        getNblocks(nLevelSets, nthreads), nthreads, 0, defaultStream,
+        nLevelSets, Kp, Ki, Kd, controllers.devPtr());    
+
+    nInsides  .clearDevice(defaultStream);
+    densities .clearDevice(defaultStream);
+    forces    .clearDevice(defaultStream);
+}
+
+
+void DensityControlPlugin::beforeForces(cudaStream_t stream)
+{
+    if (state->currentStep % tuneEvery == 0)
+        updatePids(stream);
+
+    if (state->currentStep % sampleEvery == 0)
+        sample(stream);
+
+    applyForces(stream);
+}
+
+void DensityControlPlugin::computeVolumes(cudaStream_t stream, int MCnSamples)
+{
+    const int nthreads = 128;
+    float seed = 0.42424242f;
+    auto domain = state->domain;    
+    int nLevelSets = nInsides.size();
+
+    PinnedBuffer<double> localVolumes(nLevelSets);
+    
+    nInsides    .clearDevice(stream);
+    localVolumes.clearDevice(stream);
+    
+    SAFE_KERNEL_LAUNCH(
+        DensityControlPluginKernels::countInsideRegions,
+        getNblocks(MCnSamples, nthreads), nthreads, 0, stream,
+        MCnSamples, domain, spaceDecompositionField->handler(),
+        levelBounds, seed, nInsides.devPtr());
+
+    float3 L = domain.localSize;
+    double subdomainVolume = L.x * L.y * L.z;
+
+    SAFE_KERNEL_LAUNCH(
+        DensityControlPluginKernels::computeVolumes,
+        getNblocks(localVolumes.size(), nthreads), nthreads, 0, stream,
+        localVolumes.size(), MCnSamples, nInsides.devPtr(),
+        subdomainVolume, localVolumes.devPtr());
+
+    volumes.resize(nLevelSets);
+    
+    localVolumes.downloadFromDevice(stream);
+    
+    MPI_Check( MPI_Allreduce(localVolumes.hostPtr(), volumes.data(), volumes.size(), MPI_DOUBLE, MPI_SUM, comm) );
+}
+
+void DensityControlPlugin::sample(cudaStream_t stream)
+{
+    const int nthreads = 128;
+
+    for (auto pv : pvs)
+    {
+        PVview view(pv, pv->local());
+
+        SAFE_KERNEL_LAUNCH(
+            DensityControlPluginKernels::collectSamples,
+            getNblocks(view.size, nthreads), nthreads, 0, stream,
+            view, spaceDecompositionField->handler(),
+            levelBounds, nInsides.devPtr());        
+    }
+
+    ++nSamples;
+}
+
+void DensityControlPlugin::updatePids(cudaStream_t stream)
+{
+    const int nthreads = 128;
+    
+    nInsides.downloadFromDevice(stream);    
+    
+    MPI_Check( MPI_Allreduce(MPI_IN_PLACE, nInsides.hostPtr(), nInsides.size(),
+                             MPI_UNSIGNED_LONG_LONG, MPI_SUM, comm) );
+
+    for (int i = 0; i < volumes.size(); ++i)
+    {
+        double denom = volumes[i] * nSamples;
+
+        densities[i] = (denom > 1e-6) ? 
+            nInsides[i] / denom :
+            0.0;
+    }
+    
+    densities.uploadToDevice(stream);
+
+    SAFE_KERNEL_LAUNCH(
+        DensityControlPluginKernels::updateForces,
+        getNblocks(densities.size(), nthreads), nthreads, 0, stream,
+        densities.size(), target_density, levelBounds.space, densities.devPtr(),
+        controllers.devPtr(), forces.devPtr());
+
+    nInsides.clearDevice(stream);
+    nSamples = 0;
+}
+
+void DensityControlPlugin::applyForces(cudaStream_t stream)
+{
+    const int nthreads = 128;
+    
+    for (auto pv : pvs)
+    {
+        PVview view(pv, pv->local());
+
+        SAFE_KERNEL_LAUNCH(
+            DensityControlPluginKernels::applyForces,
+            getNblocks(view.size, nthreads), nthreads, 0, stream,
+            view, spaceDecompositionField->handler(),
+            levelBounds, forces.devPtr());        
+    }    
+}
+
---
+++ src/plugins/density_control.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "interface.h"
+#include "utils/pid.h"
+
+#include <core/containers.h>
+#include <core/datatypes.h>
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+class ParticleVector;
+class Field;
+
+class DensityControlPlugin : public SimulationPlugin
+{
+public:
+
+    using RegionFunc = std::function<float(float3)>;
+    
+    DensityControlPlugin(const YmrState *state, std::string name,
+                         std::vector<std::string> pvNames, float target_density,
+                         RegionFunc region, float3 resolution,
+                         float levelLo, float levelHi, float levelSpace,
+                         float Kp, float Ki, float Kd,
+                         int tuneEvery, int sampleEvery);
+    
+    ~DensityControlPlugin();
+
+    void setup(Simulation *simulation, const MPI_Comm& comm, const MPI_Comm& interComm) override;
+
+    void beforeForces(cudaStream_t stream) override;
+
+    bool needPostproc() override { return false; }    
+
+    struct LevelBounds
+    {
+        float lo, hi, space;
+    };
+    
+private:
+
+    int sampleEvery, tuneEvery;
+    std::vector<std::string> pvNames;
+    std::vector<ParticleVector*> pvs;
+
+    LevelBounds levelBounds;
+    float target_density;
+    
+    std::unique_ptr<Field> spaceDecompositionField; /// a scalar field used to decompose the space with level sets
+
+    int nSamples;                                   /// number of times we called sample function
+    PinnedBuffer<unsigned long long int> nInsides;  /// number of samples per subregion
+    std::vector<double> volumes;                    /// volume of each subregion
+
+    PinnedBuffer<float> densities;
+    DeviceBuffer<float> forces;
+    
+    DeviceBuffer<PidControl<float>> controllers;
+    float Kp, Ki, Kd;
+    
+private:
+
+    void computeVolumes(cudaStream_t stream, int MCnSamples);
+    void sample(cudaStream_t stream);
+    void updatePids(cudaStream_t stream);
+    void applyForces(cudaStream_t stream);
+};
+
+
