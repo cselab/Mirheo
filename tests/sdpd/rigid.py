9f731680e98386a5388e83047b27f4227d6f20a6
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-03-18 16:58:57 +0100

    add test for fsi sdpd object with halo; closes #46

--- tests/sdpd/rigid.py
+++ tests/sdpd/rigid.py
@@ -9,6 +9,7 @@ parser.add_argument('--density', type=float)
 parser.add_argument('--axes', type=float, nargs=3)
 parser.add_argument('--coords', type=str)
 parser.add_argument('--bounce_back', action='store_true', default=False)
+parser.add_argument('--xpos', type=float, default=0.5)
 args = parser.parse_args()
 
 dt       = 0.001
@@ -32,7 +33,7 @@ sdpd = ymr.Interactions.SDPD('sdpd', rc, viscosity=10.0, kBT=0.01, EOS="Linear",
 
 vv = ymr.Integrators.VelocityVerlet_withPeriodicForce('vv', force=dp_force, direction="x")
 
-com_q = [[0.5 * domain[0], 0.5 * domain[1], 0.5 * domain[2],   1., 0, 0, 0]]
+com_q = [[args.xpos * domain[0], 0.5 * domain[1], 0.5 * domain[2],   1., 0, 0, 0]]
 
 coords = np.loadtxt(args.coords).tolist()
 
@@ -59,7 +60,7 @@ u.setInteraction(den, pv_ell, pv_ell)
 u.setInteraction(sdpd, pv_sol, pv_sol)
 u.setInteraction(sdpd, pv_ell, pv_sol)
 
-belonging_checker = ymr.BelongingCheckers.Ellipsoid("ellipsoidChecker")
+belonging_checker = ymr.BelongingCheckers.Ellipsoid("ellipsoid_checker")
 
 u.registerObjectBelongingChecker(belonging_checker, pv_ell)
 u.applyObjectBelongingChecker(belonging_checker, pv=pv_sol, correct_every=0, inside="none", outside="")
@@ -69,9 +70,13 @@ if args.bounce_back:
     u.registerBouncer(bb)
     u.setBouncer(bb, pv_ell, pv_sol)
 
-# u.registerPlugins(ymr.Plugins.createParticleChannelSaver("density_saver", pv_ell, "densities", "den"))
-# u.registerPlugins(ymr.Plugins.createDumpParticles('partDump', pv_ell, 500, [["den", "scalar"]], 'h5/ell_particles-'))
-u.registerPlugins(ymr.Plugins.createDumpObjectStats("objStats", ov=pv_ell, dump_every=500, path="stats"))
+dump_every=500
+u.registerPlugins(ymr.Plugins.createParticleChannelSaver("density_saver", pv_ell, "densities", "den"))
+u.registerPlugins(ymr.Plugins.createDumpParticles('partDump', pv_ell, dump_every, [["den", "scalar"]], 'h5/ell_particles-'))
+u.registerPlugins(ymr.Plugins.createParticleChannelSaver("density_saver_sol", pv_sol, "densities", "den"))
+u.registerPlugins(ymr.Plugins.createDumpParticles('partDump_sol', pv_sol, dump_every, [["den", "scalar"]], 'h5/sol_particles-'))
+
+u.registerPlugins(ymr.Plugins.createDumpObjectStats("objStats", ov=pv_ell, dump_every=dump_every, path="stats"))
 
 u.run(10000)
 
@@ -84,4 +89,14 @@ u.run(10000)
 # rho=8.0; ax=2.0; ay=1.0; az=1.0
 # cp ../../data/ellipsoid_coords_${rho}_${ax}_${ay}_${az}.txt $f
 # ymr.run --runargs "-n 2" ./rigid.py --density $rho --axes $ax $ay $az --coords $f > /dev/null
+# cat stats/ellipsoid.txt | awk '{print $2, $6, $7, $8, $9}' > rigid.out.txt
+
+# nTEST: sdpd.fsi.ellipsoid.edge
+# set -eu
+# cd sdpd
+# rm -rf stats rigid.out.txt
+# f="pos.txt"
+# rho=8.0; ax=2.0; ay=1.0; az=1.0
+# cp ../../data/ellipsoid_coords_${rho}_${ax}_${ay}_${az}.txt $f
+# ymr.run --runargs "-n 2" ./rigid.py --density $rho --axes $ax $ay $az --coords $f --xpos 0.1 > /dev/null
 # cat stats/ellipsoid.txt | awk '{print $2, $6, $7, $8, $9}' > rigid.out.txt
