83ed1c99f6b77bed436bbd9eee9f7056e074e56e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-03-21 09:11:20 +0100

    add a test for rest with different mass

--- tests/sdpd/rest.py
+++ tests/sdpd/rest.py
@@ -4,6 +4,7 @@ import ymero as ymr
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--mass', type=float, default=1.0)
 parser.add_argument('--EOS', choices=["Linear", "QuasiIncompressible"], required=True)
 args = parser.parse_args()
 
@@ -15,7 +16,7 @@ domain = (16, 16, 16)
 
 u = ymr.ymero(ranks, domain, dt, debug_level=8, log_filename='log')
 
-pv = ymr.ParticleVectors.ParticleVector('pv', mass = 1)
+pv = ymr.ParticleVectors.ParticleVector('pv', args.mass)
 ic = ymr.InitialConditions.Uniform(density=10)
 u.registerParticleVector(pv=pv, ic=ic)
 
@@ -61,5 +62,11 @@ u.run(5001)
 # cd sdpd
 # rm -rf stats.txt
 # ymr.run --runargs "-n 2" ./rest.py --EOS QuasiIncompressible > /dev/null
+# cat stats.txt | awk '{print $1, $2, $3, $4, $5}' > stats.out.txt
+
+# nTEST: sdpd.rest.mass
+# cd sdpd
+# rm -rf stats.txt
+# ymr.run --runargs "-n 2" ./rest.py --EOS Linear --mass 5.0 > /dev/null
 # cat stats.txt | awk '{print $1, $2, $3, $4, $5}' > stats.out.txt
 
