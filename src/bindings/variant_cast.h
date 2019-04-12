a5d64ea8b1846b86fe3b4f90221820a35e4ef4e1
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-04-12 15:12:53 +0200

    add variant caster for pybind11

---
+++ src/bindings/variant_cast.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <extern/variant/include/mpark/variant.hpp>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+// provide cast for `mpark::variant` as explained in
+// https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html#c-17-library-containers
+
+namespace pybind11 {
+namespace detail {
+
+template <typename... Ts>
+struct type_caster<mpark::variant<Ts...>> : variant_caster<mpark::variant<Ts...>> {};
+
+template <>
+struct visit_helper<mpark::variant> {
+    template <typename... Args>
+    static auto call(Args &&...args) -> decltype(mpark::visit(args...)) {
+        return mpark::visit(args...);
+    }
+};
+
+}} // namespace pybind11::detail
