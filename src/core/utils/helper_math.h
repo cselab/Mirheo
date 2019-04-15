c93e15626f17f4e5b3046baeb377c1b734eb4a41
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-04-15 14:34:50 +0200

    more operators in helper_math

--- src/core/utils/helper_math.h
+++ src/core/utils/helper_math.h
@@ -317,7 +317,16 @@ inline __HD__ float2 operator+(float2 a, float2 b)
 {
     return make_float2(a.x + b.x, a.y + b.y);
 }
+inline __HD__ double2 operator+(double2 a, double2 b)
+{
+    return {a.x + b.x, a.y + b.y};
+}
 inline __HD__ void operator+=(float2 &a, float2 b)
+{
+    a.x += b.x;
+    a.y += b.y;
+}
+inline __HD__ void operator+=(double2 &a, double2 b)
 {
     a.x += b.x;
     a.y += b.y;
@@ -607,7 +616,16 @@ inline __HD__ float2 operator-(float2 a, float2 b)
 {
     return make_float2(a.x - b.x, a.y - b.y);
 }
+inline __HD__ double2 operator-(double2 a, double2 b)
+{
+    return {a.x - b.x, a.y - b.y};
+}
 inline __HD__ void operator-=(float2 &a, float2 b)
+{
+    a.x -= b.x;
+    a.y -= b.y;
+}
+inline __HD__ void operator-=(double2 &a, double2 b)
 {
     a.x -= b.x;
     a.y -= b.y;
