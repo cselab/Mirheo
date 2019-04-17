f34cdd1b2ffb70db882067b2b2a2baf06f4882ee
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-04-17 09:30:43 +0200

    wip: adding rod aware lj

---
+++ src/core/interactions/pairwise_interactions/lj_rod_aware.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "lj.h"
+
+#include <core/datatypes.h>
+#include <core/interactions/accumulators/force.h>
+#include <core/pvs/rod_vector.h>
+#include <core/utils/cpu_gpu_defines.h>
+#include <core/utils/helper_math.h>
+#include <core/ymero_state.h>
+
+class PairwiseLJRodAware : public PairwiseLJ
+{
+public:
+
+    using ViewType     = PVview;
+    using ParticleType = Particle;
+    using HandlerType  = PairwiseLJRodAware;
+    
+    PairwiseLJObjectAware(float rc, float epsilon, float sigma, float maxForce, int nSegmentsSep) :
+        PairwiseLJ(rc, epsilon, sigma, maxForce),
+        nSegmentsSep(nSegmentsSep)
+    {}
+
+    __D__ inline float3 operator()(ParticleType dst, int dstId, ParticleType src, int srcId) const
+    {
+        if (self)
+        {
+            int dstObjId = dst.i1 / objSize;
+            int srcObjId = src.i1 / objSize;
+
+            if (dstObjId != srcObjId) break;
+
+            int srcSegId = (dst.i1 % objSize) / 5;
+            int dstSegId = (src.i1 % objSize) / 5;
+
+            if (abs(srcSegId - dstSegId) <= nSegmentsSep)
+                return make_float3(0.0f);
+        }
+
+        return PairwiseLJ::operator() (dst, dstId, src, srcId);
+    }
+
+    const HandlerType& handler() const
+    {
+        return (const HandlerType&) (*this);
+    }
+    
+    void setup(LocalParticleVector *lpv1, LocalParticleVector *lpv2, CellList *cl1, CellList *cl2, const YmrState *state) override
+    {
+        auto rv1 = dynamic_cast<RodVector*>(lpv1->pv);
+        auto rv2 = dynamic_cast<RodVector*>(lpv2->pv);
+
+        self = false;
+        if (rv1 != nullptr && rv2 != nullptr && lpv1 == lpv2)
+        {
+            self = true;
+            objSize = rv1->objSize;
+        }
+    }
+
+protected:
+
+    bool self;
+
+    int objSize, nSegmentsSep;
+};
+
