44574160de70cad23b29f373089891cc7433369f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-06-12 11:35:08 +0200

    use TypeShift

--- src/core/exchangers/packers/shifter.h
+++ src/core/exchangers/packers/shifter.h
@@ -5,6 +5,7 @@
 #include <core/domain.h>
 #include <core/utils/cpu_gpu_defines.h>
 #include <core/utils/type_map.h>
+#include <core/utils/type_shift.h>
 
 struct Shifter
 {
@@ -23,37 +24,12 @@ struct Shifter
                            -domain.localSize.y * dir.y,
                            -domain.localSize.z * dir.z };
         
-            _shift(var, shift);
+            TypeShift::shift(var, shift);
         }
         return var;
     }
 
 private:
-
-    template <typename T>
-    __D__ inline void _shift(T& var, float3 shift) const {}
-
-    __D__ inline void _shift(float3&      var, float3 shift) const {_add(var,   shift);}
-    __D__ inline void _shift(float4&      var, float3 shift) const {_add(var,   shift);}
-    __D__ inline void _shift(double3&     var, float3 shift) const {_add(var,   shift);}
-    __D__ inline void _shift(double4&     var, float3 shift) const {_add(var,   shift);}
-    __D__ inline void _shift(RigidMotion& var, float3 shift) const {_add(var.r, shift);}
-
-    __D__ inline void _shift(COMandExtent& var, float3 shift) const
-    {
-        _add(var.com,  shift);
-        _add(var.low,  shift);
-        _add(var.high, shift);
-    }
-
-    template <typename T>
-    __D__ inline void _add(T& v, float3 s) const
-    {
-        v.x += s.x;
-        v.y += s.y;
-        v.z += s.z;
-    }
-    
     const bool needShift;
     const DomainInfo domain;
 };
