856eba5a185a508c0ed2b01748f671e4da40d221
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-06-14 15:42:34 +0200

    activate rod test

--- tests/bounce/rod/main.py
+++ tests/bounce/rod/main.py
@@ -12,14 +12,17 @@ dt = 0.001
 
 ranks  = (1, 1, 1)
 domain = (8, 8, 8)
-L = 4
+L = 8
 R = 0.5
-num_segments = 8
+num_segments = 4
+
+mass_sol = 0.1
+mass_rod = 100.0
 
 u = ymr.ymero(ranks, domain, dt, debug_level=3, log_filename='log', no_splash=True)
 
 nparts = 1000
-pos = np.random.normal(loc   = [0.5, 0.5 * domain[1], 0.5 * domain[2]],
+pos = np.random.normal(loc   = [0.5, 0.5 * domain[1] + 0.5, 0.5 * domain[2]],
                        scale = [0.1, 0.3, 0.3],
                        size  = (nparts, 3))
 
@@ -28,7 +31,7 @@ vel = np.random.normal(loc   = [1.0, 0., 0.],
                        size  = (nparts, 3))
 
 
-pv_solvent = ymr.ParticleVectors.ParticleVector('pv', mass = 1)
+pv_solvent = ymr.ParticleVectors.ParticleVector('pv', mass_sol)
 ic_solvent = ymr.InitialConditions.FromArray(pos=pos.tolist(), vel=vel.tolist())
 vv         = ymr.Integrators.VelocityVerlet('vv')
 u.registerParticleVector(pv_solvent, ic_solvent)
@@ -44,8 +47,8 @@ l0 = L / num_segments
 #com_q = [[0.5 * domain[0], 0.5 * domain[1], 0.5 * domain[2],   0.7071, 0.0, 0.7071, 0.0]]
 com_q = [[0.5 * domain[0], 0.5 * domain[1], 0.5 * domain[2],   1.0, 0.0, 0.0, 0.0]]
 
-pv_rod = ymr.ParticleVectors.RodVector('rod', mass=1, num_segments = num_segments)
-ic_rod = ymr.InitialConditions.Rod(com_q, center_line, torsion, l0)
+pv_rod = ymr.ParticleVectors.RodVector('rod', mass_rod, num_segments)
+ic_rod = ymr.InitialConditions.Rod(com_q, center_line, torsion, 2*R)
 u.registerParticleVector(pv_rod, ic_rod)
 
 u.setIntegrator(vv, pv_rod)
@@ -58,7 +61,8 @@ if args.vis:
     dump_every = int(0.1 / dt)
     u.registerPlugins(ymr.Plugins.createDumpParticles('solvent_dump', pv_solvent, dump_every, [], 'h5/solvent-'))
     u.registerPlugins(ymr.Plugins.createDumpParticles('rod_dump', pv_rod, dump_every, [], 'h5/rod-'))
-
+    u.registerPlugins(ymr.Plugins.createStats('stats', "stats.txt", dump_every))
+    
 tend = int(5.0 / dt)
     
 u.run(tend)
@@ -68,9 +72,9 @@ if pv_rod is not None:
     np.savetxt("pos.rod.txt", rod_pos)
 
 
-# sTEST: bounce.rod
+# nTEST: bounce.rod
 # set -eu
 # cd bounce/rod
 # rm -rf pos.rod.txt pos.rod.out.txt 
-# ymr.run --runargs "-n 2" ./main.py --vis
+# ymr.run --runargs "-n 2" ./main.py
 # mv pos.rod.txt pos.rod.out.txt 
