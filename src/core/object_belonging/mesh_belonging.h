41ddc417a4e8ae19bda0504878236b1095920b9f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-06-14 18:12:26 +0200

    mino cleanup

--- src/core/object_belonging/mesh_belonging.h
+++ src/core/object_belonging/mesh_belonging.h
@@ -7,7 +7,5 @@ class MeshBelongingChecker : public ObjectBelongingChecker_Common
 public:
     using ObjectBelongingChecker_Common::ObjectBelongingChecker_Common;
 
-    void tagInner(ParticleVector* pv, CellList* cl, cudaStream_t stream) override;
-
-    virtual ~MeshBelongingChecker() = default;
+    void tagInner(ParticleVector *pv, CellList *cl, cudaStream_t stream) override;
 };
