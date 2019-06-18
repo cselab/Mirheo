dae8a5ea9279b199606bdbe02b8d7bee15425639
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-06-18 15:41:18 +0200

    add factory to help creating rigid pvs

---
+++ src/core/pvs/factory.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "rigid_ashape_object_vector.h"
+
+#include <core/analytical_shapes/api.h>
+#include <core/utils/pytypes.h>
+
+#include <memory>
+
+namespace ParticleVectorFactory
+{
+
+static std::shared_ptr<RigidShapedObjectVector<Ellipsoid>>
+createEllipsoidROV(const YmrState *state, std::string name, float mass, int objSize, PyTypes::float3 axes)
+{
+    Ellipsoid ell(make_float3(axes));
+    return std::make_shared<RigidShapedObjectVector<Ellipsoid>>
+        (state, name, mass, objSize, ell);
+}
+
+static std::shared_ptr<RigidShapedObjectVector<Ellipsoid>>
+createEllipsoidROV(const YmrState *state, std::string name, float mass, int objSize, PyTypes::float3 axes, std::shared_ptr<Mesh> mesh)
+{
+    Ellipsoid ell(make_float3(axes));
+    return std::make_shared<RigidShapedObjectVector<Ellipsoid>>
+        (state, name, mass, objSize, ell, std::move(mesh));
+}
+
+
+static std::shared_ptr<RigidShapedObjectVector<Cylinder>>
+createCylinderROV(const YmrState *state, std::string name, float mass, int objSize, float R, float L)
+{
+    Cylinder cyl(R, L);
+    return std::make_shared<RigidShapedObjectVector<Cylinder>>
+        (state, name, mass, objSize, cyl);
+}
+
+static std::shared_ptr<RigidShapedObjectVector<Cylinder>>
+createCylinderROV(const YmrState *state, std::string name, float mass, int objSize, float R, float L, std::shared_ptr<Mesh> mesh)
+{
+    Cylinder cyl(R, L);
+    return std::make_shared<RigidShapedObjectVector<Cylinder>>
+        (state, name, mass, objSize, cyl, std::move(mesh));
+}
+
+
+} // namespace ParticleVectorFactory
