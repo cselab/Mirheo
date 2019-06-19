c3fcd19485f10d40dbfe0bcc62bd341937e233f3
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-06-19 10:16:20 +0200

    add capsules ROVs to factory

--- src/core/pvs/factory.h
+++ src/core/pvs/factory.h
@@ -10,21 +10,22 @@
 namespace ParticleVectorFactory
 {
 
-static std::shared_ptr<RigidShapedObjectVector<Ellipsoid>>
-createEllipsoidROV(const YmrState *state, std::string name, float mass, int objSize, PyTypes::float3 axes)
+static std::shared_ptr<RigidShapedObjectVector<Capsule>>
+createCapsuleROV(const YmrState *state, std::string name, float mass, int objSize, float R, float L)
 {
-    Ellipsoid ell(make_float3(axes));
-    return std::make_shared<RigidShapedObjectVector<Ellipsoid>>
-        (state, name, mass, objSize, ell);
+    Capsule cap(R, L);
+    return std::make_shared<RigidShapedObjectVector<Capsule>>
+        (state, name, mass, objSize, cap);
 }
 
-static std::shared_ptr<RigidShapedObjectVector<Ellipsoid>>
-createEllipsoidROVWithMesh(const YmrState *state, std::string name, float mass, int objSize, PyTypes::float3 axes, std::shared_ptr<Mesh> mesh)
+static std::shared_ptr<RigidShapedObjectVector<Capsule>>
+createCapsuleROVWithMesh(const YmrState *state, std::string name, float mass, int objSize, float R, float L, std::shared_ptr<Mesh> mesh)
 {
-    Ellipsoid ell(make_float3(axes));
-    return std::make_shared<RigidShapedObjectVector<Ellipsoid>>
-        (state, name, mass, objSize, ell, std::move(mesh));
+    Capsule cap(R, L);
+    return std::make_shared<RigidShapedObjectVector<Capsule>>
+        (state, name, mass, objSize, cap, std::move(mesh));
 }
+
 
 
 static std::shared_ptr<RigidShapedObjectVector<Cylinder>>
@@ -43,5 +44,22 @@ createCylinderROVWithMesh(const YmrState *state, std::string name, float mass, i
         (state, name, mass, objSize, cyl, std::move(mesh));
 }
 
+
+
+static std::shared_ptr<RigidShapedObjectVector<Ellipsoid>>
+createEllipsoidROV(const YmrState *state, std::string name, float mass, int objSize, PyTypes::float3 axes)
+{
+    Ellipsoid ell(make_float3(axes));
+    return std::make_shared<RigidShapedObjectVector<Ellipsoid>>
+        (state, name, mass, objSize, ell);
+}
+
+static std::shared_ptr<RigidShapedObjectVector<Ellipsoid>>
+createEllipsoidROVWithMesh(const YmrState *state, std::string name, float mass, int objSize, PyTypes::float3 axes, std::shared_ptr<Mesh> mesh)
+{
+    Ellipsoid ell(make_float3(axes));
+    return std::make_shared<RigidShapedObjectVector<Ellipsoid>>
+        (state, name, mass, objSize, ell, std::move(mesh));
+}
 
 } // namespace ParticleVectorFactory
