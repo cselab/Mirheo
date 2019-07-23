7172f03361ce9ace5b946d448dfea88f465a226f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-07-23 14:47:58 +0200

    minor: add docs in folder.h

--- src/core/utils/folders.h
+++ src/core/utils/folders.h
@@ -1,15 +1,15 @@
 #pragma once
 
+#include <mpi.h>
 #include <string>
 #include <vector>
-#include <mpi.h>
 
 std::string getStrZeroPadded(int i, int zeroPadding = 5);
 
 std::vector<std::string> splitByDelim(std::string str, char delim = ',');
 
-std::string makePath(std::string path);
-std::string parentPath(std::string path);
+std::string makePath    (std::string path); ///< append '/' at the end of 'path' if needed
+std::string parentPath  (std::string path);
 std::string relativePath(std::string path);
 
 bool createFoldersCollective(const MPI_Comm& comm, std::string path);
