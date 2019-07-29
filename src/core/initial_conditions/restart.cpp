81dacb36f5cacac96770c725dbccfbfa27ec8773
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-07-29 16:01:44 +0200

    minor

--- src/core/initial_conditions/restart.cpp
+++ src/core/initial_conditions/restart.cpp
@@ -1,12 +1,15 @@
 #include "restart.h"
 #include <core/pvs/particle_vector.h>
 
-RestartIC::RestartIC(std::string path) : path(path)
-{   }
+RestartIC::RestartIC(std::string path) :
+    path(path)
+{}
 
-void RestartIC::exec(const MPI_Comm& comm, ParticleVector* pv, cudaStream_t stream)
+RestartIC::~RestartIC() = default;
+
+void RestartIC::exec(const MPI_Comm& comm, ParticleVector *pv, cudaStream_t stream)
 {
     pv->restart(comm, path);
 }
 
-RestartIC::~RestartIC() = default;
+
--- src/core/initial_conditions/restart.h
+++ src/core/initial_conditions/restart.h
@@ -4,19 +4,13 @@
 
 #include <string>
 
-/**
- * Initialize ParticleVector by reading the restart file.
- * \sa ParticleVector::checkpoint
- * \sa ParticleVector::restart
- */
 class RestartIC : public InitialConditions
 {
 public:
     RestartIC(std::string path);
-
-    void exec(const MPI_Comm& comm, ParticleVector* pv, cudaStream_t stream) override;
-
     ~RestartIC();
+    
+    void exec(const MPI_Comm& comm, ParticleVector *pv, cudaStream_t stream) override;
     
 private:
     std::string path;
