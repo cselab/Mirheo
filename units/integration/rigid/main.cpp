30bf895c60ad06ef9c7c0b6ab8b10faa40acc63c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-08-07 17:44:05 +0200

    wip: adding integration rigid unit test

---
+++ units/integration/rigid/main.cpp
@@ -0,0 +1,18 @@
+#include <core/logger.h>
+
+#include <cmath>
+#include <cstdio>
+#include <gtest/gtest.h>
+
+Logger logger;
+
+TEST (Integration_rigids, Analytic)
+{
+    // TODO
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
