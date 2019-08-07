1785ff70abc271de38ad49c16081881e4d7988e8
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-08-07 16:43:04 +0200

    use __HD__; pass by const ref

--- src/core/rigid/utils.h
+++ src/core/rigid/utils.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <core/datatypes.h>
+#include <core/utils/cpu_gpu_defines.h>
 
-inline __host__ __device__ SingleRigidMotion toSingleMotion(DoubleRigidMotion dm)
+inline __HD__ SingleRigidMotion toSingleMotion(const DoubleRigidMotion& dm)
 {
     SingleRigidMotion sm;
     sm.r      = make_float3(dm.r);
@@ -15,19 +16,19 @@ inline __host__ __device__ SingleRigidMotion toSingleMotion(DoubleRigidMotion dm
     return sm;
 }
 
-inline __host__ __device__ SingleRigidMotion toSingleMotion(SingleRigidMotion sm)
+inline __HD__ SingleRigidMotion toSingleMotion(const SingleRigidMotion& sm)
 {
     return sm;
 }
 
 template<class R3>
-inline __host__ __device__ RigidReal3 make_rigidReal3(R3 a)
+inline __HD__ RigidReal3 make_rigidReal3(R3 a)
 {
     return {RigidReal(a.x), RigidReal(a.y), RigidReal(a.z)};
 }
 
 template<class R4>
-inline __host__ __device__ RigidReal4 make_rigidReal4(R4 a)
+inline __HD__ RigidReal4 make_rigidReal4(R4 a)
 {
     return {RigidReal(a.x), RigidReal(a.y), RigidReal(a.z), RigidReal(a.w)};
 }
