8ce68ec4d0c351ae0e12eedeb143ce1b3a7ebdb8
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-08-15 17:58:45 +0200

    minor

--- src/core/utils/root_finder.h
+++ src/core/utils/root_finder.h
@@ -15,7 +15,7 @@ __D__ inline float2 linearSearchVerbose(Equation F, float a = 0.0f, float b = 1.
     // coordinate is inside at the current time, or outside
     // Sign mapping to inside/outside is irrelevant
 
-    const int maxNIters = 20;
+    constexpr int maxNIters = 20;
 
     float va = F(a);
     float vb = F(b);
@@ -25,8 +25,7 @@ __D__ inline float2 linearSearchVerbose(Equation F, float a = 0.0f, float b = 1.
     // Check if the collision is there in the first place
     if (va*vb > 0.0f) return make_float2(-1.0f);
 
-    int iters;
-    for (iters=0; iters<maxNIters; iters++)
+    for (int iter = 0; iter < maxNIters; ++iter)
     {
         const float lambda = min( max(vb / (vb - va),  0.1f), 0.9f );  // va*l + (1-l)*vb = 0
         mid = a *lambda + b *(1.0f - lambda);
@@ -52,18 +51,18 @@ __D__ inline float2 linearSearchVerbose(Equation F, float a = 0.0f, float b = 1.
 template <typename Equation>
 __D__ inline float linearSearch(Equation F, float a = 0.0f, float b = 1.0f, float tolerance = 1e-6f)
 {
-    float2 res = linearSearchVerbose(F, a, b, tolerance);
+    const float2 res = linearSearchVerbose(F, a, b, tolerance);
     return res.x;
 }
 
 template<typename F, typename F_prime>
 __D__ inline float2 newton(F f, F_prime f_prime, float x0, float tolerance = 1e-6f)
 {
-    const int maxNIters = 10;
+    constexpr int maxNIters = 10;
 
-    float x = x0;
+    float x {x0};
     float val;
-    for (int iter=0; iter<maxNIters; iter++)
+    for (int iter = 0; iter < maxNIters; ++iter)
     {
         val = f(x);
         if (fabsf(val) < tolerance) return {x, val};
