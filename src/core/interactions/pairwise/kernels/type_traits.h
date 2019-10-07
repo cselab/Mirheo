2ea2ada7d48cf6f7975afe6c5ab900596e74f55e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-10-07 15:48:04 +0200

    adding traits to control in/outputs and prerequisites of pairwise interactions

--- src/core/interactions/pairwise/kernels/type_traits.h
+++ src/core/interactions/pairwise/kernels/type_traits.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "density.h"
+#include "mdpd.h"
+#include "sdpd.h"
 
 template <typename T>
 struct needSelfInteraction
@@ -27,3 +29,31 @@ struct outputsForce<PairwiseDensity<T>>
 {
     static constexpr bool value = false;
 };
+
+
+
+template <class T>
+struct outputsDensity
+{
+    static constexpr bool value = !outputsForce<T>::value;
+};
+
+
+template <class T>
+struct requiresDensity
+{
+    static constexpr bool value = false;
+};
+
+template<>
+struct requiresDensity<PairwiseMDPD>
+{
+    static constexpr bool value = true;
+};
+
+template <class T0, class T1>
+struct requiresDensity<PairwiseSDPD<T0, T1>>
+{
+    static constexpr bool value = true;
+};
+
