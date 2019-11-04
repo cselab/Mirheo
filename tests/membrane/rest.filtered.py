d978946af4e70ad30b04bb9b1e89defea2a37b05
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-11-04 16:20:01 +0100

    adapt tests

--- tests/membrane/rest.filtered.py
+++ tests/membrane/rest.filtered.py
@@ -42,11 +42,7 @@ u.setIntegrator(vv, pv_rbc)
 u.registerInteraction(int_rbc)
 u.setInteraction(int_rbc, pv_rbc, pv_rbc)
 
-
-type_ids_str = "type_ids"
-u.registerPlugins(mir.Plugins.createScatterObjectData("type_id_copier", pv_rbc, "membrane_type_id", type_ids_str))
-u.registerPlugins(mir.Plugins.createDumpParticlesWithMesh("mesh_dump", pv_rbc, 150, [type_ids_str], "h5/mesh-"))
-#u.registerPlugins(mir.Plugins.createDumpMesh("mesh_dump", pv_rbc, 150, "ply/"))
+u.registerPlugins(mir.Plugins.createDumpParticlesWithMesh("mesh_dump", pv_rbc, 150, ["membrane_type_id"], "h5/mesh-"))
 
 u.run(5000)
 
