1e290381559b149bed54c9fc77fcd18bbbf93ce6
Ivica Kicic <kicici@ethz.ch>
2019-11-21 10:20:25 +0100

    CMAKE_SOURCE_DIR -> CMAKE_CURRENT_SOURCE_DIR

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -2,7 +2,7 @@
 macro(getMirheoVersion version_str version_number)
   execute_process(COMMAND
     git describe --abbrev=0
-    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
     OUTPUT_VARIABLE ${version_str}
     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
   string(REPLACE "v" "" ${version_number} ${${version_str}})
@@ -12,7 +12,7 @@ endmacro()
 macro(getMirheoSHA1 sha1)
   execute_process(COMMAND
     git describe --match=NeVeRmAtCh --always --abbrev=40 --dirty
-    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
     OUTPUT_VARIABLE ${sha1}
     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 endmacro()
