6ac0a11656c5dcbb2a7306df042e707464e622e4
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-01-31 18:13:08 +0100

    make exchangers/utils/map a class

--- src/mirheo/core/exchangers/utils/map.h
+++ src/mirheo/core/exchangers/utils/map.h
@@ -7,8 +7,32 @@
 namespace mirheo
 {
 
-struct __align__(4) MapEntry
+class __align__(4) MapEntry
 {
+ public:
+    __HD__ MapEntry() = delete;
+    __HD__ MapEntry(uint32_t id, uint32_t bufId) :
+        i_((maskId & id) | (bufId << bufShift))
+    {}
+    
+    __HD__ inline uint32_t getId()    const {return i_ & maskId;}
+    __HD__ inline uint32_t getBufId() const {return i_ >> bufShift;}
+    
+    __HD__ inline void setId(uint32_t id)
+    {
+        uint32_t bufInfo = maskBuf & i_;
+        i_ = bufInfo | id;
+    }
+
+    __HD__ inline void setBufId(uint32_t bufId)
+    {
+        uint32_t  idInfo = maskId & i_;
+        uint32_t bufInfo = bufId << bufShift;
+        i_ = bufInfo | idInfo;
+    }
+
+ private:
+
     // 27 < 2^5 buffers max
     static constexpr int bufWidth = 5;
     static constexpr int bufShift = 32 - bufWidth;
@@ -16,28 +40,7 @@ struct __align__(4) MapEntry
     static constexpr uint32_t maskId  = (maskAll << bufWidth) >> bufWidth;
     static constexpr uint32_t maskBuf = ~maskId;
     
-    uint32_t i {0};
-
-    __HD__ MapEntry() = delete;
-    __HD__ MapEntry(uint32_t id, uint32_t bufId) :
-        i((maskId & id) | (bufId << bufShift))
-    {}
-    
-    __HD__ inline uint32_t getId()    const {return i & maskId;}
-    __HD__ inline uint32_t getBufId() const {return i >> bufShift;}
-    
-    __HD__ inline void setId(uint32_t id)
-    {
-        uint32_t bufInfo = maskBuf & i;
-        i = bufInfo | id;
-    }
-
-    __HD__ inline void setBufId(uint32_t bufId)
-    {
-        uint32_t  idInfo = maskId & i;
-        uint32_t bufInfo = bufId << bufShift;
-        i = bufInfo | idInfo;
-    }
+    uint32_t i_ {0};
 };
 
 inline __HD__ int dispatchThreadsPerBuffer(int nBuffers, const int *offsets, int tid)
