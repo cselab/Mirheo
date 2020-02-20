65eab6b2f83da35b1eb7214d02f73074fd743b43
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-02-20 13:37:07 +0100

    add docs to mirheo state

--- src/mirheo/core/mirheo_state.h
+++ src/mirheo/core/mirheo_state.h
@@ -10,32 +10,49 @@
 namespace mirheo
 {
 
-/**
- * Global quantities accessible by all simulation objects in Mirheo
+/** \brief Global quantities accessible by all simulation objects in Mirheo
  */
 class MirState
 {
 public:
-    using TimeType = double;
-    using StepType = long long;
+    using TimeType = double; ///< type used to store time information
+    using StepType = long long; ///< type to store time step information
 
+    /** \brief Construct a MirState object
+        \param [in] domain The DomainInfo of the simulation
+        \param [in] dt Simulation time step
+        \param [in] state If not \nullptr, will set the current time info from snapshot info
+    */
     MirState(DomainInfo domain, real dt, const ConfigValue *state = nullptr);
+
+    /// copy constructor
     MirState(const MirState&);
+    /// assignment operator
     MirState& operator=(MirState other);
 
     virtual ~MirState();
 
+    /// swap the content of this object with that of \p other
     void swap(MirState& other);
 
-    void checkpoint(MPI_Comm comm, std::string path);  /// Save state to file
-    void restart   (MPI_Comm comm, std::string path);  /// Restore state from file
+    /** Save internal state to file
+        \param [in] comm MPI comm of the simulation 
+        \param [in] path The directory in which to save the file
+     */
+    void checkpoint(MPI_Comm comm, std::string path);
+
+    /** Load internal state from file
+        \param [in] comm MPI comm of the simulation 
+        \param [in] path The directory from which to load the file
+     */
+    void restart(MPI_Comm comm, std::string path);
 
 public:
-    DomainInfo domain;
+    DomainInfo domain; ///< Global DomainInfo
 
-    real dt;
-    TimeType currentTime;
-    StepType currentStep;
+    real dt; ///< time step
+    TimeType currentTime; ///< Current simulation time
+    StepType currentStep; ///< Current simulation step
 };
 
 template <>
