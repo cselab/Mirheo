391a8a2a95caf9d84ab88a7efecae1d2ecb5371c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-03-12 17:18:47 +0100

    wip: more doxygen comments in utils

--- src/mirheo/core/utils/strprintf.h
+++ src/mirheo/core/utils/strprintf.h
@@ -5,6 +5,10 @@
 namespace mirheo
 {
 
+/** \brief Create a std::string from a printf-style formatting
+    \param [in] fmt The c-style string that represents the format of the string, followed by extra arguments (see printf docs)
+    \return the std::string corresponding to the given format evaluated with the passed extra arguments
+ */
 std::string strprintf [[gnu::format(printf, 1, 2)]] (const char *fmt, ...);
 
 } // namespace mirheo
--- src/mirheo/core/utils/timer.h
+++ src/mirheo/core/utils/timer.h
@@ -5,11 +5,14 @@
 namespace mirheo
 {
 
+/** \brief Measure wall time: profiling tool.
+    \tparam Ratio a type of std::ratio, used to set the units of time durations (e.g. s, ms, us)
+ */
 template<typename Ratio>
 class Timer
 {
 public:
-
+    /// Construct a timer object
     Timer() :
         start_ {_none()},
         end_   {_none()}
@@ -17,23 +20,36 @@ public:
         static_assert(std::chrono::__is_ratio<Ratio>::value, "timer must be specialized with ratio");
     }
 
+    /// Start the wall clock
     void start()
     {
         start_ = Clock::now();
         end_   = _none();
     }
 
+    /// Stop the wall clock and record the wall time
     void stop()
     {
         end_ = Clock::now();
     }
 
+    /** Compute the elapsed time between the previous start() and stop() calls.
+        If no stop() was called, computes the duration between start() and the current call.
+        This call does not reset the start time stamp, so it can be called later again.
+        \note start() must have been called at least once before this called.
+
+        \return The ellapsed time
+    */
     double elapsed()
     {
         if (end_ == _none()) end_ = Clock::now();
         return std::chrono::duration <double, Ratio>(end_ - start_).count();
     }
 
+    /** \return see ellapsed()
+
+        This will also reset the start timestamp to the current time.
+     */
     double elapsedAndReset()
     {
         const double t = elapsed();
@@ -54,8 +70,8 @@ private:
     Time end_;
 };
 
-using uTimer = Timer<std::micro>;
-using mTimer = Timer<std::milli>;
-using sTimer = Timer<std::ratio<1, 1>>;
+using uTimer = Timer<std::micro>; ///< timer which measures time in us
+using mTimer = Timer<std::milli>; ///< timer which measures time in ms
+using sTimer = Timer<std::ratio<1, 1>>; ///< timer which measures time in s
 
 } // namespace mirheo
