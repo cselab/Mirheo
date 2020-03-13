1b52b5f37a2a2df9176ca55d02244014b6615441
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2020-03-13 09:12:14 +0100

    remove unused functions in cuda_rng

--- src/mirheo/core/utils/cuda_rng.h
+++ src/mirheo/core/utils/cuda_rng.h
@@ -24,10 +24,12 @@ namespace mirheo
 {
 
 #ifndef __NVCC__
+/// fused multiply - add, single precision
 float __fmaf_rz(float x, float y, float z)
 {
     return x*y + z;
 }
+/// fused multiply - add, double precision
 double __fma_rz(double x, double y, double z)
 {
     return x*y + z;
@@ -44,38 +46,6 @@ __D__ real mean0var1( real seed, real i, real j );
 
 namespace Logistic
 {
-/************************* Trunk generator ***********************
- * Make one global random number per each timestep
- * cite G. Marsaglia
- * passes BigCrush
- *****************************************************************/
-struct KISS {
-    using intType = uint32_t;
-    intType x, y, z, c;
-
-    KISS() : x( 0 ), y( 0 ), z( 0 ), c( 0 ) {}
-
-    KISS( intType x_, intType y_, intType z_, intType c_ ) :
-        x( x_ ), y( y_ ), z( z_ ), c( c_ ) {}
-
-    real get_real()
-    {
-        return static_cast<real>(get_int()) / static_cast<real>(std::numeric_limits<intType>::max());
-    }
-
-    intType get_int()
-    {
-        uint64_t t, a = 698769069ULL;
-        x = 69069 * x + 12345;
-        y ^= ( y << 13 );
-        y ^= ( y >> 17 );
-        y ^= ( y << 5 ); /* y must never be set to zero! */
-        t = a * z + c;
-        c = static_cast<intType>( t >> 32 ); /* Also avoid setting z=c=0! */
-        return x + y + ( z = static_cast<intType>(t) );
-    }
-};
-
 /************************* Branch generator **********************
  * Make one random number per pair of particles per timestep
  * Based on the Logistic map on interval [-1,1]
@@ -100,53 +70,20 @@ inline __D__ double FMA( double x, double y, double z ) {
 // <3> : 4 FMA + 1 MUL
 // <2> : 2 FMA + 1 MUL
 // <1> : 1 FMA + 1 MUL
-#if 0
-template<int N> inline __D__ real __logistic_core( real x ) {
-    real x2;
-    // saturated square
-    // clamp result to [0,1]
-    // to cancel error introduced by logistic<3> \in [-1.000001, 1.000003]
-    asm("mul.f32.sat %0, %1, %1;" : "=f"(x2) : "f"(x), "f"(x) );
-    real r = FMA( FMA( FMA( FMA( 128.0_r, x2, -256.0_r ), x2, 160.0_r ), x2, -32.0_r ), x2, 1.0_r );
-    return __logistic_core < N - 3 > ( r );
-}
-template<int N> struct __logistic_core_flops_counter {
-    const static unsigned long long FLOPS = 9 + __logistic_core_flops_counter<N-3>::FLOPS;
-};
-
-template<> inline __D__ real __logistic_core<2>( real x ) {
-    real x2 = x * x;
-    return FMA( FMA( 8.0_r, x2, -8.0_r ), x2, 1.0_r );
-}
-template<> struct __logistic_core_flops_counter<2> {
-    const static unsigned long long FLOPS = 5;
-};
-
-#else
 template<int N> inline __D__ real __logistic_core( real x )
 {
     real x2 = x * x;
     real r = FMA( FMA( 8.0_r, x2, -8.0_r ), x2, 1.0_r );
     return __logistic_core < N - 2 > ( r );
 }
-template<int N> struct __logistic_core_flops_counter {
-    const static unsigned long long FLOPS = 5 + __logistic_core_flops_counter<N-2>::FLOPS;
-};
-#endif
 
 template<> inline __D__ real __logistic_core<1>( real x ) {
     return FMA( 2.0_r * x, x, -1.0_r );
 }
-template<> struct __logistic_core_flops_counter<1> {
-    const static unsigned long long FLOPS = 3;
-};
 
 template<> inline __D__ real __logistic_core<0>( real x ) {
     return x;
 }
-template<> struct __logistic_core_flops_counter<0> {
-    const static unsigned long long FLOPS = 0;
-};
 
 // random number from the ArcSine distribution on [-sqrt(2),sqrt(2)]
 // mean = 0
@@ -154,62 +91,59 @@ template<> struct __logistic_core_flops_counter<0> {
 // can be used directly for DPD
 
 // passes of logistic map
-const static int N = 18;
-// spacing coefficints for low discrepancy numbers
-const static real gold   = 0.6180339887498948482_r;
-const static real hugegold   = 0.6180339887498948482E39_r;
-const static real silver = 0.4142135623730950488_r;
-const static real hugesilver = 0.4142135623730950488E39_r;
-const static real bronze = 0.00008877875787352212838853023_r;
-const static real tin    = 0.00004602357186447026756768986_r;
+constexpr int N = 18;
+// spacing coefficients for low discrepancy numbers
+constexpr real gold       = 0.6180339887498948482_r;
+constexpr real hugegold   = 0.6180339887498948482E39_r;
+constexpr real silver     = 0.4142135623730950488_r;
+constexpr real hugesilver = 0.4142135623730950488E39_r;
+constexpr real bronze     = 0.00008877875787352212838853023_r;
+constexpr real tin        = 0.00004602357186447026756768986_r;
 // square root of 2
-const static real sqrt2 = 1.41421356237309514547_r;
+constexpr real sqrt2 = 1.41421356237309514547_r;
 
 inline __D__ real uniform01( real seed, int i, int j )
 {
-    real val = mean0var1(seed, i, j) * (0.5_r/sqrt2) + 0.5_r;
+    const real val = mean0var1(seed, i, j) * (0.5_r/sqrt2) + 0.5_r;
     return math::max(0.0_r, math::min(1.0_r, val));
 }
 
 inline __D__ real mean0var1( real seed, int u, int v )
 {
-    real p = rem( ( ( u & 0x3FF ) * gold ) + u * bronze + ( ( v & 0x3FF ) * silver ) + v * tin ); // safe for large u or v
-    real q = rem( seed );
-    real l = __logistic_core<N>( q - p );
+    const real p = rem( ( ( u & 0x3FF ) * gold ) + u * bronze + ( ( v & 0x3FF ) * silver ) + v * tin ); // safe for large u or v
+    const real q = rem( seed );
+    const real l = __logistic_core<N>( q - p );
     return l * sqrt2;
 }
 
 inline __D__ real mean0var1( real seed, uint u, uint v )
 {
     // 7 FLOPS
-    real p = rem( ( ( u & 0x3FFU ) * gold ) + u * bronze + ( ( v & 0x3FFU ) * silver ) + v * tin ); // safe for large u or v
-    real q = rem( seed );
+    const real p = rem( ( ( u & 0x3FFU ) * gold ) + u * bronze + ( ( v & 0x3FFU ) * silver ) + v * tin ); // safe for large u or v
+    const real q = rem( seed );
 
     // 45+1 FLOPS
-    real l = __logistic_core<N>( q - p );
+    const real l = __logistic_core<N>( q - p );
     // 1 FLOP
     return l * sqrt2;
 }
-struct mean0var1_flops_counter {
-    const static unsigned long long FLOPS = 9ULL + __logistic_core_flops_counter<N>::FLOPS;
-};
 
 inline __D__ real mean0var1( real seed, real u, real v )
 {
-    real p = rem( math::sqrt(u) * gold + sqrtf(v) * silver );
-    real q = rem( seed );
+    const real p = rem( math::sqrt(u) * gold + sqrtf(v) * silver );
+    const real q = rem( seed );
 
-    real l = __logistic_core<N>( q - p );
+    const real l = __logistic_core<N>( q - p );
     return l * sqrt2;
 }
 
 inline __D__ real mean0var1_dual( real seed, real u, real v )
 {
-    real p = rem( math::sqrt(u) * gold + sqrtf(v) * silver );
-    real q = rem( seed );
+    const real p = rem( math::sqrt(u) * gold + sqrtf(v) * silver );
+    const real q = rem( seed );
 
-    real l = __logistic_core<N>( q - p );
-    real z = __logistic_core<N>( q + p - 1._r );
+    const real l = __logistic_core<N>( q - p );
+    const real z = __logistic_core<N>( q + p - 1._r );
     return l + z;
 }
 } // namespace Logistic
@@ -245,10 +179,10 @@ __D__ inline real saru( unsigned int seed1, unsigned int seed2, unsigned int see
     state  = 0x4beb5d59 * state + 0x2600e1f7; // LCG
     wstate = wstate + 0x8009d14b + ( ( ( ( signed int )wstate ) >> 31 ) & 0xda879add ); // OWS
 
-    unsigned int v = ( state ^ ( state >> 26 ) ) + wstate;
-    unsigned int r = ( v ^ ( v >> 20 ) ) * 0x6957f5a7;
+    const unsigned int v = ( state ^ ( state >> 26 ) ) + wstate;
+    const unsigned int r = ( v ^ ( v >> 20 ) ) * 0x6957f5a7;
 
-    real res = r / ( 4294967295.0_r );
+    const real res = r / ( 4294967295.0_r );
     return res;
 }
 
@@ -288,10 +222,6 @@ inline __D__ real mean0var1( real seed, real i, real j )
 {
     return mean0var1( seed, (uint) i, (uint) j );
 }
-
-struct mean0var1_flops_counter {
-    const static unsigned long long FLOPS = 2ULL;
-};
 
 } // namespace Saru
 
