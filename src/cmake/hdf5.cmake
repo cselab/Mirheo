d07cafcb3f1b2ef2177f3ab3835e8eb722972ffd
Lucas Amoudruz (daint) <lucas.amoudruz@wanadoo.fr>
2020-09-10 10:42:47 +0200

    hdf5 fix for daint

--- src/cmake/hdf5.cmake
+++ src/cmake/hdf5.cmake
@@ -4,7 +4,7 @@ find_package(HDF5 REQUIRED)
 # On CRAY systems things are complicated
 # This workaround should work to supply
 # nvcc with correct hdf paths
-if (DEFINED ENV{CRAY_HDF5_DIR})
+if (DEFINED ENV{HDF5_DIR})
   set(HDF5_INCLUDE_DIRS "$ENV{HDF5_DIR}/include")
   set(HDF5_LIBRARIES    "$ENV{HDF5_DIR}/lib/libhdf5.so")
 endif()
