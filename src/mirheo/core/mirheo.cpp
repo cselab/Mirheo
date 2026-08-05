059b7a1c0dbb1e0e0a40508dc374e16f0fe75c8b
Lucas Amoudruz <amlucas@users.noreply.github.com>
2026-08-05 14:16:37 -0600

    Revert "[Minor Bug]: Do not call MPI_Init when MPI is already initialized"

--- src/mirheo/core/mirheo.cpp
+++ src/mirheo/core/mirheo.cpp
@@ -168,14 +168,9 @@ Mirheo::Mirheo(int3 nranks3D, real3 globalDomainSize,
                LogInfo logInfo, CheckpointInfo checkpointInfo,
                real maxObjHalfLength, bool gpuAwareMPI)
 {
-    int alreadyInitialized = 0;
-    MPI_Initialized(&alreadyInitialized);
-    if (!alreadyInitialized) {
-        // e.g. mpi4py imported from python initializes MPI before us
-        MPI_Init(nullptr, nullptr);
-        initializedMpi_ = true;
-    }
+    MPI_Init(nullptr, nullptr);
     MPI_Comm_dup(MPI_COMM_WORLD, &comm_);
+    initializedMpi_ = true;
 
     initLogger(comm_, logInfo);
     init(nranks3D, globalDomainSize, logInfo,
